a474ec4bd80c7e87b73513267ada9d01a3829a4b
Arek Sanecki <areksanecki@gmail.com>
2023-08-28 22:45:12 +0200

    dodanie font style

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import tkinter.font as font
 
 root = Tk()
 root.iconbitmap('logo.ico')
@@ -9,15 +10,18 @@ root.geometry("300x300")
 # Make the window resizable false
 root.resizable(False, False)
 
+# Define font
+button_font = font.Font(family='Comic Sans MS', size=12)
+label_font = font.Font(family='Comic Sans MS', size=16)
 
 # Define Element
 logo = ImageTk.PhotoImage(Image.open("logo1.png"))
 
 # LabelFrame "TTT"
-framettt = Label(root, text="Tic Tac Toe", padx=5, pady=5)
-btn_1 = Button(root, height=2, width=30, padx=5, pady=5, text="Start")
-btn_2 = Button(root, height=2, width=30, padx=5, pady=5, text="Autor")
-btn_3 = Button(root, height=2, width=30, padx=5, pady=5, text="Koniec")
+framettt = Label(root, text="Tic Tac Toe", padx=5, pady=5, font=label_font)
+btn_1 = Button(root, height=2, width=30, padx=5, pady=5, text="Start", font=button_font)
+btn_2 = Button(root, height=2, width=30, padx=5, pady=5, text="Autor", font=button_font)
+btn_3 = Button(root, height=2, width=30, padx=5, pady=5, text="Koniec", font=button_font)
 
 # Position
 framettt.grid(row=0, column=0)
