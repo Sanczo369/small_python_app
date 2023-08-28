b1d054259b80961d102ecb656b8c1cd38d8c19b6
Arek Sanecki <areksanecki@gmail.com>
2023-08-28 22:47:20 +0200

    stylowanie przycisków i nazwy

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -5,7 +5,7 @@ import tkinter.font as font
 root = Tk()
 root.iconbitmap('logo.ico')
 root.title("Tic Tac Toe")
-root.geometry("300x300")
+root.geometry("400x400")
 
 # Make the window resizable false
 root.resizable(False, False)
@@ -18,16 +18,19 @@ label_font = font.Font(family='Comic Sans MS', size=16)
 logo = ImageTk.PhotoImage(Image.open("logo1.png"))
 
 # LabelFrame "TTT"
-framettt = Label(root, text="Tic Tac Toe", padx=5, pady=5, font=label_font)
-btn_1 = Button(root, height=2, width=30, padx=5, pady=5, text="Start", font=button_font)
-btn_2 = Button(root, height=2, width=30, padx=5, pady=5, text="Autor", font=button_font)
-btn_3 = Button(root, height=2, width=30, padx=5, pady=5, text="Koniec", font=button_font)
+framettt = Label(root, height=2, width=12, text="Tic Tac Toe",bg='#000000', fg='#ffffff', font=label_font)
+btn_1 = Button(root, height=2, width=30, text="Start",
+               bg='#45b592', fg='#ffffff', bd=0, font=button_font)
+btn_2 = Button(root, height=2, width=30, text="Autor",
+               bg='#45b592', fg='#ffffff', bd=0, font=button_font)
+btn_3 = Button(root, height=2, width=30, text="Koniec",
+               bg='#45b592', fg='#ffffff', bd=0, font=button_font)
 
 # Position
-framettt.grid(row=0, column=0)
-btn_1.grid(row=1, column=0)
-btn_2.grid(row=2, column=0)
-btn_3.grid(row=3, column=0)
+framettt.grid(row=0, column=0, columnspan=2, padx=50, pady=15)
+btn_1.grid(row=1, column=0, columnspan=4, padx=50, pady=15)
+btn_2.grid(row=2, column=0, columnspan=4, padx=50, pady=15)
+btn_3.grid(row=3, column=0, columnspan=4, padx=50, pady=15)
 
 
 if __name__ == '__main__':
