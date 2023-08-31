15057e391cbe374393e54205dbcaa7218cfa9ff1
Arek Sanecki <areksanecki@gmail.com>
2023-08-31 22:10:31 +0200

    dodanie komentarzy

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -3,15 +3,14 @@ import tkinter.font as font
 from PIL import ImageTk, Image
 
 
-# Function to show the author info window
+# Funkcja wyświetlająca okno informacji o autorze
 def show_author_info():
     # Remove Element Position
-    framettt.grid_forget()
+    text.grid_forget()
     logo0_label.grid_forget()
     btn_1.grid_forget()
     btn_2.grid_forget()
     btn_3.grid_forget()
-
 
     author_label = Label(root, text="Autor: Arkadiusz Sanecki", font=label_font)
     author_label.grid(row=0, column=0, padx=50, pady=20)
@@ -19,25 +18,26 @@ def show_author_info():
     back_button = Button(root, text="Wróć", command=root.destroy, **button_style)
     back_button.grid(row=1, column=0, padx=50, pady=200)
 
+# Fukcja Gry
 def start_game():
-    # Remove Element Position
-    framettt.grid_forget()
+    # Usunięcie Elementów
+    text.grid_forget()
     logo0_label.grid_forget()
     btn_1.grid_forget()
     btn_2.grid_forget()
     btn_3.grid_forget()
 
-    game_frame = Frame(root)  # Create a new frame for the game
+    game_frame = Frame(root)  # Tworzenie nowej ramki dla gry
     game_frame.grid(row=1, column=0, columnspan=4, padx=100, pady=60, sticky="nsew")
 
     back_button = Button(root, text="Wróć", command=root.destroy, **button_style)
     back_button.grid(row=2, column=0, columnspan=4, padx=50, pady=10)
-# Define the initial state of the game (empty cells)
+    #Zdefiniuj początkowy stan gry (puste komórki)
     game_state = [['', '', ''],
                   ['', '', ''],
                   ['', '', '']]
 
-    current_player = 'X'  # Start with player X
+    current_player = 'X'  # Kto zaczyna Gre
 
     def make_move(row, col):
         nonlocal current_player
@@ -49,24 +49,26 @@ def start_game():
             check_winner()
             current_player = 'O' if current_player == 'X' else 'X'  # Switch players
 
+    # sprawdzenie zwyciezcy
     def check_winner():
-        # Check rows
+        # Wiersz
         for row in game_state:
             if row[0] == row[1] == row[2] != '':
                 announce_winner(row[0])
 
-        # Check columns
+        # Kolumna
         for col in range(3):
             if game_state[0][col] == game_state[1][col] == game_state[2][col] != '':
                 announce_winner(game_state[0][col])
 
-        # Check diagonals
+        # Przekatna \
         if game_state[0][0] == game_state[1][1] == game_state[2][2] != '':
             announce_winner(game_state[0][0])
-
+        # Przekatna /
         if game_state[0][2] == game_state[1][1] == game_state[2][0] != '':
             announce_winner(game_state[0][2])
 
+    # Okno z info kto wygrał
     def announce_winner(winner):
 
         result_label_window = Toplevel(root)
@@ -89,15 +91,22 @@ def start_game():
 
 
 root = Tk()
+
+#konfiguracja okna
 root.iconbitmap('logo.ico')
 root.title("Tic Tac Toe")
 root.geometry("400x400")
 
-# Make the window resizable false
+# Zablokowanie zmiany rozmiaru okna
 root.resizable(False, False)
+
+# Zdefiniowanie Loga
 logo = ImageTk.PhotoImage(Image.open("logo1.png"))
+
+#Style
 button_font = font.Font(family='Comic Sans MS', size=12)
 label_font = font.Font(family='Comic Sans MS', size=16)
+
 button_style = {
     "height": 2,
     "width": 30,
@@ -116,18 +125,16 @@ label_style = {
     "fg": '#ffffff',
 }
 
-# LabelFrame
-framettt = Label(root, text="Tic Tac Toe", **label_style)
+# Zdefiniowanie Elementów
+text = Label(root, text="Tic Tac Toe", **label_style)
 logo0_label = Label(root, image=logo)
-# Define Elements
 btn_1 = Button(root, text="Start", command=start_game, **button_style)
 btn_2 = Button(root, text="Autor", command=show_author_info, **button_style)
 btn_3 = Button(root, text="Koniec", command=quit, **button_style)
 
-# Position
+# Pozycja Elementów
 logo0_label.grid(row=0, column=0, columnspan=2, padx=20, pady=15)
-framettt.grid(row=0, column=2, columnspan=2, padx=50, pady=15)
-
+text.grid(row=0, column=2, columnspan=2, padx=50, pady=15)
 btn_1.grid(row=1, column=0, columnspan=4, padx=50, pady=15)
 btn_2.grid(row=2, column=0, columnspan=4, padx=50, pady=15)
 btn_3.grid(row=3, column=0, columnspan=4, padx=50, pady=15)
