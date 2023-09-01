c514a14ab6fe361eb5a571269ee61631c3431c41
Arek Sanecki <areksanecki@gmail.com>
2023-09-01 18:31:56 +0200

    Utworzenie tkinter

--- qr_code_genrator/main.py
+++ qr_code_genrator/main.py
@@ -1,4 +1,12 @@
 import qrcode
+from tkinter import *
+root = Tk()
+root.iconbitmap('logo.ico')
+root.title("Calculator")
+root.overrideredirect(True)
 
 img = qrcode.make("Hello World! This is Qrcode")
 img.save("mycode.png")
+
+if __name__ == '__main__':
+    root.mainloop()
