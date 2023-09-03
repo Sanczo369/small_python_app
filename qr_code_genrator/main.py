f6452baee79b63b9b9a29f364c6a7853b065e596
Arek Sanecki <areksanecki@gmail.com>
2023-09-03 18:57:48 +0200

    dodanie styli i import tkinter.font

--- qr_code_genrator/main.py
+++ qr_code_genrator/main.py
@@ -1,5 +1,7 @@
 import qrcode
 from tkinter import *
+import tkinter.font as font
+
 root = Tk()
 root.iconbitmap('logo.ico')
 root.title("QR CODE GENERATOR")
@@ -13,10 +15,20 @@ root.resizable(False, False)
 # img = qrcode.make("Hello World! This is Qrcode")
 # img.save("mycode.png")
 
-
+label_font = font.Font(family='Roboto', size=14)
+button_style = {
+    "height": 2,
+    "width": 30,
+    "font": ('Comic Sans MS', 12),
+    "bg": "#e67e22",
+    "fg": '#ecf0f1',
+    "activebackground": "#45b592",
+    "borderwidth": 1,
+    "relief": "solid"
+}
 # Elementy
-content_label = Label(root, text="Wpisz co ma zawierać kod QR")
-name_label = Label(root, text="Podaj nazwe dla kodu QR (pliku png)")
+content_label = Label(root, text="Wpisz co ma zawierać kod QR", font=label_font)
+name_label = Label(root, text="Podaj nazwe dla kodu QR (pliku png)", font=label_font)
 content_entry = Entry(root)
 name_entry = Entry(root)
 generate_btn = Button(root, text="Generuj")
