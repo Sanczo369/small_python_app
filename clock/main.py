c4809f473bcccf449489e60a44c492935a66c6ba
Arek Sanecki <areksanecki@gmail.com>
2023-09-04 17:22:24 +0200

    Dodanie fukcji update oraz Label który wyświetla godzine

--- clock/main.py
+++ clock/main.py
@@ -6,4 +6,12 @@ root.title('clock')
 root.geometry("400x600")
 root.config(bg='black')
 
+def update():
+    clock.config(text=time.strftime("%H:%M:%S"))
+    clock.after(1000,update)
+
+
+clock = Label(root, background = 'black',foreground = 'white', font = ('arial', 40, 'bold'))
+clock.pack()
+update()
 root.mainloop()
