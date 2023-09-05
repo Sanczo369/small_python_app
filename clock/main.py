717a6d26f42b9fb17937b390856ac6c10489e558
Arek Sanecki <areksanecki@gmail.com>
2023-09-05 20:27:13 +0200

    dodanie funkcji reset()

--- clock/main.py
+++ clock/main.py
@@ -2,7 +2,7 @@ from tkinter import *
 import time
 
 running = False
-hours, minute, seconds = 0, 0, 0
+hours, minutes, seconds = 0, 0, 0
 
 def start():
     global running
@@ -17,15 +17,29 @@ def pause():
         clock.after_cancel(update_time)
         running = False
 
+def reset():
+    global running
+    if running:
+        # cancel updating of time using after_cancel()
+        clock.after_cancel(update_time)
+        running = False
+    # set variables back to zero
+    global hours, minutes, seconds
+    hours, minutes, seconds = 0, 0, 0
+    # set label back to zero
+    clock.config(text='00:00:00')
+
 root = Tk()
 root.title('clock')
 root.geometry("400x600")
 root.config(bg='black')
 root.iconbitmap('logo.ico')
 
-def update():
-    clock.config(text=time.strftime("%H:%M:%S"))
-    clock.after(1000, update)
+
+# def update():
+#     clock.config(text=time.strftime("%H:%M:%S"))
+#     clock.after(1000, update)
+
 
 button_style = {
     "height": 2,
