394046eba312709d75c8bf78d2b80383c5971460
Arek Sanecki <areksanecki@gmail.com>
2023-09-07 00:42:33 +0200

    dodanie elementów: clock, calendar oraz ich pozycje

--- time and data/main.py
+++ time and data/main.py
@@ -5,4 +5,10 @@ root.title('time and data')
 root.geometry("400x400")
 root.config(bg='black')
 
+
+
+clock = Label(root, background='black', foreground='white', font=('arial', 40, 'bold'))
+calendar = Label(root, background='black', foreground='white', font=('arial', 40, 'bold'))
+clock.grid(column=0, row=0)
+calendar.grid(column=0, row=1)
 root.mainloop()
