9c18e8d86ec8b2c1e65901e57fab89d2a37209f6
Arek Sanecki <areksanecki@gmail.com>
2023-09-07 00:43:17 +0200

    dodanie funkcji update()

--- time and data/main.py
+++ time and data/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from datetime import date
+import time
 
 root = Tk()
 root.title('time and data')
@@ -6,9 +8,14 @@ root.geometry("400x400")
 root.config(bg='black')
 
 
+def update():
+    calendar.config(text=date.today().strftime("%d/%m/%Y"))
+    clock.config(text=time.strftime("%H:%M:%S"))
+    clock.after(1000, update)
 
 clock = Label(root, background='black', foreground='white', font=('arial', 40, 'bold'))
 calendar = Label(root, background='black', foreground='white', font=('arial', 40, 'bold'))
 clock.grid(column=0, row=0)
 calendar.grid(column=0, row=1)
+update()
 root.mainloop()
