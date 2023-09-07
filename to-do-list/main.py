a1aa1319f0d1786264136dcc6cb9c40ab9b91a33
Arek Sanecki <areksanecki@gmail.com>
2023-09-07 12:40:34 +0200

    dodanie belki pozowalajacej dodać elementy do listy

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -5,4 +5,8 @@ root.title('To Do List')
 root.geometry("400x400")
 root.config(bg='black')
 
+addingEntry = Entry(root, bg="#FFF", width=19, font=("Arial", 25))
+add_btn=Button(root, text="+", height=1, width=3, font=("Arial", 16, 'bold'), bg="#00ff00", fg='#ffffff')
+addingEntry.grid(row=1, column=0)
+add_btn.grid(row=1, column=1, padx=4)
 root.mainloop()
