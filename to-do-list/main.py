068a214db45af4d0058f258f240e1d83108e22b5
Arek Sanecki <areksanecki@gmail.com>
2023-09-07 12:41:13 +0200

    dodanie funkcji add()

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -5,6 +5,18 @@ root.title('To Do List')
 root.geometry("400x400")
 root.config(bg='black')
 
+value=1
+def add():
+    global value
+    text=addingEntry.get()
+    addingEntry.delete(0, END)
+    lab=Label(root, width=18, text=str(value)+". "+text, font=("Arial", 25))
+
+    del_btn = Button(root, text="-", height=1, width=3, font=("Arial", 16, 'bold'), bg="#ff0000", fg='#ffffff')
+    lab.grid(row=1 + value, column=0)
+    del_btn.grid(row=1 + value, column=1)
+    value+=1
+
 addingEntry = Entry(root, bg="#FFF", width=19, font=("Arial", 25))
 add_btn=Button(root, text="+", height=1, width=3, font=("Arial", 16, 'bold'), bg="#00ff00", fg='#ffffff')
 addingEntry.grid(row=1, column=0)
