a53c8cfbceac5714104203e366df53cceb0e92b7
Arek Sanecki <areksanecki@gmail.com>
2023-09-11 14:53:09 +0200

    Dodanie do quiz_brain has_more_questions() i next_question()

--- quiz/quiz_brain.py
+++ quiz/quiz_brain.py
@@ -5,3 +5,15 @@ class QuizBrain:
         self.score = 0
         self.questions = questions
         self.current_question = None
+
+    def has_more_questions(self):
+        """sprawdza, czy quiz zawiera więcej pytań"""
+
+        return self.question_no < len(self.questions)
+
+    def next_question(self):
+        """Następne pytanie"""
+        self.current_question = self.questions[self.question_no]
+        self.question_no += 1
+        q_text = self.current_question.question_text
+        return f"Q.{self.question_no}: {q_text}"
