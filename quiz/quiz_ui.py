657fcf9259721bab02ac8cf4126cbef8e4468baa
Arek Sanecki <areksanecki@gmail.com>
2023-09-11 15:16:22 +0200

    Dodanie do quiz_ui

---
+++ quiz/quiz_ui.py
@@ -0,0 +1,45 @@
+from tkinter import Tk, Canvas, StringVar, Label, Radiobutton, Button, messagebox
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("iQuiz App")
+        self.window.geometry("850x530")
+
+        # Display Title
+        self.display_title()
+
+        # Creating a canvas for question text, and dsiplay question
+        self.canvas = Canvas(width=800, height=250)
+        self.question_text = self.canvas.create_text(400, 125,
+                                                     text="Question here",
+                                                     width=680,
+                                                     fill=THEME_COLOR,
+                                                     font=(
+                                                         'Ariel', 15, 'italic')
+                                                     )
+        self.canvas.grid(row=2, column=0, columnspan=2, pady=50)
+        self.display_question()
+
+        # Declare a StringVar to store user's answer
+        self.user_answer = StringVar()
+
+        # Display four options(radio buttons)
+        self.opts = self.radio_buttons()
+        self.display_options()
+
+        # To show whether the answer is correct or wrong
+        self.feedback = Label(self.window, pady=10, font=("ariel", 15, "bold"))
+        self.feedback.place(x=300, y=380)
+
+        # Next and Quit Button
+        self.buttons()
+
+        # Mainloop
+        self.window.mainloop()
