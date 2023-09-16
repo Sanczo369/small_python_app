31238f6ab3ccc479449d9b57f6370118de69fcc9
Arek Sanecki <areksanecki@gmail.com>
2023-09-16 17:33:02 +0200

    dodanie przycisku gumki do mazania do White_Board oraz Palety na kolory

--- white_board/main.py
+++ white_board/main.py
@@ -12,4 +12,10 @@ root.iconbitmap('logo.ico')
 color_box=PhotoImage(file="color_section.png")
 Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
 
+eraser=PhotoImage(file="rubber_96712.png")
+Button(root, image=eraser, bg='#f2f3f5').place(x=45, y=400)
+
+colors=Canvas(root, bg="#ffffff", width=40, height=300, bd=0)
+colors.place(x=42, y=55)
+
 root.mainloop()
