2a67d5fdea72a343447df40d13b7ade96d650452
Arek Sanecki <areksanecki@gmail.com>
2023-09-16 17:25:20 +0200

    dodanie color_section do White_Board

--- white_board/main.py
+++ white_board/main.py
@@ -7,4 +7,9 @@ root.config(bg='#f2f3f5')
 root.resizable(False,False)
 root.iconbitmap('logo.ico')
 
+
+
+color_box=PhotoImage(file="color_section.png")
+Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
+
 root.mainloop()
