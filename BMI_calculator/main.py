b83f8110ad5df42f5c7f33358fadfe71dbb78e5a
Arek Sanecki <areksanecki@gmail.com>
2023-09-18 17:37:00 +0200

    dodanie górnej belki do projektu BMI Calculator

--- BMI_calculator/main.py
+++ BMI_calculator/main.py
@@ -7,6 +7,12 @@ root.resizable(False, False)
 root.configure(bg="#f0f1f5")
 root.iconbitmap('logo.ico')
 
+top=PhotoImage(file="top.png")
+top_image=Label(root, image=top, background="#f0f1f5")
+top_image.place(x=50,y=0)
+
+
+
 
 
 root.mainloop()
