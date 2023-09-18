b847cf4aa12a9b03917f68115ac24329f7995cb9
Arek Sanecki <areksanecki@gmail.com>
2023-09-18 17:43:11 +0200

    dodanie 2 boxów do projektu BMI Calculator

--- BMI_calculator/main.py
+++ BMI_calculator/main.py
@@ -11,8 +11,9 @@ top=PhotoImage(file="top.png")
 top_image=Label(root, image=top, background="#f0f1f5")
 top_image.place(x=50,y=0)
 
+Label(root, width=72, height=18, bg="lightblue").pack(side=BOTTOM)
 
-
-
-
+box=PhotoImage(file="box.png")
+Label(root,image=box).place(x=40,y=100)
+Label(root,image=box).place(x=260,y=100)
 root.mainloop()
