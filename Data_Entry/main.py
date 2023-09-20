8f0b3e20c7a51c1001c1632c9b7175d5c59f973a
Sanczo369 <areksanecki@gmail.com>
2023-09-20 13:05:39 +0200

    Dodanie projektu "Data Entry"

---
+++ Data_Entry/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter.ttk import Combobox
+import tkinter as tk
+from tkinter import messagebox
+import openpyxl, xlrd
+from openpyxl import Workbook
+import pathlib
+
+root=Tk()
+root.title("Data Entry")
+root.geometry('700x400+300+200')
+root.resizable(False, False)
+root.configure(bg="#326273")
+
+root.mainloop()
