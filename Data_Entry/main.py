a54f819bbbaef9a013a37601ad0a7a786b3f8d47
Sanczo369 <areksanecki@gmail.com>
2023-09-20 13:11:06 +0200

    Dodanie górnej belki do "Data Entry"

--- Data_Entry/main.py
+++ Data_Entry/main.py
@@ -11,5 +11,11 @@ root.title("Data Entry")
 root.geometry('700x400+300+200')
 root.resizable(False, False)
 root.configure(bg="#326273")
+# root.iconbitmap('logo.ico')
+
+Label(root, text="Proszę o wypełnienie formularza zgłoszeniowego", font="arial 13", bg="#326273", fg="#fff").place(x=20,y=20)
+
+
+
 
 root.mainloop()
