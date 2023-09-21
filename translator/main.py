da40def8cc52afcdefe32618b96da21b1dd69af7
Sanczo369 <areksanecki@gmail.com>
2023-09-21 11:16:22 +0200

    Dodanie projektu "Translator"

---
+++ translator/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+# import googletrans
+# import textblob
+
+root = Tk()
+root.title("Google Translator")
+root.geometry("1080x400")
+root.configure(bg="white")
+root.mainloop()
