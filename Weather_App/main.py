c513d9bb3a86d85cedc00dc97073cffe771c6851
Arek Sanecki <areksanecki@gmail.com>
2023-09-24 18:45:51 +0200

    dodanie elementów wyszukiwarki w Weather_App

--- Weather_App/main.py
+++ Weather_App/main.py
@@ -12,4 +12,11 @@ root.title("Weather_App")
 root.geometry('900x500+300+200')
 root.resizable(False,False)
 
+#search box
+textfield = tk.Entry(root, justify='center', width=17, font=("poppins", 25, "bold"), bg="#404040", fg="white")
+textfield.place(x=50, y=40)
+textfield.focus()
+#search icon
+myimage_icon = Button(borderwidth=0, cursor="hand2", bg="#404040")
+myimage_icon.place(x=400, y=34)
 root.mainloop()
