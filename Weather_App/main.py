9c4bb59510dbc27de620551c6d83b3e94c706b7e
Arek Sanecki <areksanecki@gmail.com>
2023-09-24 19:04:44 +0200

    dodanie elementów label w Weather_App

--- Weather_App/main.py
+++ Weather_App/main.py
@@ -19,14 +19,27 @@ textfield.focus()
 #search icon
 myimage_icon = Button(borderwidth=0, cursor="hand2", bg="#404040")
 myimage_icon.place(x=400, y=34)
-root.mainloop()
+
 
 #logo
-Logo_image=PhotoImage(file="logo.png")
-logo=Label(image=Logo_image)
-logo.place(x=150, y=100)
+# Logo_image=PhotoImage(file="logo.png")
+# logo=Label(image=Logo_image)
+# logo.place(x=150, y=100)
+#
+# # Bottom box
+# Frame_image = PhotoImage(file='box.png')
+# frame_myimage=Label(image=Frame_image)
+# frame_myimage.pack(padx=5, pady=5, side=BOTTOM)
 
-# Bottom box
-Frame_image = PhotoImage(file='box.png')
-frame_myimage=Label(image=Frame_image)
-frame_myimage.pack(padx=5, pady=5, side=BOTTOM)
+#label
+label1=Label(root, text="WIATR", font=("Helvetica", 15, "bold"), fg="white",bg="#1ab5ef")
+label1.place(x=120, y=400)
+
+label2=Label(root, text="WILGOTNOŚĆ", font=("Helvetica", 15, "bold"), fg="white",bg="#1ab5ef")
+label2.place(x=250, y=400)
+
+label3=Label(root, text="OPIS", font=("Helvetica", 15, "bold"), fg="white",bg="#1ab5ef")
+label3.place(x=430, y=400)
+
+label4=Label(root, text="CIŚNIENIE", font=("Helvetica", 15, "bold"), fg="white",bg="#1ab5ef")
+label4.place(x=650, y=400)
