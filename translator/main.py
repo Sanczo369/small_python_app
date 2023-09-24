e6b4ae57dfb053d89eb0087870407a997556def1
Arek Sanecki <areksanecki@gmail.com>
2023-09-24 18:00:39 +0200

    aktualizacja Google Translator

--- translator/main.py
+++ translator/main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk, messagebox
-import googletrans
-import textblob
+from pygoogletranslation import Translator
+from googletrans import models
 
 root = Tk()
 root.title("Google Translator")
@@ -9,39 +9,35 @@ root.geometry("1080x400")
 root.iconbitmap('logo.ico')
 
 def label_change():
-    c=combo1.get()
-    c1=combo2.get()
-    label1.configure(text=c)
-    label2.configure(text=c1)
+    c1=combo1.get()
+    c2=combo2.get()
+    label1.configure(text=c1)
+    label2.configure(text=c2)
     root.after(1000, label_change)
 
-def translate_now():
-    global language
-    try:
-        text_=text1.get(1.0,END)
-        c2=combo1.get()
-        c3=combo2.get()
-        if (text_):
-            words=textblob.TextBlob(text_)
-            lan=words.detect_language()
-            for i,j in language.items():
-                if(j==c3):
-                    lan_=i
-            words=words.translate(from_lang=lan,to=str(lan_))
-            text2.delete(1.0,END)
-            text2.insert(END,words)
-    except Exception as e:
-        messagebox.showerror("googletrans", f"please try again       {e}")
+# def translate_now():
+#     global language
+#     try:
+#         c2 = combo1.get()
+#         c3 = combo2.get()
+#         # translator = Translator(service_urls=[
+#         # 	'translate.google.com',
+#         # ])
+#         trans = Translator()
+#         trans_lang=trans.translate(text=text1.get(1.0,"end-1c"),src=c2,dest=c3)
+#         text2.delete(1.0,END)
+#         text2.insert(END,trans_lang)
+#     except Exception as e:
+#         messagebox.showerror("googletrans", f"please try again {e}")
 
 arrow_image=PhotoImage(file="arrow.png")
 image_label=Label(root,image=arrow_image, width=150)
 image_label.place(x=460, y=50)
+translator = Translator()
+language = translator.glanguage()
 
-language = googletrans.LANGUAGES
-languageV = list(language.values())
-lang1 = language.keys()
 
-combo1 = ttk.Combobox(root, values=languageV, font="Roboto 14", state="r")
+combo1 = ttk.Combobox(root, values=language, font="Roboto 14", state="r")
 combo1.place(x=110, y=20)
 combo1.set("ENGLISH")
 
@@ -57,7 +53,7 @@ scrollbar1.configure(command=text1.yview)
 text1.configure(yscrollcommand=scrollbar1.set)
 
 
-combo2 = ttk.Combobox(root, values=languageV, font="Roboto 14", state="r")
+combo2 = ttk.Combobox(root, values=language, font="Roboto 14", state="r")
 combo2.place(x=730, y=20)
 combo2.set("SELECT LANGUAGE")
 
@@ -72,7 +68,7 @@ scrollbar2.pack(side="right", fill="y")
 scrollbar2.configure(command=text2.yview)
 text2.configure(yscrollcommand=scrollbar2.set)
 
-translate= Button(root, text="Translate", font="Roboto 15 bold italic", activebackground="purple", cursor="hand2", bd=5, bg='red', fg='white', command=translate_now)
+translate= Button(root, text="Translate", font="Roboto 15 bold italic", activebackground="purple", cursor="hand2", bd=5, bg='red', fg='white')
 translate.place(x=480, y=250)
 root.configure(bg="white")
 label_change()
