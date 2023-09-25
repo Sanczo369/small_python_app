577a65b1a693e8da4199d309bcff36528c71dda6
Arek Sanecki <areksanecki@gmail.com>
2023-09-25 22:36:31 +0200

    dodanie listbox w ToDoList

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -26,7 +26,7 @@ heading.place(x=130, y=20)
 frame=Frame(root, width=400, height=50, bg="white")
 frame.place(x=0,y=100)
 
-addingEntry=StringVar()
+task=StringVar()
 addingEntry = Entry(frame, bg="#FFF", width=18, font=("Arial", 25), bd=0)
 addingEntry.place(x=10, y=7)
 addingEntry.focus()
@@ -34,6 +34,16 @@ addingEntry.focus()
 
 add_btn=Button(frame, text="+", width=6, font=("Arial", 16, 'bold'), bg="#00ff00", fg='#ffffff')
 add_btn.place(x=300,y=4)
+
+frame1=Frame(root,bd=3,  width=700, height=280, bg="#32405b")
+frame1.pack(pady=(160,0))
+
+listbox= Listbox(frame1, font=("arial",12), width=40, height=16, bg="#32405b", fg="white", cursor="hand2", selectbackground="#5a95ff")
+listbox.pack(side=LEFT, fill=BOTH, padx=2)
+scrollbar= Scrollbar(frame1)
+scrollbar.pack(side=RIGHT, fill= BOTH)
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
 
 
 root.mainloop()
