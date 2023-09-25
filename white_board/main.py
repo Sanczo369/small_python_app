efbadf8456d02b9409134f028e0151b1816fc0b6
Arek Sanecki <areksanecki@gmail.com>
2023-09-25 20:30:59 +0200

    dodanie funkcji insertimage i my_callback w White Board

--- white_board/main.py
+++ white_board/main.py
@@ -1,5 +1,6 @@
+import os
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog
 import tkinter as tk
 
 root = Tk()
@@ -35,6 +36,19 @@ def new_canvas():
     canvas.delete('all')
     display_pallete()
 
+def insertimage():
+    global filename
+    global f_img
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Wybierz plik obrazu", filetypes=(("PNG file", "*.png"), ("All files", "new.txt")))
+    f_img=tk.PhotoImage(file=filename)
+    my_img=canvas.create_image(180,50,image=f_img)
+    root.bind("<B3-Motion>", my_callback)
+
+def my_callback(event):
+    global f_img
+    f_img=tk.PhotoImage(file=filename)
+    my_img=canvas.create_image(event.x, event.y, image=f_img)
+
 
 color_box = PhotoImage(file="color_section.png")
 Label(root, image=color_box, bg="#f2f3f5").place(x=10, y=20)
@@ -43,7 +57,7 @@ eraser = PhotoImage(file="rubber_96712.png")
 Button(root, image=eraser, bg='#f2f3f5', command=new_canvas).place(x=45, y=400)
 
 importimage = PhotoImage(file="img_add.png")
-Button(root, image=importimage, bg='#f2f3f5').place(x=45, y=450)
+Button(root, image=importimage, bg='#f2f3f5', command=insertimage).place(x=45, y=450)
 
 colors = Canvas(root, bg="#ffffff", width=40, height=310, bd=0)
 colors.place(x=42, y=55)
