ff732aa98376e9fb465447e693bd97d34d1d47ed
Arek Sanecki <areksanecki@gmail.com>
2023-09-26 21:28:35 +0200

    dodanie komentarzy do ToDoList

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -1,22 +1,27 @@
 from tkinter import *
 
+''' Parametry Okna '''
 root = Tk()
 root.title('To Do List')
 root.geometry("400x550+400+100")
 root.config(bg='#32405b')
 root.iconbitmap('logo.ico')
 
-task_list=[]
+task_list = []
 
+
+# Funkcje
 def add():
-    task=addingEntry.get()
+    task = addingEntry.get()
     addingEntry.delete(0, END)
     if task:
         with open("tasklist.txt", 'a') as taskfile:
             taskfile.write(f"\n{task}")
         task_list.append(task)
-        listbox.insert( END, task)
-def deleteTask():
+        listbox.insert(END, task)
+
+
+def delete_task():
     global task_list
     task = str(listbox.get(ANCHOR))
     if task in task_list:
@@ -26,14 +31,15 @@ def deleteTask():
                 taskfile.write(task+'\n')
         listbox.delete(ANCHOR)
 
-def openTaskFile():
+
+def open_task_file():
     try:
         global task_list
         with open("tasklist.txt", "r") as taskfile:
             tasks = taskfile.readlines()
 
         for task in tasks:
-            if task !='n':
+            if task != 'n':
                 task_list.append(task)
                 listbox.insert(END, task)
 
@@ -41,32 +47,38 @@ def openTaskFile():
         file = open("tasklist.txt", "w")
         file.close()
 
-heading=Label(root, text="Lista zadań", font=("Arial", 20, 'bold'), fg="white", bg="#32405b")
+
+''' Elementy'''
+# górna belka z Napisem
+heading = Label(root, text="Lista zadań", font=("Arial", 20, 'bold'), fg="white", bg="#32405b")
 heading.place(x=130, y=20)
 
-frame=Frame(root, width=400, height=50, bg="white")
-frame.place(x=0,y=100)
+frame = Frame(root, width=400, height=50, bg="white")
+frame.place(x=0, y=100)
 
-task=StringVar()
+task = StringVar()
 addingEntry = Entry(frame, bg="#FFF", width=18, font=("Arial", 25), bd=0)
 addingEntry.place(x=10, y=7)
 addingEntry.focus()
 
+# przycisk dodawania zadania
+add_btn = Button(frame, text="+", width=6, font=("Arial", 16, 'bold'), bg="#00ff00", fg='#ffffff', command=add)
+add_btn.place(x=300, y=4)
 
-add_btn=Button(frame, text="+", width=6, font=("Arial", 16, 'bold'), bg="#00ff00", fg='#ffffff', command=add)
-add_btn.place(x=300,y=4)
+frame1 = Frame(root, bd=3,  width=700, height=280, bg="#32405b")
+frame1.pack(pady=(160, 0))
 
-frame1=Frame(root,bd=3,  width=700, height=280, bg="#32405b")
-frame1.pack(pady=(160,0))
-
-listbox= Listbox(frame1, font=("arial",12), width=40, height=16, bg="#32405b", fg="white", cursor="hand2", selectbackground="#5a95ff")
+# okno z listą zadań
+listbox = Listbox(frame1, font=("arial", 12), width=40, height=16, bg="#32405b", fg="white", cursor="hand2", selectbackground="#5a95ff")
 listbox.pack(side=LEFT, fill=BOTH, padx=2)
-scrollbar= Scrollbar(frame1)
-scrollbar.pack(side=RIGHT, fill= BOTH)
+scrollbar = Scrollbar(frame1)
+scrollbar.pack(side=RIGHT, fill=BOTH)
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
-Delete_icon=PhotoImage(file="bin.png")
-Button(root,text="Usuń", width=6, font=("Arial", 16, 'bold'), bg="#ff0000", fg='#ffffff', command=deleteTask).pack(side=BOTTOM, pady=13)
+# przycisk usuwania zadania
+Delete_icon = PhotoImage(file="bin.png")
+Button(root, text="Usuń", width=6, font=("Arial", 16, 'bold'), bg="#ff0000", fg='#ffffff', command=delete_task).pack(side=BOTTOM, pady=13)
 
+open_task_file()
 root.mainloop()
