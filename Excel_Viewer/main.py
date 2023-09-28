ba0319072d655bfb2b351c8f7977845982287d05
Arek Sanecki <areksanecki@gmail.com>
2023-09-28 21:51:21 +0200

    dodanie funkcji Open do Excel Viewer

--- Excel_Viewer/main.py
+++ Excel_Viewer/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 
 root = Tk()
 root.title("Excel Viewer")
@@ -7,7 +7,8 @@ root.geometry("1100x400+200+200")
 root.configure(bg='#107C41')
 root.iconbitmap('logo.ico')
 
-
+def Open():
+    filename= filedialog.askopenfilename(title="Open a File", filetypes=(("Open a File", ".xlsx"), ("All files", "*.")))
 # Frame
 frame = Frame(root)
 frame.pack(pady=25)
@@ -17,6 +18,6 @@ tree = ttk.Treeview(frame)
 tree.pack()
 
 # Button
-button = Button(root, text='Open', width=60, height=2, font=30, fg="white", bg="#0078d7")
+button = Button(root, text='Open', width=60, height=2, font=30, fg="white", bg="#0078d7", command=Open)
 button.pack(padx=10, pady=20)
 root.mainloop()
