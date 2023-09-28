fcd6c67d325a0f522f6bf41223db1b27081e88fa
Arek Sanecki <areksanecki@gmail.com>
2023-09-28 21:45:19 +0200

    dodanie Button do Excel Viewer

--- Excel_Viewer/main.py
+++ Excel_Viewer/main.py
@@ -15,3 +15,8 @@ frame.pack(pady=25)
 # Treeview
 tree = ttk.Treeview(frame)
 tree.pack()
+
+# Button
+button = Button(root, text='Open', width=60, height=2, font=30, fg="white", bg="#0078d7")
+button.pack(padx=10, pady=20)
+root.mainloop()
