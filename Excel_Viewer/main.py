64f9b998a3ac2786becc91e9420036ed96629e92
Arek Sanecki <areksanecki@gmail.com>
2023-09-28 21:56:54 +0200

    dodanie warunku do funkcji Open w Excel Viewer

--- Excel_Viewer/main.py
+++ Excel_Viewer/main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk, messagebox, filedialog
 
+import pandas as pd
+
 root = Tk()
 root.title("Excel Viewer")
 root.geometry("1100x400+200+200")
@@ -8,7 +10,14 @@ root.configure(bg='#107C41')
 root.iconbitmap('logo.ico')
 
 def Open():
-    filename= filedialog.askopenfilename(title="Open a File", filetypes=(("Open a File", ".xlsx"), ("All files", "*.")))
+    filename= filedialog.askopenfilename(title="Open a File", filetypes=(("Open a File", ".*xlsx"), ("All files", "*.")))
+
+    if filename:
+        try:
+            filename = r"{}".format(filename)
+            df = pd.read_excel(filename)
+        except:
+            messagebox.showerror("Error", "You can't access this file!")
 # Frame
 frame = Frame(root)
 frame.pack(pady=25)
