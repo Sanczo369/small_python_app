1a082d4e6a0275f1bcbb83ae048fb75614748d0d
Arek Sanecki <areksanecki@gmail.com>
2023-09-28 21:33:08 +0200

    dodanie Projektu Excel Viewer

---
+++ Excel_Viewer/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+root = Tk()
+root.title("Excel Viewer")
+root.geometry("1100x400+200+200")
+root.configure(bg='#107C41')
+
+root.mainloop()
