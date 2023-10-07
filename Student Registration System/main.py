6376f3156c0e07b8c20041ac868ed998f96e7f1d
Arek Sanecki <areksanecki@gmail.com>
2023-10-07 08:47:58 +0200

    Generowanie pliku Student_data.xlsx

--- Student Registration System/main.py
+++ Student Registration System/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
-
-
+from xlsxwriter import Workbook
+import pathlib
 background = '#06283D'
 framebg= '#EDEDED'
 framefg= '#06283D'
@@ -9,7 +9,30 @@ root = Tk()
 root.title("Student Registration System")
 root.geometry("1250x700+210+100")
 root.resizable(False,False)
-root.iconbitmap('logo.ico')
-root.config()
+root.iconbitmap('img/logo.ico')
+root.config(bg=background)
+
+file_path = 'Student_data.xlsx'
+file = pathlib.Path(file_path)
+if file.exists():
+    pass
+else:
+    workbook = Workbook(file_path)
+    sheet = workbook.add_worksheet()
+    sheet.write('A1',"Registration No.")
+    sheet.write('B1',"Name")
+    sheet.write('C1',"Class")
+    sheet.write('D1',"Gender")
+    sheet.write('E1',"DOB")
+    sheet.write('F1',"Date Of Registration")
+    sheet.write('G1',"Religion")
+    sheet.write('H1',"Skill")
+    sheet.write('I1',"Father Name")
+    sheet.write('J1',"Mother Name")
+    sheet.write('K1',"Father's Occupation")
+    sheet.write('L1',"Mother's Occupation")
+    workbook.close()
+
+
 
 root.mainloop()
