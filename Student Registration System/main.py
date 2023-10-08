2e8dad5265292e8c5e9bb68142b35a3678614918
Arek Sanecki <areksanecki@gmail.com>
2023-10-08 21:45:15 +0200

    Dodanie grafiki photo.png

--- Student Registration System/main.py
+++ Student Registration System/main.py
@@ -143,6 +143,12 @@ Mather_Occupation = StringVar()
 MO_entry = Entry(obj2, textvariable=Mather_Occupation, width=20, font="arial 10")
 MO_entry.place(x=630, y=100)
 
+# image
+f=Frame(root, bd=3, bg="black", width=200, height=200, relief=GROOVE)
+f.place(x=1000, y=150)
 
+img=PhotoImage(file="img/photo.png")
+lbl=Label(f, bg="black", image=img)
+lbl.place(x=30, y=10)
 
 root.mainloop()
