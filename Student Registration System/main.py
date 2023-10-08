74b01ce719968877691aa2899ec0f1535a7958a1
Arek Sanecki <areksanecki@gmail.com>
2023-10-08 21:07:21 +0200

    Dodanie Entry, Combobox, Radiobutton umożliwiających wprowadzania wartości

--- Student Registration System/main.py
+++ Student Registration System/main.py
@@ -1,5 +1,7 @@
 from datetime import date
 from tkinter import *
+from tkinter.ttk import Combobox
+
 from xlsxwriter import Workbook
 import pathlib
 background = '#06283D'
@@ -94,9 +96,34 @@ obj.place(x=30, y=470)
 Label(obj,text="Father Name:", font="arial 13", bg=framebg, fg=framefg).place(x=30,y=50)
 Label(obj,text="Father's Occupation:", font="arial 13", bg=framebg, fg=framefg).place(x=30,y=100)
 
-Label(obj,text="Mother Name:", font="arial 13", bg=framebg, fg=framefg).place(x=500,y=50)
+Label(obj,text="Mother Name:", font="arial 13", bg=framebg, fg=framefg).placEntrye(x=500,y=50)
 Label(obj,text="Mother's Occupation:", font="arial 13", bg=framebg, fg=framefg).place(x=500,y=100)
 
+#
+DOB = StringVar()
+dob_entry = Entry(obj, textvariable=DOB, width=20, font="arial 10")
+dob_entry.place(x=160, y=100)
+
+Name = StringVar()
+name_entry = Entry(obj, textvariable=Name,width=20,font="arial 10")
+name_entry.place(x=160, y=50)
+
+radio = IntVar()
+R1 = Radiobutton(obj,text="M", variable=radio, value=1, bg=framebg, fg=framefg, command=selection)
+R1.place(x=160,y=150)
+R2 = Radiobutton(obj,text="K", variable=radio, value=2, bg=framebg, fg=framefg, command=selection)
+R2.place(x=210,y=150)
+
+Religion = StringVar()
+religion_entry = Entry(obj,textvariable=Religion,width=20,font="arial 10")
+religion_entry.place(x=630, y=100)
+
+Skill = StringVar()
+skill_entry = Entry(obj,textvariable=Name, width=20, font="arial 10")
+skill_entry.place(x=630, y=150)
+
+Class = Combobox(obj, values=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"], font="Roboto 10", width=17, state="r")
+Class.place(x=630, y=50)
 
 
 
