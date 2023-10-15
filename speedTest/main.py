9f40600256ff1718563dc9bd636a636cb54ffc50
Arek Sanecki <areksanecki@gmail.com>
2023-10-15 23:47:41 +0200

    dodanie def Check() do "SpeedTest"

--- speedTest/main.py
+++ speedTest/main.py
@@ -1,10 +1,24 @@
 from tkinter import *
+import speedtest
 
 root=Tk()
 root.title("Internet Speed Test")
 root.geometry("360x600")
 root.resizable(False,False)
 root.configure(bg="#1a212d")
+
+def Check():
+    test=speedtest.Speedtest()
+
+    Uploading=test.upload()
+    print(Uploading)
+    downloading= test.download()
+    print(downloading)
+    servernames=[]
+
+    test.get_servers(servernames)
+    print(test.results.ping)
+
 
 
 root.mainloop()
