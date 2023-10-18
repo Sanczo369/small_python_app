22ce4eb9816dde8d5f4bbc87f469cc775ba60a5d
Arek Sanecki <areksanecki@gmail.com>
2023-10-18 21:43:48 +0200

    dodanie logo do projektu "PDF Protector"

--- pdf_protector/main.py
+++ pdf_protector/main.py
@@ -1,8 +1,13 @@
 from tkinter import *
-
+from tkinter import filedialog, messagebox
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os
 root=Tk()
 root.title("PDF Protector")
-root.geometry("60x430+300+100")
+root.geometry("600x430+300+100")
 root.resizable(False,False)
+
+img_logo=PhotoImage(file="logo.png")
+root.iconphoto(False, img_logo)
 
 root.mainloop()
