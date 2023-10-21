d1bdf9db16a879e94857eb8eb3c219b046ad5de4
Arek Sanecki <areksanecki@gmail.com>
2023-10-21 21:41:48 +0200

    Dodanie szkieletu projetu "Snake_Game"

--- snake_game/main.py
+++ snake_game/main.py
@@ -7,4 +7,21 @@ root.resizable(False,False)
 img_logo=PhotoImage(file="logo.png")
 root.iconphoto(False, img_logo)
 
+
+# Klasy
+class Snake:
+    pass
+class Food:
+    pass
+
+# Funkcje
+def next_turn():
+    pass
+def change_direction():
+    pass
+def check_collisions():
+    pass
+def game_over():
+    pass
+
 root.mainloop()
