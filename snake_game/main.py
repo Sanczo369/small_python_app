0f1d9cbaf483cd370b600f08f637605699fa07ad
Arek Sanecki <areksanecki@gmail.com>
2023-10-21 21:51:11 +0200

    Dodanie górej belki oraz pola gry "Snake_Game"

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,7 +2,7 @@ from tkinter import  *
 import random
 root=Tk()
 root.title("Snake Game")
-root.geometry("700x700+300+100")
+root.geometry("720x780+300+100")
 root.resizable(False,False)
 img_logo=PhotoImage(file="logo.png")
 root.iconphoto(False, img_logo)
@@ -14,6 +14,11 @@ BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
+score = 0
+direction = "down"
+GAME_HEIGHT = 700
+GAME_WIDTH = 700
+
 
 
 # Klasy
@@ -31,5 +36,11 @@ def check_collisions():
     pass
 def game_over():
     pass
+
+top_label=Label(root, text="Score:{}".format(score), font=("consolas", 40))
+top_label.pack()
+
+canvas = Canvas(root, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
 
 root.mainloop()
