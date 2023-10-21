dc56c858f91b3994826f21103517c36b55cac146
Arek Sanecki <areksanecki@gmail.com>
2023-10-21 21:39:00 +0200

    Dodanie nowego Projektu "Snake_Game"

---
+++ snake_game/main.py
@@ -0,0 +1,10 @@
+from tkinter import  *
+import random
+root=Tk()
+root.title("Snake Game")
+root.geometry("600x600+300+100")
+root.resizable(False,False)
+img_logo=PhotoImage(file="logo.png")
+root.iconphoto(False, img_logo)
+
+root.mainloop()
