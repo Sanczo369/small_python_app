3ea37987c9c3d56437c555ab1fcd02e595e76e8e
Arek Sanecki <areksanecki@gmail.com>
2023-10-21 21:44:32 +0200

    Dodanie używanych zmiennycg do projetu "Snake_Game"

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,10 +2,18 @@ from tkinter import  *
 import random
 root=Tk()
 root.title("Snake Game")
-root.geometry("600x600+300+100")
+root.geometry("700x700+300+100")
 root.resizable(False,False)
 img_logo=PhotoImage(file="logo.png")
 root.iconphoto(False, img_logo)
+
+# Zmienne
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 
 
 # Klasy
@@ -17,7 +25,7 @@ class Food:
 # Funkcje
 def next_turn():
     pass
-def change_direction():
+def change_direction(new_direction):
     pass
 def check_collisions():
     pass
