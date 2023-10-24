281769122609b692f7b1c5536f7c7f088a69bcec
Arek Sanecki <areksanecki@gmail.com>
2023-10-24 22:20:12 +0200

    dodanie przykładowego Labela CustomTkinter

--- CustomTkinter/main.py
+++ CustomTkinter/main.py
@@ -3,23 +3,30 @@ from PIL import Image
 app = CTk()
 app.geometry("500x400")
 
+# Tryby Wyglądu
 set_appearance_mode("dark")
 # set_appearance_mode("light")
 
-btn = CTkButton(master=app, text="Click Me")
-btn.place(relx= 0.2, rely = 0.1, anchor = "n")
+# Przyciski
+# btn = CTkButton(master=app, text="Click Me")
+# btn.place(relx= 0.2, rely = 0.1, anchor = "n")
+#
+# btn2 = CTkButton(master=app, text="Click Me", corner_radius=32)
+# btn2.place(relx= 0.5, rely = 0.1, anchor = "n")
+#
+# btn3 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="#C850C0", hover_color="#4158D0")
+# btn3.place(relx= 0.8, rely = 0.1, anchor = "n")
+#
+# btn4 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="transparent", hover_color="#4158D0", border_color="#FFCC70", border_width=2)
+# btn4.place(relx= 0.2, rely = 0.3, anchor = "n")
+#
+# img = Image.open("icon.png")
+# btn5 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70", border_width=2, image=CTkImage(dark_image=img, light_image=img))
+# btn5.place(relx= 0.5, rely = 0.3, anchor = "n")
 
-btn2 = CTkButton(master=app, text="Click Me", corner_radius=32)
-btn2.place(relx= 0.5, rely = 0.1, anchor = "n")
+label= CTkLabel(master=app, text="Some Text...", font=("Arial", 20), text_color="#FFCC70")
+label.place(relx=0.5, rely=0.5, anchor="center")
 
-btn3 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="#C850C0", hover_color="#4158D0")
-btn3.place(relx= 0.8, rely = 0.1, anchor = "n")
 
-btn4 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="transparent", hover_color="#4158D0", border_color="#FFCC70", border_width=2)
-btn4.place(relx= 0.2, rely = 0.3, anchor = "n")
-
-img = Image.open("icon.png")
-btn5 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70", border_width=2, image=CTkImage(dark_image=img, light_image=img))
-btn5.place(relx= 0.5, rely = 0.3, anchor = "n")
 
 app.mainloop()
