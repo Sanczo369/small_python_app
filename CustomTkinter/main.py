cd5acce13036c27e43a03727ab236581d327f730
Arek Sanecki <areksanecki@gmail.com>
2023-10-24 22:10:37 +0200

    dodanie przykładowych przycisków do CustomTkinter

--- CustomTkinter/main.py
+++ CustomTkinter/main.py
@@ -3,4 +3,20 @@ from PIL import Image
 app = CTk()
 app.geometry("500x400")
 
+btn = CTkButton(master=app, text="Click Me")
+btn.place(relx= 0.2, rely = 0.1, anchor = "n")
+
+btn2 = CTkButton(master=app, text="Click Me", corner_radius=32)
+btn2.place(relx= 0.5, rely = 0.1, anchor = "n")
+
+btn3 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="#C850C0", hover_color="#4158D0")
+btn3.place(relx= 0.8, rely = 0.1, anchor = "n")
+
+btn4 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="transparent", hover_color="#4158D0", border_color="#FFCC70", border_width=2)
+btn4.place(relx= 0.2, rely = 0.3, anchor = "n")
+
+img = Image.open("icon.png")
+btn5 = CTkButton(master=app, text="Click Me", corner_radius=32, fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70", border_width=2, image=CTkImage(dark_image=img, light_image=img))
+btn5.place(relx= 0.5, rely = 0.3, anchor = "n")
+
 app.mainloop()
