c8d77fed68b04473a04896da34b0eabd2e417003
Arek Sanecki <areksanecki@gmail.com>
2023-10-25 22:58:05 +0200

    dodanie przykładowego Slider w CustomTkinter

--- CustomTkinter/main.py
+++ CustomTkinter/main.py
@@ -39,4 +39,9 @@ set_appearance_mode("dark")
 # checkbox = CTkCheckBox(master=app, text= "Option", fg_color="#C850C0", checkbox_height=30, checkbox_width=30, corner_radius=36)
 # checkbox.place(relx=0.5, rely=0.5, anchor="center")
 
+# Slider
+# slider = CTkSlider(master=app)
+# slider.place(relx=0.5, rely=0.5, anchor="center")
+
+
 app.mainloop()
