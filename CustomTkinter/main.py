1d46794f9ee98ceaf6d8bf1c0a6fdd4b7fdaa949
Arek Sanecki <areksanecki@gmail.com>
2023-10-25 23:10:49 +0200

    dodanie przykładowego textbox w CustomTkinter

--- CustomTkinter/main.py
+++ CustomTkinter/main.py
@@ -52,5 +52,11 @@ set_appearance_mode("dark")
 # entry= CTkEntry(master=app, placeholder_text="Start typing...", width=300, text_color="#FFCC70")
 # entry.place(relx=0.5, rely=0.5, anchor="center")
 
+# textbox
+# textbox = CTkTextbox(master=app, scrollbar_button_color="#FFCC70")
+# textbox.place(relx=0.5, rely=0.5, anchor="center")
+
+# textbox = CTkTextbox(master=app, scrollbar_button_color="#FFCC70", corner_radius=16, border_color="#FFCC70", border_width=2)
+# textbox.place(relx=0.5, rely=0.5, anchor="center")
 
 app.mainloop()
