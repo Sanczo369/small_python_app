543df6dba62cdf33acdbf632bf62a5cb21cd1d09
Arek Sanecki <areksanecki@gmail.com>
2023-10-27 14:37:03 +0200

    zmiana tkinter na customtkinterdo projektu Dice_Roll

--- Dice_Roll/main.py
+++ Dice_Roll/main.py
@@ -1,23 +1,22 @@
 import random
-from tkinter import *
+from customtkinter import *
 
-root=Tk()
+root = CTk()
 root.title("Dice Roll")
 root.geometry('700x450+300+200')
 root.resizable(False, False)
 root.iconbitmap('logo.ico')
-
+set_appearance_mode("dark")
 def roll():
     number=['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
     x1=random.choice(number)
     x2=random.choice(number)
-    l1.config(text=f'{x1}{x2}')
-    l1.pack()
+    l1 = CTkLabel(master=root, text=f'{x1}{x2}', font=("Arial", 270), text_color="#FFCC70")
+    l1.place(relx= 0.5, rely = 0.2, anchor = "n")
+
+b1=CTkButton(master=root, text="lets roll...",font=("Arial", 30), corner_radius=32, fg_color="#4158D0", hover_color="#4158D0", command=roll)
+b1.place(relx= 0.5, rely = 0.1, anchor = "n")
 
 
-l1=Label(root, font=("times", 200))
-b1=Button(root, text="lets roll...", command=roll)
-b1.place(x=330,y=0)
 
-
-mainloop()
+root.mainloop()
