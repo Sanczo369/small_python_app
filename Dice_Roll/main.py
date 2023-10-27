5131f1aa0d66c0f858eeb1e05e4f55ac5288f3af
Arek Sanecki <areksanecki@gmail.com>
2023-10-27 14:22:59 +0200

    dodanie funkcji roll do projektu Dice_Roll

--- Dice_Roll/main.py
+++ Dice_Roll/main.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *
 
 root=Tk()
@@ -6,5 +7,11 @@ root.geometry('700x450+300+200')
 root.resizable(False, False)
 root.iconbitmap('logo.ico')
 
+def roll():
+    number=['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    x1=random.choice(number)
+    x2=random.choice(number)
+    l1.config(text=f'{x1}{x2}')
+    l1.pack()
 
 mainloop()
