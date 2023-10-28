c5c25d120c92f90f8672d9fbd2e8a992611e95cd
Arek Sanecki <areksanecki@gmail.com>
2023-10-28 17:10:05 +0200

    Dodanie lower_farme do Projektu "Media Player"

--- player/main.py
+++ player/main.py
@@ -7,4 +7,7 @@ root.resizable(False, False)
 root.iconbitmap('logo.ico')
 root.config(bg="#333333")
 
+
+lower_farme = Frame(root, bg ="#ffffff", width=485, height=180)
+lower_farme.place(x=0, y=400)
 mainloop()
