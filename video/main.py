2ae6a6281f8909ba5a00505133dce1de575b8eea
Arek Sanecki <areksanecki@gmail.com>
2023-10-31 22:18:06 +0100

    Dodanie bibliotek do Projektu "Video Player"

--- video/main.py
+++ video/main.py
@@ -1,6 +1,14 @@
+from tkinter import *
+import datetime
 import tkinter as tk
+from tkinter import filedialog
+from tkVideoPlayer import TkinterVideo
 
 root = tk.Tk()
 root.title("Simpli Video Player")
 root.geometry("800x700+290+10")
+frame = tk.Frame(root)
+frame.pack()
+
+
 root.mainloop()
