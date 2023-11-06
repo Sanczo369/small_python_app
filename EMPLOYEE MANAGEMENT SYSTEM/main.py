7e7ee90ee84e662839c937c90e467724b0347c33
Arek Sanecki <areksanecki@gmail.com>
2023-11-06 22:50:42 +0100

    Utworzenie projektu "EMPLOYEE MANAGEMENT SYSTEM"

---
+++ EMPLOYEE MANAGEMENT SYSTEM/main.py
@@ -0,0 +1,8 @@
+from customtkinter import *
+
+app = CTk()
+app.geometry("500x400")
+app.title("EMPLOYEE MANAGEMENT SYSTEM")
+app.resizable(False, False)
+app.configure(bg="#326273")
+app.mainloop()
