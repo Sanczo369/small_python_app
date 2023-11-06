95bef8bfe9bed82133e98ec9e25d94df44e1d6b6
Arek Sanecki <areksanecki@gmail.com>
2023-11-06 22:57:11 +0100

    dodanie do pliku database.py funkcji fetch_employees()

--- EMPLOYEE MANAGEMENT SYSTEM/database.py
+++ EMPLOYEE MANAGEMENT SYSTEM/database.py
@@ -13,3 +13,11 @@ def create_table():
             status TEXT)''')
     conn.commit()
     conn.close()
+
+def fetch_employees():
+    conn = sqlite3.connect('Employees.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Employees')
+    employees = cursor.fetchall()
+    conn.close()
+    return employees
