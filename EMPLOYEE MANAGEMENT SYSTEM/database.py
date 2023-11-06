d0a59d52b5fea4fab9b0d93458ec0789a625b08e
Arek Sanecki <areksanecki@gmail.com>
2023-11-06 22:54:18 +0100

    Utworzenie pliku database.py z funkcją create_table()

---
+++ EMPLOYEE MANAGEMENT SYSTEM/database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect('Employees.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Employees (
+            id TEXT PRIMARY KEY,
+            name TEXT,
+            role TEXT,
+            gender TEXT,
+            status TEXT)''')
+    conn.commit()
+    conn.close()
