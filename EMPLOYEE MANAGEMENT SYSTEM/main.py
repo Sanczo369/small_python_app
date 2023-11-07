f520b9ea48b9ba69ce0e3384688613e41520403d
Arek Sanecki <areksanecki@gmail.com>
2023-11-07 19:00:57 +0100

    aktualizacja danych konfiguracyjnych w pliku main.py

--- EMPLOYEE MANAGEMENT SYSTEM/main.py
+++ EMPLOYEE MANAGEMENT SYSTEM/main.py
@@ -10,4 +10,8 @@ app.title("Employee Management SystemM")
 app.geometry("900x420")
 app.resizable(False, False)
 app.configure(bg="#161C25")
+
+font1 = ('Arial',20,'bold')
+font2 = ('Arial',12,'bold')
+
 app.mainloop()
