175204e8591f735d969daef6a552d596fb63507e
Arek Sanecki <areksanecki@gmail.com>
2023-11-08 23:31:06 +0100

    dodanie add_button i clear_button do main.py

--- EMPLOYEE MANAGEMENT SYSTEM/main.py
+++ EMPLOYEE MANAGEMENT SYSTEM/main.py
@@ -42,4 +42,13 @@ status_label = customtkinter.CTkLabel(app, font=font1, text="Status:", text_colo
 status_label.place(x=20, y=260)
 status_entry = customtkinter.CTkEntry(app, font=font1, text_color='#000', fg_color="#fff", border_color='#0C9295', border_width=2, width=180)
 status_entry.place(x=100, y=260)
+
+add_button = customtkinter.CTkButton(app, font = font1, text_color='#fff', text='Add Employee', fg_color='#05A312', hover_color="#00850B", bg_color='#161C25', cursor='hand2', corner_radius=15, width=260)
+add_button.place(x=20, y=310)
+
+
+clear_button = customtkinter.CTkButton(app, font = font1, text_color='#fff', text='New Employee', fg_color='#161C25', hover_color="#FF5002", bg_color='#161C25',border_color='#F15704', border_width=2, cursor='hand2', corner_radius=15, width=260)
+clear_button.place(x=20, y=360)
+
+
 app.mainloop()
