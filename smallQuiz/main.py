f4891b9c7dd72ca5d6221c719aa7cd5e7663c8db
Arek Sanecki <areksanecki@gmail.com>
2023-11-11 16:40:53 +0100

    dodanie UI do projektu smallQuiz

--- smallQuiz/main.py
+++ smallQuiz/main.py
@@ -8,5 +8,17 @@ from quiz_data import quiz_data
 app = customtkinter.CTk()
 app.title("SmallQuiz")
 app.geometry('600x500')
+style=Style(theme='flatly')
 
 app.mainloop()
+
+# Create the question label
+qs_label = customtkinter.CTkLabel(app, anchor='center', wraplength=500, padding=10)
+qs_label.pack(pady=10)
+
+# Create the choice buttons
+choice_btns = []
+for i in range(4):
+    button = customtkinter.CTkButton(app, command=lambda i=i: check_answer(i))
+    button.pack(pady=5)
+    choice_btns.append(button)
