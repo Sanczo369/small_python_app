b2f905e15d5cef90d873b35f0e8d1e5266dffb07
Arek Sanecki <areksanecki@gmail.com>
2023-11-11 16:34:17 +0100

    dodanie main.py do projektu smallQuiz

---
+++ smallQuiz/main.py
@@ -0,0 +1,12 @@
+import customtkinter
+import tkinter as tk
+from tkinter import messagebox, ttk
+from ttkbootstrap import Style
+from quiz_data import quiz_data
+
+# Create the main Window
+app = customtkinter.CTk()
+app.title("SmallQuiz")
+app.geometry('600x500')
+
+app.mainloop()
