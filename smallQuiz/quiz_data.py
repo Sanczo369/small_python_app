4dbf5c859216f5397581bd8216c020354f3512f9
Arek Sanecki <areksanecki@gmail.com>
2023-11-11 16:29:48 +0100

    dodanie quiz_data do projektu smallQuiz

---
+++ smallQuiz/quiz_data.py
@@ -0,0 +1,22 @@
+quiz_data = [
+    {
+        "question": "Question 1",
+        "choices": ['choices1','choices2','choices3','choices4'],
+        "answer":'choices1'
+    },
+    {
+        "question": "Question 2",
+        "choices": ['choices1', 'choices2', 'choices3', 'choices4'],
+        "answer": 'choices2'
+    },
+    {
+        "question": "Question 3",
+        "choices": ['choices1', 'choices2', 'choices3', 'choices4'],
+        "answer": 'choices3'
+    },
+    {
+        "question": "Question 4",
+        "choices": ['choices1', 'choices2', 'choices3', 'choices4'],
+        "answer": 'choices4'
+    }
+]
