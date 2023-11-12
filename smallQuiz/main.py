5ee468a672f5f2776b7e2826c23ba8c0c798ce8c
Arek Sanecki <areksanecki@gmail.com>
2023-11-12 22:53:49 +0100

    zmiana na tkinter w projekcie smallQuiz

--- smallQuiz/main.py
+++ smallQuiz/main.py
@@ -1,24 +1,38 @@
-import customtkinter
 import tkinter as tk
 from tkinter import messagebox, ttk
 from ttkbootstrap import Style
 from quiz_data import quiz_data
 
+
+def show_question():
+    pass
+
+def  check_answer():
+    pass
+
+def  next_question():
+    pass
+
 # Create the main Window
-app = customtkinter.CTk()
+app = tk.Tk()
 app.title("SmallQuiz")
-app.geometry('600x500')
+app.geometry('600x500+20+20')
 style=Style(theme='flatly')
 
-app.mainloop()
+
 
 # Create the question label
-qs_label = customtkinter.CTkLabel(app, anchor='center', wraplength=500, padding=10)
+qs_label = ttk.Label(app, anchor='center', wraplength='500', padding=10)
 qs_label.pack(pady=10)
 
 # Create the choice buttons
 choice_btns = []
 for i in range(4):
-    button = customtkinter.CTkButton(app, command=lambda i=i: check_answer(i))
+    button = ttk.Button(app, command=lambda i=i: check_answer(i))
     button.pack(pady=5)
     choice_btns.append(button)
+
+
+
+
+app.mainloop()
