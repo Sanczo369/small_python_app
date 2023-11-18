60d635c00ceaef2a0fc8435f89e696fa86823308
Arek Sanecki <areksanecki@gmail.com>
2023-11-18 14:15:01 +0100

    Nowy Projekt 'Youtube Downloader'

---
+++ Youtube-Downloader/main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import os
+
+app = tk.Tk()
+app.title('Youtube Downloader')
+app.geometry("500x400")
+app.configure(background='#cccccc')
+
+
+
+app.mainloop()
