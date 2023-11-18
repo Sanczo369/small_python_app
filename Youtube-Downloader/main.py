c3d0b99dfbd04b550f4b6e515ca97dbd027e24c0
Arek Sanecki <areksanecki@gmail.com>
2023-11-18 14:21:21 +0100

    dodanie URL Message do Projekt 'Youtube Downloader'

--- Youtube-Downloader/main.py
+++ Youtube-Downloader/main.py
@@ -10,6 +10,14 @@ app.geometry("500x400")
 app.iconbitmap('yt.ico')
 app.configure(background='#cccccc')
 
+
+# URL Message
+lbl_url = tk.Label(window, text="Enter URL :", width=10, height=2, fg="black", bg="light steel blue", font=('times', 15, ' bold '))
+lbl_url.place(x=20, y=150)
+
+url = tk.Entry(window, width=40, bg="linen", fg="gray9",font=('times', 15, ' bold '))
+url.place(x=150, y=160)
+
 # Browse Button
 browse = tk.Button(app, text="Browse",command=browse_location,fg='black'  ,bg="DarkGoldenrod1"  ,width=11 ,height=1 , activebackground = "Red" ,font=('times', 15, ' bold '))
 browse.place(x=600, y=270)
