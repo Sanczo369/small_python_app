c5c238ed2d2ddffc8230c8e2504a276c964beb6c
Arek Sanecki <areksanecki@gmail.com>
2023-12-03 18:38:06 +0100

    Dodanie funkcji submit_handler do Projektu "BudgetTracker"

--- BudgetTracker/main.py
+++ BudgetTracker/main.py
@@ -6,4 +6,14 @@ window.configure(bg = "#FFFFFF")
 window.title("Calculator")
 expenses = 1400
 window.resizable(False, False)
+
+def submit_handler():
+    global expenses
+    new_expense_amt = float(entry_2.get())
+    expenses += new_expense_amt
+    canvas.itemconfig(tagOrId=expenses_text, text=f"${expenses}")
+
+
+
+
 window.mainloop()
