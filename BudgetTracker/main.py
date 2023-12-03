983f13aaa5c4af9d151181d86a804f9f7a9b0e76
Arek Sanecki <areksanecki@gmail.com>
2023-12-03 18:37:39 +0100

    Dodanie nowego Projektu "BudgetTracker"

---
+++ BudgetTracker/main.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+window = Tk()
+window.geometry("700x550")
+window.configure(bg = "#FFFFFF")
+window.title("Calculator")
+expenses = 1400
+window.resizable(False, False)
+window.mainloop()
