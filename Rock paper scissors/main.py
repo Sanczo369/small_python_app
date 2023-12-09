3c755ff3c740bb602e7950f5ff0bbfc7b845dc7e
Arek Sanecki <areksanecki@gmail.com>
2023-12-09 22:45:46 +0100

    Dodanie elementów do Projektu GRA "PAPIER, KAMIEŃ, NOŻYCZKI"

--- Rock paper scissors/main.py
+++ Rock paper scissors/main.py
@@ -11,6 +11,20 @@ def main():
     img3 = ImageTk.PhotoImage(Image.open("nozyczki.jpg"))
     var = StringVar()
 
+    #  element
+    tilte_label=Label(root, text='GRA "PAPIER, KAMIEŃ, NOŻYCZKI"', font=("Comic Sans MS", 20, "bold"))
+    choose_label=Label(root,text="PROSZĘ, WYBIERZ:", font=("Arial", 10))
+    frame= LabelFrame(root, pady=10, padx=10)
+    pick1=Button(frame, image=img1,borderwidth=1, relief="solid", bg="white", command=lambda:player_pick("papier"))
+    pick2=Button(frame, image=img2,borderwidth=1, relief="solid", bg="white", command=lambda:player_pick("kamien"))
+    pick3=Button(frame, image=img3,borderwidth=1, relief="solid", bg="white", command=lambda:player_pick("nozyczki"))
+
+    tilte_label.grid(row=0, columnspan=2, sticky=N)
+    choose_label.grid(row=1, columnspan=2)
+    frame.grid(row=2,columnspan=2)
+    pick1.grid(row=1, column=1)
+    pick2.grid(row=1, column=2)
+    pick3.grid(row=1, column=3)
 
 
     root.mainloop()
