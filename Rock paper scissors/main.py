9f80578487a442b6807e701b9c43992e9094c6aa
Arek Sanecki <areksanecki@gmail.com>
2023-12-09 22:45:02 +0100

    Dodanie grafik do Projektu GRA "PAPIER, KAMIEŃ, NOŻYCZKI"

--- Rock paper scissors/main.py
+++ Rock paper scissors/main.py
@@ -5,6 +5,14 @@ def main():
     root.iconbitmap("logo.ico")
     root.geometry("692x522")
     root.resizable(False,False)
+
+    img1 = ImageTk.PhotoImage(Image.open("papier.jpg"))
+    img2 = ImageTk.PhotoImage(Image.open("kamien.jpg"))
+    img3 = ImageTk.PhotoImage(Image.open("nozyczki.jpg"))
+    var = StringVar()
+
+
+
     root.mainloop()
 if __name__ == '__main__':
     main()
