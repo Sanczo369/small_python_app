92c2ad42ad3166c5ff7126920d61bbf592e5cfb5
Arek Sanecki <areksanecki@gmail.com>
2023-12-09 22:43:50 +0100

    Dodanie Projektu GRA "PAPIER, KAMIEŃ, NOŻYCZKI"

---
+++ Rock paper scissors/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+def main():
+    root=Tk()
+    root.title('GRA "PAPIER, KAMIEŃ, NOŻYCZKI"')
+    root.iconbitmap("logo.ico")
+    root.geometry("692x522")
+    root.resizable(False,False)
+    root.mainloop()
+if __name__ == '__main__':
+    main()
