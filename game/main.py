b9fba085212c665bf4829fbf642f71c928a5ba48
Sanczo369 <areksanecki@gmail.com>
2023-12-23 19:29:58 +0100

    dodanie zmiennych kolorów oraz współrzędnych do Projetu "game"

--- game/main.py
+++ game/main.py
@@ -2,5 +2,18 @@ import pygame
 import random
 
 pygame.init()
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
 
+x = random.randint(180,500)
+y = random.randint(0,200)
+a = random.randint(180,500)
+b = random.randint(0,200)
+
+e = 350
+f = 600
+ch = 3
+cha = 5
 
