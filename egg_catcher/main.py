85655381c871bd1139bb7296c272e17a05ff2672
Sanczo369 <areksanecki@gmail.com>
2023-12-24 23:09:32 +0100

    dodanie funkcji create_egg() do Projektu "Egg Catcher"

--- egg_catcher/main.py
+++ egg_catcher/main.py
@@ -37,3 +37,10 @@ lives_remaining = 3
 lives_text = c.create_text(canvas_width-10, 10, anchor="ne", font=game_font, fill="darkblue", text="Lives: "+ str(lives_remaining))
 
 eggs=[]
+
+def create_egg():
+    x = randrange(10, 740)
+    y = 40
+    new_egg = c.create_oval(x, y, x+egg_width, y+egg_height, fill=next(color_cycle), width=0)
+    eggs.append(new_egg)
+    root.after(egg_interval, create_egg)
