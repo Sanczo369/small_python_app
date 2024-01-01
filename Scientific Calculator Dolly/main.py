29becc0346af09737e96d83bdced701826c4cd4a
Arek Sanecki <areksanecki@gmail.com>
2024-01-01 19:46:01 +0100

    Dodanie def take_input() do Aplikacji "Scientific Calculator Dolly"

--- Scientific Calculator Dolly/main.py
+++ Scientific Calculator Dolly/main.py
@@ -23,6 +23,36 @@ class Calculator:
 
         self.butn()
         self.take_input()
+
+    def take_input(self):
+        Label(self.window, text="Number 1 ", width=8, font=("arial", 15, "bold", "italic"), bg="#141414",
+              fg="#00FF00").place(x=20, y=175)
+
+        self.number1 = Entry(self.window, width=8, bg="#3d3d3d", font=("arial", 20, "bold", "italic"),
+                             insertbackground="gold",
+                             fg="gold", bd=3, relief=SUNKEN)
+        self.number1.place(x=130, y=170)
+
+        self.number1.focus()
+
+        Label(self.window, text="Number 2 ", width=8, font=("arial", 16, "bold", "italic"),
+              fg="#00FF00", bg="#141414").place(x=340, y=175)
+
+        self.number2 = Entry(self.window, width=8, bg="#3d3d3d", font=("arial", 20, "bold", "italic"),
+                             insertbackground="gold",
+                             fg="gold", relief=SUNKEN, bd=4)
+        self.number2.place(x=455, y=170)
+
+        self.operator_name = Label(self.window, text="Operation ", width=12, font=("arial", 17, "bold", "italic"),
+                                   bg="#141414", fg="#d96b6b")
+        self.operator_name.place(x=100, y=120)
+
+        self.operator = Entry(self.window, width=12, font=("arial", 20, "bold", "italic"), disabledbackground="#3d3d3d",
+                              disabledforeground="gold",
+                              state="disable", textvar=self.textoperator, bd=5, relief=SUNKEN)
+        self.operator.place(x=250, y=117)
+
+
 if __name__ == '__main__':
     window = Tk()
     window.title("Smart Scientific Calculator")
