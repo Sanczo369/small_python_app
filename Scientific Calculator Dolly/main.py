8b89d3735d1e5fbe254c8c4f4967e6e35d09671d
Arek Sanecki <areksanecki@gmail.com>
2024-01-05 12:59:36 +0100

    Dodanie evaluation_opr(Exponent) do Aplikacji "Scientific Calculator Dolly"

--- Scientific Calculator Dolly/main.py
+++ Scientific Calculator Dolly/main.py
@@ -295,6 +295,15 @@ class Calculator:
                 messagebox.showerror("Error","Something error in input.please check it.")
                 self.information()
                 self.reset_now()
+
+        elif self.work_done == "Exponent":
+            try:
+                result_take = math.exp(eval(self.n1))
+                self.text_value.set(int(result_take)) if int(result_take) == result_take else self.text_value.set(result_take)
+            except:
+                messagebox.showerror("Error","Something error in input.please check it.")
+                self.information()
+                self.reset_now()
 if __name__ == '__main__':
     window = Tk()
     window.title("Smart Scientific Calculator")
