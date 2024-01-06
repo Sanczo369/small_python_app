800b7c274dc97fcd3d5c624b8ab8a05573c1c703
Arek Sanecki <areksanecki@gmail.com>
2024-01-06 14:56:57 +0100

    Dodanie evaluation_opr(Modulus) do Aplikacji "Scientific Calculator Dolly"

--- Scientific Calculator Dolly/main.py
+++ Scientific Calculator Dolly/main.py
@@ -369,6 +369,14 @@ class Calculator:
                 messagebox.showerror("Error", "Something error in input.please check it.")
                 self.information()
                 self.reset_now()
+        elif self.work_done == "Modulus":
+            try:
+                result_take = eval(self.n1)%eval(self.n2)
+                self.text_value.set(int(result_take)) if int(result_take) == result_take else self.text_value.set(result_take)
+            except:
+                messagebox.showerror("Error","Something error in input.please check it.")
+                self.information()
+                self.reset_now()
 if __name__ == '__main__':
     window = Tk()
     window.title("Smart Scientific Calculator")
