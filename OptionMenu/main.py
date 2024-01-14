9e6610f5d635148168fe5e89eb9c583bdacca951
Arek Sanecki <areksanecki@gmail.com>
2024-01-14 23:39:02 +0100

    Dodanie show_values() do projektu "RadioButton"

--- OptionMenu/main.py
+++ OptionMenu/main.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 
 win = tk.Tk()
+
+def show_values():
+    a = " ".join([str(i.get()) for i in values])
+    tk.Label(win, text=a).grid()
+
 tk.Button(win, text='Show Values', command=show_values).grid(columnspan=2)
 
 win.mainloop()
