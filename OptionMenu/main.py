2cf9702d494367a547abd15d9863978814b68ae8
Arek Sanecki <areksanecki@gmail.com>
2024-01-14 23:43:51 +0100

    Dodanie projektu "GasGen"

--- OptionMenu/main.py
+++ OptionMenu/main.py
@@ -1,5 +1,12 @@
 from tkinter import OptionMenu
 import tkinter as Tkinter
+class GasGen(Tkinter.Tk):
+	def __init__(self):
+		super().__init__()
+		self.vars = []
+		self.initialize()
+		self.grid()
+
 
 if __name__ == "__main__":
 	app = GasGen()
