135b21266486e7f430ca36bb232bc53ee31125a6
Arek Sanecki <areksanecki@gmail.com>
2024-01-22 14:44:13 +0100

    Dodanie generate_password 2/3 do projektu "MyPass"

--- MyPass/main.py
+++ MyPass/main.py
@@ -20,7 +20,12 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
 window = Tk()
 window.title("MyPass")
 window.config(padx=50, pady=50, bg="#F0F0F0")
