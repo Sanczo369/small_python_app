f9d77ac4e06e3be4bb9e8a764b2696004a278709
Arek Sanecki <areksanecki@gmail.com>
2024-01-22 14:43:42 +0100

    Dodanie resource_path do projektu "MyPass"

--- MyPass/main.py
+++ MyPass/main.py
@@ -4,6 +4,16 @@ from random import choice, randint, shuffle
 import pyperclip
 import os
 import sys
+
+
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 window = Tk()
 window.title("MyPass")
 window.config(padx=50, pady=50, bg="#F0F0F0")
