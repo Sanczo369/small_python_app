d2b795b980bfa0b98077a3b360197757257d63f7
Arek Sanecki <areksanecki@gmail.com>
2024-01-22 14:44:42 +0100

    Dodanie save do projektu "MyPass"

--- MyPass/main.py
+++ MyPass/main.py
@@ -31,7 +31,21 @@ def generate_password():
     password_entry.delete(0, END)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
 
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                             f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("passwords.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 window = Tk()
 window.title("MyPass")
 window.config(padx=50, pady=50, bg="#F0F0F0")
