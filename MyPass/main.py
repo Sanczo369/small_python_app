335d05533c504b80270d32e32e9736ec11cf33d8
Arek Sanecki <areksanecki@gmail.com>
2024-01-22 14:43:24 +0100

    Dodanie nowego projektu "MyPass"

---
+++ MyPass/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox, simpledialog
+from random import choice, randint, shuffle
+import pyperclip
+import os
+import sys
+window = Tk()
+window.title("MyPass")
+window.config(padx=50, pady=50, bg="#F0F0F0")
+window.mainloop()
