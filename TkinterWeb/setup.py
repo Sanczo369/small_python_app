a47c46f7e2981b958cb749d7ad7ea4e919770afe
Arek Sanecki <areksanecki@gmail.com>
2024-01-28 18:49:48 +0100

    dodanie cls_termninal()  do "Interleaving Simulator Python"

--- TkinterWeb/setup.py
+++ TkinterWeb/setup.py
@@ -3,6 +3,15 @@ from tkinter import messagebox
 from time import sleep
 import random
 
+def cls_termninal() -> None:
+    import sys, subprocess
+
+    operating_system = sys.platform
+    if operating_system == 'win32':
+        subprocess.run('cls', shell=True)
+    elif operating_system == 'linux' or operating_system == 'darwin':
+        subprocess.run('clear', shell=True)
+
 if __name__ == '__main__':
     InputUI = InputMessageUI()
     InputUI.run()
