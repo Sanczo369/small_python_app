5dea9c553f5180a3d76bd433eead221997d4654d
Arek Sanecki <areksanecki@gmail.com>
2024-01-28 18:50:44 +0100

    dodanie generate_broken_message()  do "Interleaving Simulator Python"

--- TkinterWeb/setup.py
+++ TkinterWeb/setup.py
@@ -19,6 +19,36 @@ def processing_text_input(text: str) -> tuple[list[str], list[str]]:
 def get_widget_size(widget: tk.Label | tk.Button) -> tuple[int, int]:
     return widget.winfo_reqwidth(), widget.winfo_reqheight()
 
+
+def generate_broken_message(array: list[str], indicator: list[bool], orient: str) -> tuple[list[str], list[bool]]:
+    """
+    array       : list of words/packets
+    indicator   : [True, False, ...]
+    orient      : 'row' | 'col'
+    """
+
+    orient = orient.lower()
+    n = len(array)
+
+    result = [None] * n
+
+    text_length = len(array[0])
+    for i in range(n):
+        if indicator[i]:
+            if orient == 'row':
+                result[i] = "#" * text_length
+            elif orient == 'col':
+                current_message = array[i].replace('\n', '')
+
+                result[i] = "#\n" * len(current_message)
+                result[i] = result[i][:-1]
+            else:
+                raise TypeError('orient parameter only take \'row\' or \'col\'')
+        else:
+            result[i] = array[i]
+
+    return result, indicator
+
 if __name__ == '__main__':
     InputUI = InputMessageUI()
     InputUI.run()
