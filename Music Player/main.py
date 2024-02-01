772bc17034853f20cbd371f8199a47d16d923d20
Arek Sanecki <areksanecki@gmail.com>
2024-02-01 23:37:47 +0100

    dodanie "Music-Player"

---
+++ Music Player/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Music Player Tkinter
+#
+
+import os,sys
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from pygame import mixer,error
+
+
+if __name__ == "__main__":
+    main()
