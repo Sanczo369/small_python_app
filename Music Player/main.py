3e8a25207a4e194d3bba5a92d17d319b741edc7a
Arek Sanecki <areksanecki@gmail.com>
2024-02-01 23:38:44 +0100

    dodanie main() do "Music-Player"

--- Music Player/main.py
+++ Music Player/main.py
@@ -7,6 +7,9 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 from pygame import mixer,error
 
+def main():
+    window = Music_Player()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
