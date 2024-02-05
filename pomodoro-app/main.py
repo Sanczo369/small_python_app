838e77cc723e82926c8c2f5cd5d2a290f0db8f45
Arek Sanecki <areksanecki@gmail.com>
2024-02-05 20:00:49 +0100

    Dodanie nowego projektu "Pomodoro-App"

---
+++ pomodoro-app/main.py
@@ -0,0 +1,16 @@
+import tkinter
+import math
+
+# ----- CONSTANTS ----- #
+
+PINK = "#e2979c"
+RED = "#C84B31"
+GREEN = "#dce0cd"
+GREEN_MSG = "#125C13"
+YELLOW = "#ECDBBA"
+FONT_NAME = "Lucida Console"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+repetition = 0
+timer = None
