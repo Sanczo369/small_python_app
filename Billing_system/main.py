42a8b6135aea879187bb3c843116a7a698bcddc6
Arek Sanecki <areksanecki@gmail.com>
2024-02-16 20:31:43 +0100

    dodanie Bill_App __init__ "Billing_system"

--- Billing_system/main.py
+++ Billing_system/main.py
@@ -5,7 +5,14 @@ import os
 from tkinter import messagebox
 
 # ============main============================
-
+class Bill_App:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1350x700+0+0")
+        self.root.title("Billing Software")
+        bg_color = "#badc57"
+        title = Label(self.root, text="Billing Software", font=('times new roman', 30, 'bold'), pady=2, bd=12, bg="white", fg="Black", relief=GROOVE)
+        title.pack(fill=X)
 
 root = Tk()
 
