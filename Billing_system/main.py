90a9123f4df90adfda6b0e0fddc120f324832c9a
Arek Sanecki <areksanecki@gmail.com>
2024-02-19 11:56:06 +0100

    dodanie Bill_App __init__(GroceryItems - food_oil) "Billing_system"

--- Billing_system/main.py
+++ Billing_system/main.py
@@ -132,6 +132,11 @@ class Bill_App:
         rice_txt = Entry(F3, width=10, textvariable=self.rice, font=('times new roman', 16, 'bold'), bd=5, relief=GROOVE)
         rice_txt.grid(row=0, column=1, padx=10, pady=10)
 
+        food_oil_lbl = Label(F3, text="Food Oil", font=('times new roman', 16, 'bold'), bg="#badc57", fg="black")
+        food_oil_lbl.grid(row=1, column=0, padx=10, pady=10, sticky='W')
+        food_oil_txt = Entry(F3, width=10, textvariable=self.food_oil, font=('times new roman', 16, 'bold'), bd=5, relief=GROOVE)
+        food_oil_txt.grid(row=1, column=1, padx=10, pady=10)
+
 root = Tk()
 
 root.mainloop()
