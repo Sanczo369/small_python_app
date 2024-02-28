97d3768cb15499b13529b25cd5628711a070ecd1
Arek Sanecki <areksanecki@gmail.com>
2024-02-28 23:01:19 +0100

    dodanie projektu "Window and Widgets" ttk label

--- widget/1_0_window_basic_widgets.py
+++ widget/1_0_window_basic_widgets.py
@@ -6,5 +6,9 @@ window = tk.Tk()
 window.title('Window and Widgets')
 window.geometry('800x500')
 
+# ttk label
+label = ttk.Label(master = window, text = 'This is a test')
+label.pack()
+
 # run
 window.mainloop()
