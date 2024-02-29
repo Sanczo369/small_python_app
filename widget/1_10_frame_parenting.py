db85aa2e2ea514e12137ca059b73b27335e94639
Arek Sanecki <areksanecki@gmail.com>
2024-02-29 23:29:02 +0100

    dodanie do "Frames and parenting" (frame)

--- widget/1_10_frame_parenting.py
+++ widget/1_10_frame_parenting.py
@@ -6,5 +6,11 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Frames and parenting')
 
+
+# frame
+frame = ttk.Frame(window, width = 200, height = 200, borderwidth = 10, relief = tk.GROOVE)
+frame.pack_propagate(False)
+frame.pack(side = 'left')
+
 # run
 window.mainloop()
