fd03dc99b01582c3d2952928c1f20e27a2e0a2c8
Arek Sanecki <areksanecki@gmail.com>
2024-03-01 15:04:53 +0100

    dodanie projektu "Tab Widget" (tab 1)

--- widget/1_11_tabs.py
+++ widget/1_11_tabs.py
@@ -5,7 +5,12 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title("Tab Widget")
 
-
+# tab 1
+tab1 = ttk.Frame(notebook)
+label1 = ttk.Label(tab1, text = 'Text in tab 1')
+label1.pack()
+button1 = ttk.Button(tab1, text = 'Button in tab 1')
+button1.pack()
 
 
 # run
