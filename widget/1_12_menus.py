8ef48105370f909955bce001585589e3de2559c1
Arek Sanecki <areksanecki@gmail.com>
2024-03-02 18:08:49 +0100

    dodanie do "Menu" (another sub menu)

--- widget/1_12_menus.py
+++ widget/1_12_menus.py
@@ -17,5 +17,14 @@ file_menu.add_separator()
 menu.add_cascade(label = 'File', menu = file_menu)
 
 
+# another sub menu
+help_menu = tk.Menu(menu, tearoff = False)
+help_menu.add_command(label = 'Help entry', command = lambda: print(help_check_string.get()))
+
+help_check_string = tk.StringVar()
+help_menu.add_checkbutton(label = 'check', onvalue = 'on', offvalue = 'off', variable = help_check_string)
+
+menu.add_cascade(label = 'Help', menu = help_menu)
+
 # run
 window.mainloop()
