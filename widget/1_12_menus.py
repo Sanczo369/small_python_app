635132d753e77a7f2a1bf1241ea03fc0ef639c42
Arek Sanecki <areksanecki@gmail.com>
2024-03-02 18:08:32 +0100

    dodanie do "Menu" (menu i sub menu)

--- widget/1_12_menus.py
+++ widget/1_12_menus.py
@@ -6,6 +6,16 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Menu')
 
+# menu
+menu = tk.Menu(window)
+
+# sub menu
+file_menu = tk.Menu(menu, tearoff = False)
+file_menu.add_command(label = 'New', command = lambda: print('New file'))
+file_menu.add_command(label = 'Open', command = lambda: print('Open file'))
+file_menu.add_separator()
+menu.add_cascade(label = 'File', menu = file_menu)
+
 
 # run
 window.mainloop()
