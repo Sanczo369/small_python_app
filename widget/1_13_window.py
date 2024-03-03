49100c2879d96cdb7b5aa1b2369bfd077d6d502c
Arek Sanecki <areksanecki@gmail.com>
2024-03-03 23:54:09 +0100

    dodanie projektu "More on the window" (exercise)

--- widget/1_13_window.py
+++ widget/1_13_window.py
@@ -7,6 +7,12 @@ window.title('More on the window')
 # window.geometry('600x400+100+200')
 window.iconbitmap('python.ico')
 
+# exercise:
+# start window in the middle of the screen
+window_width = 1400
+window_height = 600
+display_width = window.winfo_screenwidth()
+display_height = window.winfo_screenheight()
 
 
 # run
