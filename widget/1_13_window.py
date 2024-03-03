cbf9e43bdd6edda9554583e2787e0e46fb9aae8b
Arek Sanecki <areksanecki@gmail.com>
2024-03-03 23:53:54 +0100

    dodanie projektu "More on the window"

---
+++ widget/1_13_window.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = tk.Tk()
+window.title('More on the window')
+# window.geometry('600x400+100+200')
+window.iconbitmap('python.ico')
+
+
+
+# run
+window.mainloop()
