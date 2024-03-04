da2b48646246fe587ffba5384a8c56be86507ea1
Arek Sanecki <areksanecki@gmail.com>
2024-03-04 01:37:57 +0100

    dodanie do "Getting and setting widgets" (widgets)

--- widget/1_1_getting_widget_data.py
+++ widget/1_1_getting_widget_data.py
@@ -15,6 +15,16 @@ def button_func():
 window = tk.Tk()
 window.title('Getting and setting widgets')
 
+# widgets
+label = ttk.Label(master = window, text = 'Some text')
+label.pack()
+
+entry = ttk.Entry(master = window)
+entry.pack()
+
+button = ttk.Button(master = window, text = 'The button', command = button_func)
+button.pack()
+
 def reset_func():
 	label['text'] = 'some text'
 	entry['state'] = 'enabled'
