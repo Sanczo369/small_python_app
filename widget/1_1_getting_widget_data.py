271f1ae3dc9e768ea6907558283accaec0393139
Arek Sanecki <areksanecki@gmail.com>
2024-03-04 01:36:14 +0100

    dodanie do "Getting and setting widgets" button_func()

--- widget/1_1_getting_widget_data.py
+++ widget/1_1_getting_widget_data.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
 
+def button_func():
+	# get the content of the entry
+	entry_text = entry.get()
 
+	# update the label
+	# label.configure(text = 'some other text')
+	label['text'] = entry_text
+	entry['state'] = 'disabled'
+	# print(label.configure())
 
 # window
 window = tk.Tk()
