c69d3bb9d210f4e71e5a9c3ab15f27e4b964f8ba
Arek Sanecki <areksanecki@gmail.com>
2024-03-07 23:54:30 +0100

    dodanie projektu "buttons" (button)

--- widget/1_3_buttons.py
+++ widget/1_3_buttons.py
@@ -6,6 +6,15 @@ window = tk.Tk()
 window.title('buttons')
 window.geometry('600x400')
 
+# button
+def button_func():
+	print('a basic button')
+	print(radio_var.get())
+
+button_string = tk.StringVar(value = 'A button with string var')
+button = ttk.Button(window, text = 'A simple button', command = button_func, textvariable = button_string)
+button.pack()
+
 # data
 radio_string = tk.StringVar()
 check_bool = tk.BooleanVar()
