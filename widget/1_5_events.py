1f8fc49ffbaa1261f0d33d6cc065da474b2cd08c
Arek Sanecki <areksanecki@gmail.com>
2024-03-09 18:33:21 +0100

    dodanie projektu "Event Binding" (window)

--- widget/1_5_events.py
+++ widget/1_5_events.py
@@ -6,7 +6,10 @@ window = tk.Tk()
 window.geometry('600x500')
 window.title('Event Binding')
 
+# window.bind('<Motion>', get_pos)
 
+entry.bind('<FocusIn>', lambda event: print('entry field was selected'))
+entry.bind('<FocusOut>', lambda event: print('entry field was unselected'))
 
 # exercise :
 # print 'Mousewheel' when the user holds down shift and uses the mousewheel while text is selected
