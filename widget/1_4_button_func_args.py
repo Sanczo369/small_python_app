4af4dad3ba2a8ce2f5298febe08d61d4553ffbbb
Arek Sanecki <areksanecki@gmail.com>
2024-03-09 18:30:46 +0100

    dodanie do "buttons, functions and arguments" (outer_func)

--- widget/1_4_button_func_args.py
+++ widget/1_4_button_func_args.py
@@ -5,6 +5,12 @@ def button_func(entry_string):
 	print('a button was pressed')
 	print(entry_string.get())
 
+def outer_func(parameter):
+	def inner_func():
+		print('a button was pressed')
+		print(parameter.get())
+	return inner_func
+
 
 # setup
 window = tk.Tk()
