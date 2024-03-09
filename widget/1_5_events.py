2c8a5ae177e9366f000272cfba1f4355856f6fda
Arek Sanecki <areksanecki@gmail.com>
2024-03-09 18:33:01 +0100

    dodanie projektu "Event Binding" (exercise)

--- widget/1_5_events.py
+++ widget/1_5_events.py
@@ -6,5 +6,11 @@ window = tk.Tk()
 window.geometry('600x500')
 window.title('Event Binding')
 
+
+
+# exercise :
+# print 'Mousewheel' when the user holds down shift and uses the mousewheel while text is selected
+text.bind('<Shift-MouseWheel>', lambda event: print('Mousewheel'))
+
 # run
 window.mainloop()
