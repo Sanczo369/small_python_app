7f674aeb622a89b6b65fe8d60f81d226aa68c6c9
Arek Sanecki <areksanecki@gmail.com>
2024-03-11 23:52:42 +0100

    dodanie projektu "Combo and Spin" (Combobox)

--- widget/1_6_combo_spin.py
+++ widget/1_6_combo_spin.py
@@ -6,5 +6,13 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Combo and Spin')
 
+# Combobox
+items = ('Ice cream', 'Pizza', 'Broccoli')
+food_string = tk.StringVar(value = items[0])
+combo = ttk.Combobox(window, textvariable = food_string)
+combo['values'] = items
+# combo.configure(values = items)
+combo.pack()
+
 # run
 window.mainloop()
