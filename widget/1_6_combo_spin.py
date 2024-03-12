1b5c3462db3a7d839685bd27d319d0117150d512
Arek Sanecki <areksanecki@gmail.com>
2024-03-12 14:41:47 +0100

    dodanie projektu "Combo and Spin" (Spinbox)

--- widget/1_6_combo_spin.py
+++ widget/1_6_combo_spin.py
@@ -20,5 +20,20 @@ combo.bind('<<ComboboxSelected>>', lambda event: combo_label.config(text = f'Sel
 combo_label = ttk.Label(window, text = 'a label')
 combo_label.pack()
 
+
+# Spinbox
+spin_int = tk.IntVar(value = 12)
+spin = ttk.Spinbox(
+	window,
+	from_ = 3,
+	to = 20,
+	increment = 3,
+	command = lambda: print(spin_int.get()),
+	textvariable = spin_int)
+spin.bind('<<Increment>>', lambda event: print('up'))
+spin.bind('<<Decrement>>', lambda event: print('down'))
+# spin['value'] = (1,2,3,4,5)
+spin.pack()
+
 # run
 window.mainloop()
