f1a4996258f21563fd34e8beb12482068a624e28
Arek Sanecki <areksanecki@gmail.com>
2024-03-12 14:46:12 +0100

    dodanie projektu "Canvas" (canvas)

--- widget/1_7_canvas.py
+++ widget/1_7_canvas.py
@@ -10,6 +10,17 @@ window.title('Canvas')
 canvas = tk.Canvas(window, bg = 'white')
 canvas.pack()
 
+# canvas.create_rectangle((50, 20, 100, 200), fill = 'red', width = 10, dash = (4,2,1,1), outline = 'green')
+# canvas.create_oval((200, 0, 300, 100), fill = 'green')
+# canvas.create_arc(
+# 	(200, 0, 300, 100),
+# 	fill = 'red',
+# 	start = 45,
+# 	extent = 140,
+# 	style = tk.CHORD,
+# 	outline = 'red',
+# 	width = 1)
+
 # Exercise
 # use event binding to create a basic paint app
 def draw_on_canvas(event):
