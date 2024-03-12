79b724d49f9177b5bb332e41c085bab3a11cff3d
Arek Sanecki <areksanecki@gmail.com>
2024-03-12 14:45:08 +0100

    dodanie projektu "Canvas" (draw_on_canvas)

--- widget/1_7_canvas.py
+++ widget/1_7_canvas.py
@@ -6,5 +6,12 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Canvas')
 
+
+# Exercise
+# use event binding to create a basic paint app
+def draw_on_canvas(event):
+	x = event.x
+	y = event.y
+	canvas.create_oval((x - brush_size / 2,y - brush_size / 2, x + brush_size / 2,y + brush_size / 2), fill = 'black')
 # run
 window.mainloop()
