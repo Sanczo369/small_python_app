6c77c41dab7b18fc06cc54a91af2523d42fee3b1
Arek Sanecki <areksanecki@gmail.com>
2024-03-13 13:39:08 +0100

    dodanie projektu "Treeview"

---
+++ widget/1_8_treeview.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+from random import choice
+
+# window
+window = tk.Tk()
+window.geometry('600x400')
+window.title('Treeview')
+
+# run
+window.mainloop()
