cb83bdf2dff69e10277d09be49a2e265def0ba62
Arek Sanecki <areksanecki@gmail.com>
2024-03-13 13:39:26 +0100

    dodanie projektu "Treeview"(data)

--- widget/1_8_treeview.py
+++ widget/1_8_treeview.py
@@ -7,5 +7,9 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Treeview')
 
+# data
+first_names = ['Bob', 'Maria', 'Alex', 'James', 'Susan', 'Henry', 'Lisa', 'Anna', 'Lisa']
+last_names = ['Smith', 'Brown', 'Wilson', 'Thomson', 'Cook', 'Taylor', 'Walker', 'Clark']
+
 # run
 window.mainloop()
