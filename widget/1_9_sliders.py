6aee1c75231cf8180f2c0fec8e53125038fffbf8
Arek Sanecki <areksanecki@gmail.com>
2024-03-14 22:32:54 +0100

    dodanie projektu "Sliders"(slider)

--- widget/1_9_sliders.py
+++ widget/1_9_sliders.py
@@ -6,6 +6,18 @@ from tkinter import scrolledtext
 window = tk.Tk()
 window.title('Sliders')
 
+# slider
+scale_float = tk.DoubleVar(value = 15)
+scale = ttk.Scale(
+	window,
+	command = lambda value: progress.stop(),
+	from_ = 0,
+	to = 25,
+	length = 300,
+	orient = 'horizontal',
+	variable = scale_float)
+scale.pack()
+
 
 # run
 window.mainloop()
