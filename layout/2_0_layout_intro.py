caf6ccf5c95be4fec3e9c238990697a6afe12a85
Arek Sanecki <areksanecki@gmail.com>
2024-03-15 17:15:21 +0100

    dodanie projektu "Layout intro" (widgets)

--- layout/2_0_layout_intro.py
+++ layout/2_0_layout_intro.py
@@ -6,6 +6,9 @@ window = tk.Tk()
 window.title('Layout intro')
 window.geometry('600x400')
 
+# widgets
+label1 = ttk.Label(window, text = 'Label 1', background = 'red')
+label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
 
 # run
 window.mainloop()
