0ef7191b6bcae6e6786de9b6731f67669259e422
Arek Sanecki <areksanecki@gmail.com>
2024-03-16 17:59:48 +0100

    dodanie projektu "Layout intro" (pack)

--- layout/2_0_layout_intro.py
+++ layout/2_0_layout_intro.py
@@ -10,5 +10,9 @@ window.geometry('600x400')
 label1 = ttk.Label(window, text = 'Label 1', background = 'red')
 label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
 
+# pack
+# label1.pack(side = 'left', expand = True, fill = 'y')
+# label2.pack(side = 'left', expand = True, fill = 'both')
+
 # run
 window.mainloop()
