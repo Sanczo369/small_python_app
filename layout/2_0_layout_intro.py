08eef2d10c08ef206f96fdcc69ef198e97fde610
Arek Sanecki <areksanecki@gmail.com>
2024-03-16 18:13:48 +0100

    dodanie projektu "Layout intro" (grid)

--- layout/2_0_layout_intro.py
+++ layout/2_0_layout_intro.py
@@ -14,5 +14,12 @@ label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
 # label1.pack(side = 'left', expand = True, fill = 'y')
 # label2.pack(side = 'left', expand = True, fill = 'both')
 
+# grid
+# window.columnconfigure(0, weight = 1)
+# window.columnconfigure(1, weight = 1)
+# window.columnconfigure(2, weight = 2)
+# window.rowconfigure(0, weight = 1)
+# window.rowconfigure(1, weight = 1)
+
 # run
 window.mainloop()
