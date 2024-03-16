b154cd649fb143ce392fe109e6fc85e3a41907b7
Arek Sanecki <areksanecki@gmail.com>
2024-03-16 18:16:21 +0100

    dodanie do "Widgets and return" (create_segment)

--- layout/2_10_return.py
+++ layout/2_10_return.py
@@ -1,6 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def create_segment(parent, label_text, button_text):
+	frame = ttk.Frame(master = parent)
+
+	# grid layout
+	frame.rowconfigure(0, weight = 1)
+	frame.columnconfigure((0,1,2), weight = 1, uniform = 'a')
+
+	# widgets
+	ttk.Label(frame, text = label_text).grid(row = 0, column = 0, sticky = 'nsew')
+	ttk.Button(frame, text = button_text).grid(row = 0, column = 1, sticky = 'nsew')
+
+	return frame
+
 # window
 window = tk.Tk()
 window.title('Widgets and return')
