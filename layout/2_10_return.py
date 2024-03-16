eb9c39417e14cc4e64cd0c0c1ee7ad646dd94f66
Arek Sanecki <areksanecki@gmail.com>
2024-03-16 18:16:49 +0100

    dodanie do "Widgets and return" (Segment)

--- layout/2_10_return.py
+++ layout/2_10_return.py
@@ -15,6 +15,22 @@ def create_segment(parent, label_text, button_text):
 
 	return frame
 
+
+class Segment(ttk.Frame):
+    def __init__(self, parent, label_text, button_text, exercise_text):
+        super().__init__(master=parent)
+
+        # grid layout
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure((0, 1, 2), weight=1, uniform='a')
+
+        # widgets
+        ttk.Label(self, text=label_text).grid(row=0, column=0, sticky='nsew')
+        ttk.Button(self, text=button_text).grid(row=0, column=1, sticky='nsew')
+        self.create_exercise_box(exercise_text).grid(row=0, column=2, sticky='nsew')
+
+        self.pack(expand=True, fill='both', padx=10, pady=10)
+
 # window
 window = tk.Tk()
 window.title('Widgets and return')
