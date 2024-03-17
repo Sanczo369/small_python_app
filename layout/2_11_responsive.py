59e3c7e5a69fdf66d7a578e23377fdbac57ae354
Arek Sanecki <areksanecki@gmail.com>
2024-03-17 19:33:26 +0100

    dodanie projektu "Responsive"

---
+++ layout/2_11_responsive.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+
+# exercise
+# create a a third layout where the widgets are next to each other (I used grid)
+# make it appear once the window is wider than 1200 pixels
+
+
+app = App((400,300))
