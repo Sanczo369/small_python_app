1166e3e9d7cd2ab0405498fc6c5e97d32c2216b9
Arek Sanecki <areksanecki@gmail.com>
2024-03-17 19:59:31 +0100

    dodanie do "Responsive"(create_medium_layout)

--- layout/2_11_responsive.py
+++ layout/2_11_responsive.py
@@ -21,14 +21,28 @@ class App(tk.Tk):
 		self.mainloop()
 
 
-def create_small_layout(self):
-    self.frame.pack_forget()
-    self.frame = ttk.Frame(self)
-    ttk.Label(self.frame, text='Label 1', background='red').pack(expand=True, fill='both', padx=10, pady=5)
-    ttk.Label(self.frame, text='Label 2', background='green').pack(expand=True, fill='both', padx=10, pady=5)
-    ttk.Label(self.frame, text='Label 3', background='blue').pack(expand=True, fill='both', padx=10, pady=5)
-    ttk.Label(self.frame, text='Label 4', background='yellow').pack(expand=True, fill='both', padx=10, pady=5)
-    self.frame.pack(expand=True, fill='both')
+    def create_small_layout(self):
+        self.frame.pack_forget()
+        self.frame = ttk.Frame(self)
+        ttk.Label(self.frame, text='Label 1', background='red').pack(expand=True, fill='both', padx=10, pady=5)
+        ttk.Label(self.frame, text='Label 2', background='green').pack(expand=True, fill='both', padx=10, pady=5)
+        ttk.Label(self.frame, text='Label 3', background='blue').pack(expand=True, fill='both', padx=10, pady=5)
+        ttk.Label(self.frame, text='Label 4', background='yellow').pack(expand=True, fill='both', padx=10, pady=5)
+        self.frame.pack(expand=True, fill='both')
+
+    def create_medium_layout(self):
+        self.frame.pack_forget()
+        self.frame = ttk.Frame(self)
+        self.frame.columnconfigure((0, 1), weight=1, uniform='a')
+        self.frame.rowconfigure((0, 1), weight=1, uniform='a')
+        self.frame.pack(expand=True, fill='both')
+
+        ttk.Label(self.frame, text='Label 1', background='red').grid(column=0, row=0, sticky='nsew', padx=10, pady=10)
+        ttk.Label(self.frame, text='Label 2', background='green').grid(column=1, row=0, sticky='nsew', padx=10, pady=10)
+        ttk.Label(self.frame, text='Label 3', background='blue').grid(column=0, row=1, sticky='nsew', padx=10, pady=10)
+        ttk.Label(self.frame, text='Label 4', background='yellow').grid(column=1, row=1, sticky='nsew', padx=10,
+                                                                        pady=10)
+
 
 # exercise
 # create a a third layout where the widgets are next to each other (I used grid)
