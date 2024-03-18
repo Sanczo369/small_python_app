cfb344180686b95db32520645a0cddca35d674ea
Arek Sanecki <areksanecki@gmail.com>
2024-03-18 21:18:04 +0100

    dodanie projektu "Scrolling"

---
+++ layout/2_12_scroll_basics.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+from random import randint, choice
+
+# setup
+window = tk.Tk()
+window.geometry('600x400')
+window.title('Scrolling')
+
+# run window
+window.mainloop()
