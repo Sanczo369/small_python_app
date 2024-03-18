1ecf156a067c9273f5cf39b96f21d6b3722924d1
Arek Sanecki <areksanecki@gmail.com>
2024-03-18 21:19:05 +0100

    dodanie do "Scrolling" (canvas)

--- layout/2_12_scroll_basics.py
+++ layout/2_12_scroll_basics.py
@@ -7,5 +7,18 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Scrolling')
 
+
+# canvas
+# canvas = tk.Canvas(window, bg = 'white', scrollregion = (0,0,2000,5000))
+# canvas.create_line(0,0,2000,5000, fill = 'green', width = 10)
+# for i in range(100):
+# 	l = randint(0,2000)
+# 	t = randint(0,5000)
+# 	r = l + randint(10,500)
+# 	b = t + randint(10,500)
+# 	color = choice(('red', 'green', 'blue', 'yellow', 'orange'))
+# 	canvas.create_rectangle(l,t,r,b, fill = color)
+# canvas.pack(expand = True, fill = 'both')
+
 # run window
 window.mainloop()
