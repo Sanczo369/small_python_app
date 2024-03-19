9a4b5a56be97838f5a0c36a174a08c2ad330b2cd
Arek Sanecki <areksanecki@gmail.com>
2024-03-19 14:22:37 +0100

    dodanie do "Scrolling" (scroll left)

--- layout/2_12_scroll_basics.py
+++ layout/2_12_scroll_basics.py
@@ -34,5 +34,8 @@ window.title('Scrolling')
 # canvas.configure(xscrollcommand = scrollbar_bottom.set)
 # scrollbar_bottom.place(relx = 0, rely = 1, relwidth = 1, anchor = 'sw')
 
+# # also add an event to scroll left / right on Ctrl + mousewheel
+# canvas.bind('<Control MouseWheel>', lambda event: canvas.xview_scroll(-int(event.delta / 60), "units"))
+
 # run window
 window.mainloop()
