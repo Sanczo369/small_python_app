07c5fb13f477265090cc0fb280362a631808ecc0
Arek Sanecki <areksanecki@gmail.com>
2024-03-22 18:55:42 +0100

    dodanie do "Multiple windows" (Extra)

--- layout/2_14_multi_window.py
+++ layout/2_14_multi_window.py
@@ -2,6 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+class Extra(tk.Toplevel):
+	def __init__(self):
+		super().__init__()
+		self.title('extra window')
+		self.geometry('300x400')
+		ttk.Label(self, text = 'A label').pack()
+		ttk.Button(self, text = 'A button').pack()
+		ttk.Label(self, text = 'another label').pack(expand = True)
+
 # window
 window = tk.Tk()
 window.geometry('600x400')
