3fc2e2964c19cf3f412644c0b4cb714c4285872e
Arek Sanecki <areksanecki@gmail.com>
2024-03-22 18:55:20 +0100

    dodanie projektu "Multiple windows"

---
+++ layout/2_14_multi_window.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+# window
+window = tk.Tk()
+window.geometry('600x400')
+window.title('Multiple windows')
+
+
+# run
+window.mainloop()
