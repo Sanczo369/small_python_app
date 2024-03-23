33073922b47ef2400ba60a5ecbfae9f98a7f7aba
Arek Sanecki <areksanecki@gmail.com>
2024-03-23 18:38:28 +0100

    dodanie do "Pack" (widgets)

--- layout/2_1_pack.py
+++ layout/2_1_pack.py
@@ -6,5 +6,12 @@ window = tk.Tk()
 window.title('Pack')
 window.geometry('400x600')
 
+
+# widgets
+label1 = ttk.Label(window, text = 'First label', background = 'red')
+label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
+label3 = ttk.Label(window, text = 'Last of the labels', background = 'green')
+button = ttk.Button(window, text = 'Button')
+
 # run
 window.mainloop()
