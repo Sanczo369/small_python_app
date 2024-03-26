ff945218b717a12ca8819b01ce27846ffaf785aa
Arek Sanecki <areksanecki@gmail.com>
2024-03-26 13:51:27 +0100

    dodanie do "Pack parenting" (Top frame)

--- layout/2_2_pack_parenting.py
+++ layout/2_2_pack_parenting.py
@@ -6,5 +6,9 @@ window = tk.Tk()
 window.title('Pack parenting')
 window.geometry('400x600')
 
+# Top frame
+top_frame = ttk.Frame(window)
+label1 = ttk.Label(top_frame, text = 'First label', background = 'red')
+label2 = ttk.Label(top_frame, text = 'Label 2', background = 'blue')
 # run
 window.mainloop()
