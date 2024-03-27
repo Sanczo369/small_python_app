e9bb214de854c151393098298afe694a34b20097
Arek Sanecki <areksanecki@gmail.com>
2024-03-27 02:19:44 +0100

    dodanie do "Grid" (widgets)

--- layout/2_3_grid.py
+++ layout/2_3_grid.py
@@ -6,5 +6,14 @@ window = tk.Tk()
 window.title('Grid')
 window.geometry('600x400')
 
+# widgets
+label1 = ttk.Label(window, text = 'Label 1', background = 'red')
+label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
+label3 = ttk.Label(window, text = 'Label 3', background = 'green')
+label4 = ttk.Label(window, text = 'Label 4', background = 'yellow')
+button1 = ttk.Button(window, text = 'Button 1')
+button2 = ttk.Button(window, text = 'Button 2')
+entry = ttk.Entry(window)
+
 # run
 window.mainloop()
