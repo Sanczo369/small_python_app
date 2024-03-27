a47081c709c94293b5b5ab0f15cdb0ebf2e12e4f
Arek Sanecki <areksanecki@gmail.com>
2024-03-27 02:20:41 +0100

    dodanie do "Grid" (define a grid)

--- layout/2_3_grid.py
+++ layout/2_3_grid.py
@@ -15,5 +15,12 @@ button1 = ttk.Button(window, text = 'Button 1')
 button2 = ttk.Button(window, text = 'Button 2')
 entry = ttk.Entry(window)
 
+# define a grid
+window.columnconfigure((0,1,2), weight = 1, uniform = 'a')
+window.columnconfigure(3, weight = 2, uniform = 'a')
+window.rowconfigure(0, weight = 1, uniform = 'a')
+window.rowconfigure(1, weight = 1, uniform = 'a')
+window.rowconfigure(2, weight = 1, uniform = 'a')
+window.rowconfigure(3, weight = 3, uniform = 'a')
 # run
 window.mainloop()
