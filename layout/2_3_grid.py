e8aa2ee7b59c8218c875e8b012b08b6aaad53830
Arek Sanecki <areksanecki@gmail.com>
2024-03-28 20:25:24 +0100

    dodanie do "Grid" (exercise)

--- layout/2_3_grid.py
+++ layout/2_3_grid.py
@@ -28,5 +28,12 @@ label1.grid(row = 0, column = 0, sticky = 'nsew')
 label2.grid(row = 1, column = 1, rowspan = 3, sticky = 'nsew')
 label3.grid(row = 1, column = 0, columnspan = 3, sticky = 'nsew', padx = 20, pady = 10)
 label4.grid(row = 3, column = 3, sticky = 'se')
+
+# exercise
+# add the buttons and the entry field
+button1.grid(row = 0, column = 3, sticky = 'nesw')
+button2.grid(row = 2, column = 2, sticky = 'nesw')
+entry.grid(row = 2, column = 3, rowspan = 2, sticky = 'ew')
+
 # run
 window.mainloop()
