bc561aefbab798743cb15ad38e44eba41beebc85
Sanczo369 <areksanecki@gmail.com>
2024-03-29 18:45:00 +0100

    dodanie do "Place"(widgets)

--- layout/2_4_place.py
+++ layout/2_4_place.py
@@ -5,5 +5,12 @@ from tkinter import ttk
 window = tk.Tk()
 window.title('Place')
 window.geometry('400x600')
+
+# widgets
+label1 = ttk.Label(window, text = 'Label 1', background = 'red')
+label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
+label3 = ttk.Label(window, text = 'Label 3', background = 'green')
+button = ttk.Button(window, text = 'Button')
+
 # run
 window.mainloop()
