7bc3803a099f1051c886b1935160c5e984259e7e
Sanczo369 <areksanecki@gmail.com>
2024-03-29 18:49:58 +0100

    dodanie do "Place"(layout)

--- layout/2_4_place.py
+++ layout/2_4_place.py
@@ -12,5 +12,10 @@ label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
 label3 = ttk.Label(window, text = 'Label 3', background = 'green')
 button = ttk.Button(window, text = 'Button')
 
+# layout
+label1.place(x = 300, y = 100, width = 100, height = 200)
+label2.place(relx = 0.2, rely = 0.1, relwidth = 0.4, relheight = 0.5)
+label3.place(x = 80, y = 60, width = 160, height = 300)
+button.place(relx = 1, rely = 1, anchor = 'se')
 # run
 window.mainloop()
