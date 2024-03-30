90c63e275d0161f241064037c1813fdb363aae6a
Sanczo369 <areksanecki@gmail.com>
2024-03-30 22:04:16 +0100

    dodanie do widget sizes(layout)

--- layout/2_5_widget_sizes.py
+++ layout/2_5_widget_sizes.py
@@ -8,5 +8,9 @@ window.geometry('400x300')
 # widgets
 label1 = ttk.Label(window, text = 'Label 1', background = 'green')
 label2 = ttk.Label(window, text = 'Label 2', background = 'red', width = 50)
+
+# layout
+# label1.pack()
+# label2.pack()
 # run
 window.mainloop()
