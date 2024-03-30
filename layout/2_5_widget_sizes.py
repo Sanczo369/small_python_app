e2b893e9781e79dd900c284cf275d91e213e1127
Sanczo369 <areksanecki@gmail.com>
2024-03-30 22:05:07 +0100

    dodanie do widget sizes(grid)

--- layout/2_5_widget_sizes.py
+++ layout/2_5_widget_sizes.py
@@ -16,5 +16,10 @@ label2 = ttk.Label(window, text = 'Label 2', background = 'red', width = 50)
 # grid
 window.columnconfigure((0,1), weight = 1, uniform = 'a')
 window.rowconfigure((0,1), weight = 1, uniform = 'a')
+
+
+label1.grid(row = 0, column = 0)
+label2.grid(row = 1, column = 0, sticky = 'nsew')
+
 # run
 window.mainloop()
