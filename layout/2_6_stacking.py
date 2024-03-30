e8b1382452d452300a5eb64e70dc5096114c2dc8
Sanczo369 <areksanecki@gmail.com>
2024-03-30 22:09:11 +0100

    dodanie do Stacking order(label)

--- layout/2_6_stacking.py
+++ layout/2_6_stacking.py
@@ -10,5 +10,8 @@ window.geometry('400x400')
 label1 = ttk.Label(window, text = 'Label 1', background = 'green')
 label2 = ttk.Label(window, text = 'Label 2', background = 'red')
 label3 = ttk.Label(window, text = 'Label 3', background = 'blue')
+
+# label1.lift()
+# label2.lower()
 # run
 window.mainloop()
