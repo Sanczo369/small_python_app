9fa888dbfe8661f507c0630bc1302e545ba16856
Sanczo369 <areksanecki@gmail.com>
2024-03-31 17:16:37 +0200

    dodanie do Hide widgets(place)

--- layout/2_7_toggle_widgets.py
+++ layout/2_7_toggle_widgets.py
@@ -6,5 +6,16 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Hide widgets')
 
+# place
+# def toggle_label_place():
+# 	global label_visible
+
+# 	if label_visible:
+# 		label.place_forget()
+# 		label_visible = False
+# 	else:
+# 		label_visible = True
+# 		label.place(relx = 0.5, rely = 0.5, anchor = 'center')
+
 # run
 window.mainloop()
