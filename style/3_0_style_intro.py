7d691dd1fea533f19fd3dbce21b6d1b2c3b35787
Arek Sanecki <areksanecki@gmail.com>
2024-04-02 22:27:51 +0200

    dodanie do style projektu "classic"(style)

--- style/3_0_style_intro.py
+++ style/3_0_style_intro.py
@@ -6,5 +6,11 @@ window = tk.Tk()
 window.title('Styling')
 window.geometry('400x300')
 
+style.configure('new.TButton', foreground = 'green', font = ('Jokerman', 20))
+style.map('new.TButton',
+	foreground = [('pressed', 'red'),('disabled', 'yellow')],
+	background = [('pressed', 'green'), ('active', 'blue')])
+style.configure('TFrame', background = 'pink')
+
 # run
 window.mainloop()
