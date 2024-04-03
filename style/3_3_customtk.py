ed32b60d2aee3e10f75b5c7935be473b6e889382
Arek Sanecki <areksanecki@gmail.com>
2024-04-03 19:07:20 +0200

    dodanie do style projektu "customtkinter app"

---
+++ style/3_3_customtk.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = ctk.CTk()
+window.title('customtkinter app')
+window.geometry('600x400')
+
+# run
+window.mainloop()
