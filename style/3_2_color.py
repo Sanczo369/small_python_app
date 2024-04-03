bf8564f392d9013e909626beef9ba6b4261bb0c0
Arek Sanecki <areksanecki@gmail.com>
2024-04-03 19:05:05 +0200

    dodanie do style projektu "Colors"(widgets)

--- style/3_2_color.py
+++ style/3_2_color.py
@@ -6,5 +6,9 @@ window = tk.Tk()
 window.title('Colors')
 window.geometry('400x300')
 
+# widgets
+ttk.Label(window, background = 'red').pack(expand = True, fill = 'both')
+ttk.Label(window, background = '#08F').pack(expand = True, fill = 'both')
+ttk.Label(window, background = '#4fc296').pack(expand = True, fill = 'both')
 # run
 window.mainloop()
