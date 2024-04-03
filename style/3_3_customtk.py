c2ec8efeb85a39d4c4ffce80ad0a5a0aa43f646b
Arek Sanecki <areksanecki@gmail.com>
2024-04-03 19:07:51 +0200

    dodanie do style projektu "customtkinter app"(widgets)

--- style/3_3_customtk.py
+++ style/3_3_customtk.py
@@ -7,5 +7,16 @@ window = ctk.CTk()
 window.title('customtkinter app')
 window.geometry('600x400')
 
+# widgets
+string_var = ctk.StringVar(value = 'a custom string')
+label = ctk.CTkLabel(
+	window,
+	text = 'A ctk label',
+	fg_color = ('blue','red'),
+	text_color = ('black','white'),
+	corner_radius = 10,
+	textvariable = string_var)
+label.pack()
+
 # run
 window.mainloop()
