807c5191367452c8ff5e7a6753e0784051ca945e
Arek Sanecki <areksanecki@gmail.com>
2024-04-07 23:59:51 +0200

    dodanie do style projektu "customtkinter"(Menu)

--- style/3_4_customtk_app.py
+++ style/3_4_customtk_app.py
@@ -19,3 +19,10 @@ class App(ctk.CTk):
 
         # run
         self.mainloop()
+
+class Menu(ctk.CTkFrame):
+	def __init__(self, parent):
+		super().__init__(parent)
+		self.place(x = 0, y = 0, relwidth = 0.3, relheight = 1)
+
+		self.create_widgets()
