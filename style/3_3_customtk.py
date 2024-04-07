53309a2c58c5dd9fb3e0579372eb3e7747da230a
Arek Sanecki <areksanecki@gmail.com>
2024-04-07 23:58:09 +0200

    dodanie do style projektu "customtkinter app"(exercise)

--- style/3_3_customtk.py
+++ style/3_3_customtk.py
@@ -32,5 +32,19 @@ frame.pack()
 
 slider = ctk.CTkSlider(frame)
 slider.pack(padx = 20, pady = 20)
+
+# exercise
+switch = ctk.CTkSwitch(
+	window,
+	text = 'Exercise Switch',
+	fg_color = ('blue','red'),
+	progress_color = 'pink',
+	button_color = 'green',
+	button_hover_color = 'yellow',
+	switch_width = 60,
+	switch_height = 30,
+	corner_radius = 2)
+switch.pack()
+
 # run
 window.mainloop()
