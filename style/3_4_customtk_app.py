6ba95cd428015e05a01b98defaf90595c67626b1
Arek Sanecki <areksanecki@gmail.com>
2024-04-07 23:59:32 +0200

    dodanie do style projektu "customtkinter"(App)

--- style/3_4_customtk_app.py
+++ style/3_4_customtk_app.py
@@ -4,3 +4,18 @@ import customtkinter as ctk
 
 # exercise:
 # convert the app to use ctk
+
+class App(ctk.CTk):
+    def __init__(self, title, size):
+        # main setup
+        super().__init__()
+        self.title(title)
+        self.geometry(f'{size[0]}x{size[1]}')
+        self.minsize(size[0], size[1])
+
+        # widgets
+        self.menu = Menu(self)
+        self.main = Main(self)
+
+        # run
+        self.mainloop()
