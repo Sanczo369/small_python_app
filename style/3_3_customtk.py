1af2cfd6a47f33ecb59d464887311c9b2ce44637
Arek Sanecki <areksanecki@gmail.com>
2024-04-07 23:57:22 +0200

    dodanie do style projektu "customtkinter app"(button)

--- style/3_3_customtk.py
+++ style/3_3_customtk.py
@@ -18,5 +18,13 @@ label = ctk.CTkLabel(
 	textvariable = string_var)
 label.pack()
 
+button = ctk.CTkButton(
+	window,
+	text = 'A ctk button',
+	fg_color = '#FF0',
+	text_color = '#000',
+	hover_color = '#AA0',
+	command = lambda: ctk.set_appearance_mode('light'))
+button.pack()
 # run
 window.mainloop()
