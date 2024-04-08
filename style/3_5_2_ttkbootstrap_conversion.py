04851c3fe2a23be7610c6a0cc5efa5b1ad273a57
Arek Sanecki <areksanecki@gmail.com>
2024-04-09 00:42:31 +0200

    dodanie do projektu "ttkbootstrap_conversion" klasy App

--- style/3_5_2_ttkbootstrap_conversion.py
+++ style/3_5_2_ttkbootstrap_conversion.py
@@ -9,3 +9,10 @@ class App(ttk.Window):
         self.title(title)
         self.geometry(f'{size[0]}x{size[1]}')
         self.minsize(size[0], size[1])
+
+        # widgets
+        self.menu = Menu(self)
+        self.main = Main(self)
+
+        # run
+        self.mainloop()
