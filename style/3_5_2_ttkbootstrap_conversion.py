7a1823e402457286a238be142ff32807513784b6
Arek Sanecki <areksanecki@gmail.com>
2024-04-09 00:37:44 +0200

    dodanie do projektu "ttkbootstrap_conversion" klasy App

--- style/3_5_2_ttkbootstrap_conversion.py
+++ style/3_5_2_ttkbootstrap_conversion.py
@@ -1,3 +1,11 @@
 import tkinter as tk
 # from tkinter import ttk
 import ttkbootstrap as ttk
+
+class App(ttk.Window):
+    def __init__(self, title, size):
+        # main setup
+        super().__init__(themename='custom')
+        self.title(title)
+        self.geometry(f'{size[0]}x{size[1]}')
+        self.minsize(size[0], size[1])
