fbbc9eeff5a095ad60c03b0d433a9b4b9307a43e
Arek Sanecki <areksanecki@gmail.com>
2024-04-11 22:55:01 +0200

    konfiguracja w projekcie "ttkbootstrap_conversion"

--- style/3_5_2_ttkbootstrap_conversion.py
+++ style/3_5_2_ttkbootstrap_conversion.py
@@ -2,6 +2,7 @@ import tkinter as tk
 # from tkinter import ttk
 import ttkbootstrap as ttk
 
+
 class App(ttk.Window):
     def __init__(self, title, size):
         # main setup
@@ -17,12 +18,13 @@ class App(ttk.Window):
         # run
         self.mainloop()
 
-class Menu(ttk.Frame):
-	def __init__(self, parent):
-		super().__init__(parent)
-		self.place(x = 0, y = 0, relwidth = 0.3, relheight = 1)
 
-		self.create_widgets()
+class Menu(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(x=0, y=0, relwidth=0.3, relheight=1)
+
+        self.create_widgets()
 
     def create_widgets(self):
         # create the widgets
@@ -61,22 +63,24 @@ class Menu(ttk.Frame):
 
 
 class Main(ttk.Frame):
-	def __init__(self, parent):
-		super().__init__(parent)
-		self.place(relx = 0.3, y = 0, relwidth = 0.7, relheight = 1)
-		Entry(self, 'Entry 1','Button 1','green')
-		Entry(self, 'Entry 2','Button 2','blue')
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(relx=0.3, y=0, relwidth=0.7, relheight=1)
+        Entry(self, 'Entry 1', 'Button 1', 'green')
+        Entry(self, 'Entry 2', 'Button 2', 'blue')
+
 
 class Entry(ttk.Frame):
-	def __init__(self, parent, label_text, button_text, label_background):
-		super().__init__(parent)
+    def __init__(self, parent, label_text, button_text, label_background):
+        super().__init__(parent)
 
-		label = ttk.Label(self, text = label_text, background = label_background)
-		button = ttk.Button(self, text = button_text)
+        label = ttk.Label(self, text=label_text, background=label_background)
+        button = ttk.Button(self, text=button_text)
 
-		label.pack(expand = True, fill = 'both')
-		button.pack(expand = True, fill = 'both', pady = 10)
+        label.pack(expand=True, fill='both')
+        button.pack(expand=True, fill='both', pady=10)
 
-		self.pack(side = 'left', expand = True, fill = 'both', padx = 20, pady = 20)
+        self.pack(side='left', expand=True, fill='both', padx=20, pady=20)
 
-App('Class based app', (600,600))
+
+App('Class based app', (600, 600))
