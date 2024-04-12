8d6a7fc88b9cda3d697bd05e7dc784eee363cbdc
Arek Sanecki <areksanecki@gmail.com>
2024-04-12 16:46:30 +0200

    dodanie projektu "ttk bootstrap intro"(button2)

--- style/3_5_ttkbootstrap_intro.py
+++ style/3_5_ttkbootstrap_intro.py
@@ -12,5 +12,8 @@ label.pack(pady = 10)
 
 button1 = ttk.Button(window, text = 'Red', bootstyle = 'danger-outline')
 button1.pack(pady = 10)
+
+button2 = ttk.Button(window, text = 'Warning', bootstyle = 'warning')
+button2.pack(pady = 10)
 # run
 window.mainloop()
