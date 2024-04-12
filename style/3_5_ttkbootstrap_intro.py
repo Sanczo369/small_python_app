22481e5b254dc547b9f07b1c6af7e52fd26623a0
Arek Sanecki <areksanecki@gmail.com>
2024-04-12 16:45:48 +0200

    dodanie projektu "ttk bootstrap intro"(window)

--- style/3_5_ttkbootstrap_intro.py
+++ style/3_5_ttkbootstrap_intro.py
@@ -2,6 +2,10 @@ import tkinter as tk
 # from tkinter import ttk
 import ttkbootstrap as ttk
 
+# window
+window = ttk.Window(themename = 'journal')
+window.title('ttk bootstrap intro')
+window.geometry('400x300')
 
 # run
 window.mainloop()
