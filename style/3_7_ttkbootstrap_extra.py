cf1df6ed09e65d41c1d07afaabb757c1b38cfc91
Arek Sanecki <areksanecki@gmail.com>
2024-04-13 17:58:39 +0200

    dodanie projektu "textra widgets"

---
+++ style/3_7_ttkbootstrap_extra.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.scrolled import ScrolledFrame
+from ttkbootstrap.toast import ToastNotification
+from ttkbootstrap.tooltip import ToolTip
+from ttkbootstrap.widgets import DateEntry, Floodgauge, Meter
+
+# window
+window = ttk.Window(themename = 'darkly')
+window.title('extra widgets')
+
+# run
+window.mainloop()
