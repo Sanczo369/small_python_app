f4d478ee02c03bc1308e8031f52695865e45cb1b
Arek Sanecki <areksanecki@gmail.com>
2024-04-13 17:59:54 +0200

    dodanie projektu "textra widgets"(toast)

--- style/3_7_ttkbootstrap_extra.py
+++ style/3_7_ttkbootstrap_extra.py
@@ -19,5 +19,15 @@ for i in range(100):
 	ttk.Button(frame, text = f'Button :{i}').pack(fill = 'x', side = 'left')
 	frame.pack(fill = 'x', expand = True)
 
+# toast
+toast = ToastNotification(
+	title = 'This is a message title',
+	message = 'This is the actual message',
+	duration = 2000,
+	bootstyle = 'dark',
+	position = (50, 100, 'ne'))
+
+ttk.Button(window, text = 'show toast', command = toast.show_toast).pack(pady = 10)
+
 # run
 window.mainloop()
