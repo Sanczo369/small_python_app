17212ad61b2f24fd61c2951e5dfee88d6f772c42
Arek Sanecki <areksanecki@gmail.com>
2024-04-15 23:25:17 +0200

    dodanie do projektu 'Combined layout'(main place layout)

--- layout/2_8_combined.py
+++ layout/2_8_combined.py
@@ -12,5 +12,8 @@ window.minsize(600,600)
 menu_frame = ttk.Frame(window)
 main_frame = ttk.Frame(window)
 
+# main place layout
+menu_frame.place(x = 0, y = 0, relwidth = 0.3, relheight = 1)
+main_frame.place(relx = 0.3, y = 0, relwidth = 0.7, relheight = 1)
 # run
 window.mainloop()
