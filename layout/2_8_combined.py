0e3ed6cc53128816171b1b9d3db067c34fdc18c5
Arek Sanecki <areksanecki@gmail.com>
2024-04-15 23:24:29 +0200

    dodanie  projektu 'Combined layout'

---
+++ layout/2_8_combined.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+# from tkinter import ttk
+
+# window
+window = ttk.Window(themename = 'lumen')
+window.title('Combined layout')
+window.geometry('600x600')
+window.minsize(600,600)
+
+# run
+window.mainloop()
