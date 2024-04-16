c6577b334292f7f17f565ba15b193c52bb69d32e
Arek Sanecki <areksanecki@gmail.com>
2024-04-16 22:32:30 +0200

    dodanie do projektu 'Combined layout'(menu widgets - menu_slider1)

--- layout/2_8_combined.py
+++ layout/2_8_combined.py
@@ -20,5 +20,8 @@ main_frame.place(relx = 0.3, y = 0, relwidth = 0.7, relheight = 1)
 menu_button1 = ttk.Button(menu_frame, text = 'Button 1')
 menu_button2 = ttk.Button(menu_frame, text = 'Button 2')
 menu_button3 = ttk.Button(menu_frame, text = 'Button 3')
+
+menu_slider1 = ttk.Scale(menu_frame, orient = 'vertical')
+menu_slider2 = ttk.Scale(menu_frame, orient = 'vertical')
 # run
 window.mainloop()
