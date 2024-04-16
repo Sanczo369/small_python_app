ea0ce91bf28f73d91680aa82d02c84ea799f72c5
Arek Sanecki <areksanecki@gmail.com>
2024-04-16 22:31:56 +0200

    dodanie do projektu 'Combined layout'(menu widgets)

--- layout/2_8_combined.py
+++ layout/2_8_combined.py
@@ -15,5 +15,10 @@ main_frame = ttk.Frame(window)
 # main place layout
 menu_frame.place(x = 0, y = 0, relwidth = 0.3, relheight = 1)
 main_frame.place(relx = 0.3, y = 0, relwidth = 0.7, relheight = 1)
+
+# menu widgets
+menu_button1 = ttk.Button(menu_frame, text = 'Button 1')
+menu_button2 = ttk.Button(menu_frame, text = 'Button 2')
+menu_button3 = ttk.Button(menu_frame, text = 'Button 3')
 # run
 window.mainloop()
