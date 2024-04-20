1d90474b9af9ce3e9f75208e94c0970d13187ad0
Arek Sanecki <areksanecki@gmail.com>
2024-04-20 18:00:31 +0200

    dodanie projektu 'Animations'(AnimatedButton)

--- style/3_10_animations/3_10_animations.py
+++ style/3_10_animations/3_10_animations.py
@@ -5,6 +5,15 @@ from os import walk
 # exercise:
 # create an animation that runs forever
 
+class AnimatedButton(ctk.CTkButton):
+    def __init__(self, parent, light_path, dark_path):
+        # animation logic setup
+        self.frames = self.import_folders(light_path, dark_path)
+        self.frame_index = 0
+        self.animation_length = len(self.frames) - 1
+        self.animation_status = ctk.StringVar(value='start')
+
+        self.animation_status.trace('w', self.animate)
 
 
 # window
