2bc30ba3ca4d2f4273fda6195c466ae7f7f2daed
Arek Sanecki <areksanecki@gmail.com>
2024-04-22 23:48:36 +0200

    dodanie do projektu 'Images'(stretch_image)

--- style/3_9_images/images.py
+++ style/3_9_images/images.py
@@ -3,6 +3,21 @@ from tkinter import ttk
 import customtkinter as ctk
 from PIL import Image, ImageTk
 
+
+def stretch_image(event):
+	global resized_tk
+
+	# size
+	width = event.width
+	height = event.height
+
+	# create an image
+	resized_image = image_original.resize((width, height))
+	resized_tk = ImageTk.PhotoImage(resized_image)
+
+	# place on the canvas
+	canvas.create_image(0,0,image = resized_tk, anchor = 'nw')
+
 # setup
 window = tk.Tk()
 window.geometry('600x400')
