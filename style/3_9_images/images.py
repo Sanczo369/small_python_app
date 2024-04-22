72e57582dcfc2389c7d67475ada3190ab56f2e03
Arek Sanecki <areksanecki@gmail.com>
2024-04-22 23:49:11 +0200

    dodanie do projektu 'Images'(grid layout)

--- style/3_9_images/images.py
+++ style/3_9_images/images.py
@@ -23,5 +23,9 @@ window = tk.Tk()
 window.geometry('600x400')
 window.title('Images')
 
+# grid layout
+window.columnconfigure((0,1,2,3), weight = 1, uniform = 'a')
+window.rowconfigure(0, weight = 1)
+
 # run
 window.mainloop()
