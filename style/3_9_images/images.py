274d3f4093f2c5d656064172ed0c4d573ef00809
Arek Sanecki <areksanecki@gmail.com>
2024-04-22 23:48:10 +0200

    dodanie projektu 'Images'

---
+++ style/3_9_images/images.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+import customtkinter as ctk
+from PIL import Image, ImageTk
+
+# setup
+window = tk.Tk()
+window.geometry('600x400')
+window.title('Images')
+
+# run
+window.mainloop()
