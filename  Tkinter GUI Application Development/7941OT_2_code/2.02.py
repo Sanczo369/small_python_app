0e93ee55c54fb0c4749ce604bc26de40ca19cc73
Arek Sanecki <areksanecki@gmail.com>
2024-04-29 13:48:52 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.02(Edit menu)

---  Tkinter GUI Application Development/7941OT_2_code/2.02.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.02.py
@@ -32,3 +32,17 @@ filemenu.add_separator()
 filemenu.add_command(label="Exit", accelerator='Alt+F4')
 menubar.add_cascade(label="File", menu=filemenu)
 
+#Edit menu - Undo, Redo, Cut, Copy and Paste
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo",compound=LEFT,  image=undoicon, accelerator='Ctrl+Z')
+editmenu.add_command(label="Redo",compound=LEFT,  image=redoicon, accelerator='Ctrl+Y')
+editmenu.add_separator()
+editmenu.add_command(label="Cut", compound=LEFT, image=cuticon, accelerator='Ctrl+X')
+editmenu.add_command(label="Copy", compound=LEFT, image=copyicon,  accelerator='Ctrl+C')
+editmenu.add_command(label="Paste",compound=LEFT, image=pasteicon, accelerator='Ctrl+V')
+editmenu.add_separator()
+editmenu.add_command(label="Find",underline= 0, accelerator='Ctrl+F')
+editmenu.add_separator()
+editmenu.add_command(label="Select All", underline=7, accelerator='Ctrl+A')
+menubar.add_cascade(label="Edit", menu=editmenu)
+
