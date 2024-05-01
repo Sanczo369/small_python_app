ba91dbb519d142bacc9188d526175a78be46c86c
Arek Sanecki <areksanecki@gmail.com>
2024-05-01 23:12:50 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.03(funkcje)

---  Tkinter GUI Application Development/7941OT_2_code/2.03.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.03.py
@@ -8,4 +8,23 @@ from Tkinter import *
 root = Tk()
 root.geometry('350x350')
 
+########################################################################
+# Levaraging built in text widget functionalities
+
+def undo():
+    textPad.event_generate("<<Undo>>")
+
+def redo():
+    textPad.event_generate("<<Redo>>")
+
+def cut():
+    textPad.event_generate("<<Cut>>")
+
+def copy():
+    textPad.event_generate("<<Copy>>")
+
+def paste():
+    textPad.event_generate("<<Paste>>")
+
+
 root.mainloop()
