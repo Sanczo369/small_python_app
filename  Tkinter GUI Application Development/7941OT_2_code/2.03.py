aae384fc4fbbe4a9b15630d1ad4fc151d124de9c
Arek Sanecki <areksanecki@gmail.com>
2024-05-01 23:12:14 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.03

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.03.py
@@ -0,0 +1,11 @@
+"""
+Code illustration: 2.03
+Leveraging power of text widget built-in options
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+root = Tk()
+root.geometry('350x350')
+
+root.mainloop()
