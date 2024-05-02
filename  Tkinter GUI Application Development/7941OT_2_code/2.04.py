f286d31754024006486b61824effe412b0c4eefd
Arek Sanecki <areksanecki@gmail.com>
2024-05-02 18:06:22 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.04

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.04.py
@@ -0,0 +1,16 @@
+"""
+Code illustration: 2.04
+Indexing and tagging demonstrated
+Implementation of:
+1. select_all
+2. on_find
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import tkFileDialog
+root = Tk()
+root.geometry('350x350')
+
+
+root.mainloop()
