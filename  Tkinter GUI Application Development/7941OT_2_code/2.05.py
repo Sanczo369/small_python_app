73e50653f037572d17a55f56b713474de9ca4602
Arek Sanecki <areksanecki@gmail.com>
2024-05-04 18:38:37 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.05(transient)

---  Tkinter GUI Application Development/7941OT_2_code/2.05.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.05.py
@@ -11,5 +11,9 @@ root.title('Toplevel Window')
 root.geometry('300x300')
 Label(root, text='I am the Main Toplevel window\n All other windows here are my children').pack()
 
+#transient window
+t = Toplevel(root)
+Label(t, text='I am a transient window of root\n I always stay on top of my parent\n I get hidden if my parent window is minimized').pack()
+t.transient(root)
 
 root.mainloop()
