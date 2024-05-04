10617b3a9ff62a158eb7a515e3412b0c292e3278
Arek Sanecki <areksanecki@gmail.com>
2024-05-04 18:40:49 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.06

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.06.py
@@ -0,0 +1,15 @@
+"""
+Code illustration: 2.06
+A demonstration of tkFileDialog
+@Tkinter GUI Application Development Hotshot
+"""
+
+
+
+from Tkinter import *
+import tkFileDialog
+
+root = Tk()
+
+
+root.mainloop()
