1ec4be8c83f24c62cc97a225fc3121e2ee2ca537
Arek Sanecki <areksanecki@gmail.com>
2024-05-05 19:57:00 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.07

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.07.py
@@ -0,0 +1,24 @@
+"""
+Code illustration: 2.07
+
+Adding
+File>New,
+File>Open,
+File>Save
+File>Save As
+functionality
+
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import tkFileDialog
+import os
+root = Tk()
+root.geometry('350x350')
+#######################################################################
+
+
+
+
+root.mainloop()
