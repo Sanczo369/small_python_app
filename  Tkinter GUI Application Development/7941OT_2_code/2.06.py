46b061136d475bebb0365483d633709ce70af886
Arek Sanecki <areksanecki@gmail.com>
2024-05-05 19:54:30 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.06(fr1 i fr2)

---  Tkinter GUI Application Development/7941OT_2_code/2.06.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.06.py
@@ -27,4 +27,20 @@ def askDirectory():
     tkFileDialog.askdirectory()
 
 
+fr1 = Frame(root)
+fr2 = Frame(root)
+opt = {'fill': BOTH, 'side':LEFT, 'padx': 2, 'pady': 3}
+Label(text="Demo of tkFileDialog").pack()
+Button(fr1, text='open_file', command=askopenfile).pack(opt)
+Button(fr1, text='open_filename', command=askopenfilename).pack(opt)
+Button(fr1, text='save_asfile', command=asksaveasFile).pack(opt)
+Button(fr2, text='save_asfilename', command=asksaveasfilename).pack(opt)
+Button(fr2, text='directory', command=askDirectory).pack(opt)
+
+fr1.pack()
+fr2.pack()
+
+
+
+
 root.mainloop()
