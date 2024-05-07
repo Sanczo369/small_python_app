17768dc582a2635070ea21cbb906d5bdd709ffe3
Arek Sanecki <areksanecki@gmail.com>
2024-05-07 23:06:48 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.07(About menu)

---  Tkinter GUI Application Development/7941OT_2_code/2.07.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.07.py
@@ -190,4 +190,15 @@ for k in sorted(clrschms):
     themesmenu.add_radiobutton(label=k, variable=themechoice)
 menubar.add_cascade(label="View", menu=viewmenu)
 
+
+# About menu - Aboutus, Help
+
+aboutmenu = Menu(menubar, tearoff=0)
+
+aboutmenu.add_command(label="About")
+
+aboutmenu.add_command(label="Help")
+
+menubar.add_cascade(label="About",  menu=aboutmenu)
+
 root.mainloop()
