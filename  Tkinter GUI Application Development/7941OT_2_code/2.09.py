997661340364c36314c725115b632c94a7e79e16
Arek Sanecki <areksanecki@gmail.com>
2024-05-08 23:48:31 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.09

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.09.py
@@ -0,0 +1,21 @@
+"""
+Code illustration: 2.09
+Adding About, Help Functionality
+Adding 'Really Quit ?' Prompt  to exit button
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import tkFileDialog
+import os
+import tkMessageBox
+
+root = Tk()
+
+root.geometry('350x350')
+
+
+
+
+
+root.mainloop()
