c7c624625c4c26358c6a843f4d0793055e9f4245
Arek Sanecki <areksanecki@gmail.com>
2024-05-08 23:50:10 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.09(open_file)

---  Tkinter GUI Application Development/7941OT_2_code/2.09.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.09.py
@@ -35,7 +35,17 @@ def new_file():
     textPad.delete(1.0,END)
 
 
-
+def open_file():
+    global filename
+    filename = tkFileDialog.askopenfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+    if filename == "":  # If no file chosen.
+        filename = None  # Absence of file.
+    else:
+        root.title(os.path.basename(filename) + " - pyPad")  # Returning the basename of 'file'
+        textPad.delete(1.0, END)
+        fh = open(filename, "r")
+        textPad.insert(1.0, fh.read())
+        fh.close()
 
 
 root.mainloop()
