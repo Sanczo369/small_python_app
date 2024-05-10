d99a9fa1ee661d3c64acbb19fd4a56365742b200
Arek Sanecki <areksanecki@gmail.com>
2024-05-10 18:05:07 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.10(Displaying Line Numbers)

---  Tkinter GUI Application Development/7941OT_2_code/2.10.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.10.py
@@ -14,4 +14,14 @@ import tkMessageBox
 root = Tk()
 root.geometry('350x350')
 
+
+# Displaying Line Numbers
+def update_line_number(event=None):
+    txt = ''
+    if showln.get():
+        endline, endcolumn = textPad.index('end-1c').split('.')
+        txt = '\n'.join(map(str, range(1, int(endline))))
+    lnlabel.config(text=txt, anchor='nw')
+
+
 root.mainloop()
