a315ceb080e5e2def5d4b3f250bd581d54e1e18a
Arek Sanecki <areksanecki@gmail.com>
2024-05-10 18:04:37 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.10

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.10.py
@@ -0,0 +1,17 @@
+"""
+Code illustration: 2.10
+Adding Shortcut icon toolbar
+Displaying Line Numbers
+Highlighting Current Line
+
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import tkFileDialog
+import os
+import tkMessageBox
+root = Tk()
+root.geometry('350x350')
+
+root.mainloop()
