9f5af5628285ee7b95c848b48df6250c720d0f81
Arek Sanecki <areksanecki@gmail.com>
2024-05-11 15:30:49 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.10(about method)

---  Tkinter GUI Application Development/7941OT_2_code/2.10.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.10.py
@@ -37,5 +37,10 @@ def toggle_highlight(event=None):
     val = hltln.get()
     undo_highlight() if not val else highlight_line()
 
+##########################################################################
+
+#Defining about method
+def about(event=None):
+    tkMessageBox.showinfo("About","Tkinter GUI Application\n Development Hotshot")
 
 root.mainloop()
