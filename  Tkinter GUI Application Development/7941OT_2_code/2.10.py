a4a6bc5c2e2b4a8e417da4bce22f3df6b7f9d07b
Arek Sanecki <areksanecki@gmail.com>
2024-05-14 23:49:10 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.10(creating icon toolbar)

---  Tkinter GUI Application Development/7941OT_2_code/2.10.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.10.py
@@ -239,5 +239,16 @@ menubar.add_cascade(label="About",  menu=aboutmenu)
 # Returning defined setting for widget
 root.config(menu=menubar)
 
+shortcutbar = Frame(root,  height=25)
+
+#creating icon toolbar
+icons = ['new_file' ,'open_file', 'save', 'cut', 'copy', 'paste', 'undo', 'redo','on_find', 'about']
+for i, icon in enumerate(icons):
+    tbicon = PhotoImage(file='icons/'+icon+'.gif')
+    cmd = eval(icon)
+    toolbar = Button(shortcutbar, image=tbicon, command=cmd)
+    toolbar.image = tbicon
+    toolbar.pack(side=LEFT)
+
 
 root.mainloop()
