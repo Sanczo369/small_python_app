469682b467dd17bec0b105ada1bb2ec5693af7a7
Arek Sanecki <areksanecki@gmail.com>
2024-05-15 19:24:04 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.11(Creating Info Bar // widget within text widget)

---  Tkinter GUI Application Development/7941OT_2_code/2.11.py
+++  Tkinter GUI Application Development/7941OT_2_code/2.11.py
@@ -15,9 +15,13 @@ root.geometry('350x350')
 
 
 
+
+#Creating Info Bar // widget within text widget
+infobar = Label(textPad, text='Line: 1 | Column: 0')
+infobar.pack(expand=NO, fill=None, side=RIGHT, anchor='se')
+
 #Add events
 textPad.bind("<Any-KeyPress>", update_line_number)
 textPad.tag_configure("active_line", background="ivory2")
-
 
 root.mainloop()
