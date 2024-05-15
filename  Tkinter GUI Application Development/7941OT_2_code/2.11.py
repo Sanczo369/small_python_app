2a587e3aa81b101d47f6c7cc8149e3bda0b47108
Arek Sanecki <areksanecki@gmail.com>
2024-05-15 19:23:19 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.11

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.11.py
@@ -0,0 +1,17 @@
+"""
+Code illustration: 2.11
+Adding infoBar at bottom
+Adding Color Theme
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import tkFileDialog
+import os
+import tkMessageBox
+
+root = Tk()
+root.geometry('350x350')
+
+
+root.mainloop()
