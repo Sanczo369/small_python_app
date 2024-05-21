94612ff303999ba90bfaf5b79545348a3e4a4966
Arek Sanecki <areksanecki@gmail.com>
2024-05-21 22:19:41 +0200

    Projekt "Tkinter GUI Application Development" dodanie 2.12

---
+++  Tkinter GUI Application Development/7941OT_2_code/2.12.py
@@ -0,0 +1,19 @@
+"""
+Code illustration: 2.12.py
+Features Added:
+1.	Event Handling
+2.	Contextual Menu
+3.      Add TitleBar Icon
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import tkFileDialog
+import os
+import tkMessageBox
+
+root = Tk()
+root.geometry('350x350')
+root.iconbitmap('icons/pypad.ico')
+
+root.mainloop()
