2c4704817290ffd86886d7b649eba69ad29173e8
Arek Sanecki <areksanecki@gmail.com>
2024-05-27 22:58:55 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.02()

---
+++  Tkinter GUI Application Development/7941OT_3_code/3.02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Code illustration: 3.02
+Drum Program Editor Code
+Setting up Widgets
+@Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+
+class DrumMachine():
+    pass
+# ======================================================================
+
+if __name__ == '__main__':
+    dm = DrumMachine()
+    dm.app()
+
+
