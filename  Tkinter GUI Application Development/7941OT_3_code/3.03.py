1bad25525367a6ef8fa960ca81c043445b1e9b5e
Arek Sanecki <areksanecki@gmail.com>
2024-05-28 17:39:09 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.03(button_clicked)

---  Tkinter GUI Application Development/7941OT_3_code/3.03.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.03.py
@@ -11,9 +11,17 @@ from tkinter import *
 
 
 class DrumMachine():
-    pass
 
-# ======================================================================
+    def button_clicked(self, i, j, bpu):
+        def callback():
+            btn = self.button[i][j]
+            color = 'grey55' if (j / bpu) % 2 else 'khaki'
+            new_color = 'green' if btn.cget('bg') != 'green' else color
+            btn.config(bg=new_color)
+        return callback
+
+
+    # ======================================================================
 
 if __name__ == '__main__':
     dm = DrumMachine()
