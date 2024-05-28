facde1c05c12079769434c427cc8a06e18daf33c
Arek Sanecki <areksanecki@gmail.com>
2024-05-28 17:36:14 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.02(create_top_bar-BPUs)

---  Tkinter GUI Application Development/7941OT_3_code/3.02.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.02.py
@@ -59,6 +59,12 @@ class DrumMachine():
         self.units_widget = Spinbox(topbar_frame, from_=1, to=8, width=5, textvariable=self.units)
         self.units_widget.grid(row=0, column=5)
 
+        Label(topbar_frame, text='BPUs:').grid(row=0, column=6)
+        self.bpu = IntVar()
+        self.bpu.set(4)
+        self.bpu_widget = Spinbox(topbar_frame, from_=1, to=10, width=5, textvariable=self.bpu)
+        self.bpu_widget.grid(row=0, column=7)
+
 # ======================================================================
 
 if __name__ == '__main__':
