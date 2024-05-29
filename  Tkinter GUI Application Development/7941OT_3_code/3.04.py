e1ace80df18ef484edaddf93f50c5b8f37d37533
Arek Sanecki <areksanecki@gmail.com>
2024-05-29 18:20:23 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.04(drum_load)

---  Tkinter GUI Application Development/7941OT_3_code/3.04.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.04.py
@@ -16,6 +16,25 @@ class DrumMachine():
         self.widget_drum_file_name = [0]*MAX_DRUM_NUM
         self.current_drum_no = 0
 
+    def drum_load(self, drum_no):
+        def callback():
+            self.current_drum_no = drum_no
+            try:
+                file_name = tkFileDialog.askopenfilename(defaultextension=".wav",
+                                                        filetypes=[("Wave Files", "*.wav"), ("OGG Files", "*.ogg")])
+                if not file_name: return
+                try:
+                    del self.widget_drum_file_name[drum_no]
+                except:
+                    pass
+                self.widget_drum_file_name.insert(drum_no, file_name)
+                drum_name = os.path.basename(file_name)
+                self.widget_drum_name[drum_no].delete(0, END)
+                self.widget_drum_name[drum_no].insert(0, drum_name)
+            except:
+                tkMessageBox.showerror('Invalid', "Error loading drum samples")
+        return callback
+
 
 # ======================================================================
 
