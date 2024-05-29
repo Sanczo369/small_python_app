b98f3327a8cef7ac47c323ec8a0325b853ee564a
Arek Sanecki <areksanecki@gmail.com>
2024-05-29 18:18:48 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.04(DrumMachine)

---  Tkinter GUI Application Development/7941OT_3_code/3.04.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.04.py
@@ -10,6 +10,13 @@ import tkFileDialog
 import tkMessageBox
 import os
 
+class DrumMachine():
+    def __init__(self):
+        self.widget_drum_name = []
+        self.widget_drum_file_name = [0]*MAX_DRUM_NUM
+        self.current_drum_no = 0
+
+
 # ======================================================================
 
 if __name__ == '__main__':
