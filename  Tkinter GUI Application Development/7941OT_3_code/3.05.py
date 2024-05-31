23458ba14bbecef9ff03105c7bf7e337034c84c9
Arek Sanecki <areksanecki@gmail.com>
2024-05-31 14:53:53 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.05

---
+++  Tkinter GUI Application Development/7941OT_3_code/3.05.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Code illustration: 3.05
+Playing Sound Files with pymedia module
+
+@Tkinter GUI Application Development Hotshot
+"""
+
+from tkinter import *
+import tkFileDialog
+import tkMessageBox
+import os
+
+class DrumMachine():
+    pass
+
+
+# ======================================================================
+if __name__ == '__main__':
+    dm = DrumMachine()
+    dm.app()
