aa579de7ea41b51ab6e14582d98f42cb709b0f19
Arek Sanecki <areksanecki@gmail.com>
2024-05-31 14:55:23 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.05(play_sound)

---  Tkinter GUI Application Development/7941OT_3_code/3.05.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.05.py
@@ -22,6 +22,29 @@ class DrumMachine():
         self.widget_drum_file_name = [0] * MAX_DRUM_NUM
         self.current_drum_no = 0
 
+    def play(self):
+        for i in range(len(self.button[0])):
+            for item in self.button:
+                try:
+                    if item[i].cget('bg') == 'green':
+                        if not self.widget_drum_file_name[self.button.index(item)]: continue
+                        sound_filename = self.widget_drum_file_name[self.button.index(item)]
+                        self.play_sound(sound_filename)
+                except:
+                    continue
+            time.sleep(3 / 4.0)  # corresponds to 80 beats per minute (80 BPM)(60/80)
+
+    def play_sound(self, sound_filename):
+        try:
+            self.s = wave.open(sound_filename, 'rb')
+            sample_rate = self.s.getframerate()
+            channels = self.s.getnchannels()
+            frmt = sound.AFMT_S16_LE
+            self.snd= sound.Output(sample_rate, channels, frmt)
+            s = self.s.readframes(300000)
+            self.snd.play(s)
+        except:
+            pass
 
 # ======================================================================
 if __name__ == '__main__':
