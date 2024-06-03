8961b9f874b543fb3aef836c4a3d04103b9b2d46
Arek Sanecki <areksanecki@gmail.com>
2024-06-03 21:43:51 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.06(print error)

---  Tkinter GUI Application Development/7941OT_3_code/3.06.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.06.py
@@ -12,9 +12,9 @@ widget = Button(root, text="#", bg='green')
 widget.pack()
 
 # prints out a dictionary of all config items for that widget
-print widget.config()
+print(widget.config())
 
 #prints out the dictionary key value pair for the 'bg' option of the said widget
-print widget.config('bg')
+print(widget.config('bg'))
 
 root.mainloop()
