2913d661fb34b64afaaf8e5fcfc3a08d36fcf569
Arek Sanecki <areksanecki@gmail.com>
2024-06-03 21:46:52 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.07(app)

---  Tkinter GUI Application Development/7941OT_3_code/3.07.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.07.py
@@ -18,8 +18,18 @@ import pymedia.audio.sound as sound
 import threading
 
 class DrumMachine():
-    pass
+
+
+
+    def app(self):
+        self.root = Tk()
+        self.root.title('Drum Beast')
+        self.create_top_bar()
+        self.create_left_pad()
+        self.create_play_bar()
+        self.root.mainloop()
 
 # ======================================================================
 if __name__ == '__main__':
     dm = DrumMachine()
+    dm.app()
