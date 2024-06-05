80dcb40159b7944b023ba5fb4c72988f5e11e91b
Arek Sanecki <areksanecki@gmail.com>
2024-06-05 23:25:31 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.07(create_play_bar)

---  Tkinter GUI Application Development/7941OT_3_code/3.07.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.07.py
@@ -99,6 +99,19 @@ class DrumMachine():
                 btn.config(bg=new_color)
             return callback
 
+    def create_play_bar(self):
+        playbar_frame = Frame(self.root, height=15)
+        ln = MAX_DRUM_NUM+10
+        playbar_frame.grid(row=ln, columnspan=13,sticky=W+E,padx=15, pady=10)
+        self.start_button = Button( playbar_frame, text ='Play', command= self.play_in_thread)
+        self.start_button.grid(row= ln, column=1, padx=1)
+        button = Button( playbar_frame, text ='Stop', command= self.stop_play)
+        button.grid(row= ln, column=3,padx=1)
+        loop = BooleanVar()
+        loopbutton = Checkbutton(playbar_frame, text='Loop', variable=loop, command=lambda: self.loop_play(loop.get()))
+        loopbutton.grid(row=ln, column=16,padx=1)
+
+
     def app(self):
         self.root = Tk()
         self.root.title('Drum Beast')
