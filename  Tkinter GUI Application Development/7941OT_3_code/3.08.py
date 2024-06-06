df3283249fbac0ce250a88c4a6bf148e28f58a93
Arek Sanecki <areksanecki@gmail.com>
2024-06-06 23:27:39 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.08

---
+++  Tkinter GUI Application Development/7941OT_3_code/3.08.py
@@ -0,0 +1,29 @@
+"""
+Code illustration: 3.08
+Adding Support For Multiple beat patterns
+@Tkinter GUI Application Development Hotshot
+"""
+
+from tkinter import *
+import tkinter.filedialog
+import tkinter.messagebox
+import os
+
+
+#modules for playing sounds
+import time
+import wave
+import pymedia.audio.sound as sound
+
+import threading
+
+
+class DrumMachine():
+    pass
+
+# ======================================================================
+
+if __name__ == '__main__':
+    dm = DrumMachine()
+
+
