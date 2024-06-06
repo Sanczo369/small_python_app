0d8e38c9602c386abb8d054c8e987e545c159b68
Arek Sanecki <areksanecki@gmail.com>
2024-06-06 23:28:58 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.08(record_pattern)

---  Tkinter GUI Application Development/7941OT_3_code/3.08.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.08.py
@@ -30,6 +30,24 @@ class DrumMachine():
         self.loop = False
         self.pattern_list = [None] * 10
 
+    def record_pattern(self):
+        pattern_num, bpu, units = self.patt.get(), self.bpu.get(), self.units.get()
+        self.pat_name.config(state='normal')
+        self.pat_name.delete(0, END)
+        self.pat_name.insert(0, 'Pattern %s' % pattern_num)
+        self.pat_name.config(state='readonly')
+        prevpval = self.prevpatvalue
+        self.prevpatvalue = pattern_num
+        c = bpu * units
+        self.buttonpickleformat = [[0] * c for x in range(MAX_DRUM_NUM)]
+        for i in range(MAX_DRUM_NUM):
+            for j in range(c):
+                if self.button[i][j].config('bg')[-1] == 'green':
+                    self.buttonpickleformat[i][j] = 'active'
+        self.pattern_list[prevpval] = {'df': self.widget_drum_file_name, 'bl': self.buttonpickleformat, 'bpu': bpu,
+                                       'units': units}
+        self.reconstruct_pattern(pattern_num, bpu, units)
+
 
 # ======================================================================
 
