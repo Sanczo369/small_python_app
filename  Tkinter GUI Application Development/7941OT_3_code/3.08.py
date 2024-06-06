b3f816ee32878dddf1e2b72d41addd92ade43ca7
Arek Sanecki <areksanecki@gmail.com>
2024-06-06 23:28:35 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.08(__init__)

---  Tkinter GUI Application Development/7941OT_3_code/3.08.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.08.py
@@ -22,7 +22,14 @@ MAX_DRUM_NUM = 5
 
 
 class DrumMachine():
-    pass
+    def __init__(self):
+        self.widget_drum_name = []
+        self.widget_drum_file_name = [0] * MAX_DRUM_NUM
+        self.current_drum_no = 0
+        self.keep_playing = True
+        self.loop = False
+        self.pattern_list = [None] * 10
+
 
 # ======================================================================
 
