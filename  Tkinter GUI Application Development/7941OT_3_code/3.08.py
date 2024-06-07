e1443d6a5c6fec3484e6dc5dbab6eafa405cf91a
Arek Sanecki <areksanecki@gmail.com>
2024-06-07 15:56:38 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.08(play_sound)

---  Tkinter GUI Application Development/7941OT_3_code/3.08.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.08.py
@@ -107,6 +107,18 @@ class DrumMachine():
                 time.sleep(1 / 8.0)  # change this to modify the tempo
                 if self.loop == False: self.keep_playing = False
 
+    def play_sound(self, sound_filename):
+        try:
+            self.s = wave.open(sound_filename, 'rb')
+            sample_rate = self.s.getframerate()
+            channels = self.s.getnchannels()
+            frmt = sound.AFMT_S16_LE
+            self.snd = sound.Output(sample_rate, channels, frmt)
+            s = self.s.readframes(300000)
+            self.snd.play(s)
+        except:
+            pass
+
 
 # ======================================================================
 
