5a330c93bdfb7acdbf772dfa0afa76a390ade058
Arek Sanecki <areksanecki@gmail.com>
2024-06-08 19:07:00 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.09

---
+++  Tkinter GUI Application Development/7941OT_3_code/3.09.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Code illustration: 3.09
+Object Persistence: pickling and unpickling
+@Tkinter GUI Application Development Hotshot
+"""
+
+from tkinter import *
+import tkinter.filedialog
+import tkinter.messagebox
+import os
+
+#modules for playing sounds
+import time
+import wave
+import pymedia.audio.sound as sound
+
+import threading
+import pickle
+
+
+class DrumMachine():
+    pass
+
+# ======================================================================
+if __name__ == '__main__':
+    dm = DrumMachine()
+
