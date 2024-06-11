a51d8d1ab75adf0eca373804dab8d9b91e0ee402
Arek Sanecki <areksanecki@gmail.com>
2024-06-11 10:41:01 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.09(button_clicked)

---  Tkinter GUI Application Development/7941OT_3_code/3.09.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.09.py
@@ -169,6 +169,13 @@ class DrumMachine():
                 tkMessageBox.showerror('Invalid', "Error loading drum samples")
         return callback
 
+    def button_clicked(self,i,j,bpu):
+            def callback():
+                btn = self.button[i][j]
+                color = 'grey55' if (j/bpu)%2 else 'khaki'
+                new_color = 'green' if btn.cget('bg') != 'green' else color
+                btn.config(bg=new_color)
+            return callback
 
 # ======================================================================
 if __name__ == '__main__':
