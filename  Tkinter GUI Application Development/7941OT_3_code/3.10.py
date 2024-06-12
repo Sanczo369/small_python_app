559c51046a67c155745ac930b970a83ee50ea1ba
Arek Sanecki <areksanecki@gmail.com>
2024-06-12 23:01:40 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.10(Notebook)

---  Tkinter GUI Application Development/7941OT_3_code/3.10.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.10.py
@@ -16,6 +16,14 @@ style = ttk.Style()
 print(style.theme_names())
 #style.theme_use('clam')
 
+n = ttk.Notebook(root)
+n.grid(row=12, column=1)
+f1 = ttk.Frame(n); # you can embed other widgets into these frames
+f2 = ttk.Frame(n);
+n.add(f1, text='Tab One')
+n.add(f2, text='Tab Two')
+
+
 mytree = ttk.Treeview(root, height=2, columns=2)
 mytree.grid(row=14, columnspan=2)
 mytree.heading('#0', text='Column A', anchor=W)
