304f582119e9d0ab14a36188e2f5b235b4308b18
Arek Sanecki <areksanecki@gmail.com>
2024-06-12 22:56:18 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.09(app)

---  Tkinter GUI Application Development/7941OT_3_code/3.09.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.09.py
@@ -260,7 +260,19 @@ class DrumMachine():
         self.menubar.add_cascade(label="About", menu=self.aboutmenu)
         self.root.config(menu=self.menubar)
 
+    def app(self):
+        self.root = Tk()
+        self.root.title('Drum Beast')
+        self.top_menu()
+        self.create_top_bar()
+        self.create_left_pad()
+        self.create_play_bar()
+        self.root.protocol('WM_DELETE_WINDOW', self.exit_app)
+        self.root.mainloop()
+
+
 # ======================================================================
 if __name__ == '__main__':
     dm = DrumMachine()
+    dm.app()
 
