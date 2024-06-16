07fa13cc3c020260489ee3e173946a8c851b1402
Arek Sanecki <areksanecki@gmail.com>
2024-06-16 16:59:47 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.11

---
+++  Tkinter GUI Application Development/7941OT_3_code/3.11.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Code illustration: 3.11.py
+tttk widgets styling and theming explained
+@Tkinter GUI Application Development Hotshot
+"""
+from tkinter import *
+import tkinter.ttk
+root= Tk()
+
+
+
+
+root.mainloop()
+
+
