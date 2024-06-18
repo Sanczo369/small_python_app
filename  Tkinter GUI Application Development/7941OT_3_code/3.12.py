be19fdc86bc3ade910aa01702f8577395ae2bb67
Arek Sanecki <areksanecki@gmail.com>
2024-06-18 23:06:58 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.12(play_in_thread)

---  Tkinter GUI Application Development/7941OT_3_code/3.12.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.12.py
@@ -118,6 +118,10 @@ class DrumMachine():
             return
 
 
+    def play_in_thread(self):
+        self.thread = threading.Thread(None,self.play, None, (), {})
+        self.thread.start()
+
 # ======================================================================
 if __name__ == '__main__':
     dm = DrumMachine()
