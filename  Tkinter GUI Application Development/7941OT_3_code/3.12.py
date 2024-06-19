6c7c81557a8a8f016bdb4f0bad460a8527f88ebd
Arek Sanecki <areksanecki@gmail.com>
2024-06-19 12:16:35 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.12(loop_play)

---  Tkinter GUI Application Development/7941OT_3_code/3.12.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.12.py
@@ -154,6 +154,9 @@ class DrumMachine():
         self.keep_playing = False
         return
 
+    def loop_play(self, xval):
+        self.loop = xval
+
 
 # ======================================================================
 if __name__ == '__main__':
