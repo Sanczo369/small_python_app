87d74819f2f3b354522410c7b3403f77d11b487e
Arek Sanecki <areksanecki@gmail.com>
2024-06-19 12:18:46 +0200

    Projekt "Tkinter GUI Application Development" dodanie 3.12(create_play_bar)

---  Tkinter GUI Application Development/7941OT_3_code/3.12.py
+++  Tkinter GUI Application Development/7941OT_3_code/3.12.py
@@ -181,6 +181,23 @@ class DrumMachine():
                 btn.config(bg=new_color)
             return callback
 
+    def create_play_bar(self):
+        playbar_frame = Frame(self.root, height=15)
+        ln = MAX_DRUM_NUM + 10
+        playbar_frame.grid(row=ln, columnspan=13, sticky=W + E, padx=15, pady=10)
+        button = ttk.Button(playbar_frame, text='Play', command=self.play_in_thread)
+        button.grid(row=ln, column=1, padx=1)
+        button = ttk.Button(playbar_frame, text='Stop', command=self.stop_play)
+        button.grid(row=ln, column=3, padx=1)
+        loop = BooleanVar()
+        loopbutton = ttk.Checkbutton(playbar_frame, text='Loop', variable=loop,
+                                     command=lambda: self.loop_play(loop.get()))
+        loopbutton.grid(row=ln, column=16, padx=1)
+        photo = PhotoImage(file='images/sig.gif')
+        label = Label(playbar_frame, image=photo)
+        label.image = photo
+        label.grid(row=ln, column=35, padx=1, sticky=E)
+
 
 # ======================================================================
 if __name__ == '__main__':
