af2e8870f77818ea26e27bbf9fd76a068398e323
Arek Sanecki <areksanecki@gmail.com>
2024-06-21 10:57:52 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.01 gui.py(GUI - var)

---  Tkinter GUI Application Development/7941OT_4_code/4.01/gui.py
+++  Tkinter GUI Application Development/7941OT_4_code/4.01/gui.py
@@ -6,6 +6,17 @@ Tkinter GUI Application Development Hotshot
 """
 from tkinter import *
 
+
+class GUI():
+    rows = 8
+    columns = 8
+    color1 = "#DDB88C"
+    color2 = "#A66D4F"
+    dim_square = 64
+
+
+
+
 def main():
     root = Tk()
     root.title("Chess")
