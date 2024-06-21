f1d2ca841ef258fe1e0f77036eedccd706ca7e3c
Arek Sanecki <areksanecki@gmail.com>
2024-06-21 10:57:25 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.01 gui.py

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.01/gui.py
@@ -0,0 +1,16 @@
+"""
+Code illustration: 4.01
+1) Creating the View File
+2) Drawing Chess Board With the Canvas Widget
+Tkinter GUI Application Development Hotshot
+"""
+from tkinter import *
+
+def main():
+    root = Tk()
+    root.title("Chess")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
