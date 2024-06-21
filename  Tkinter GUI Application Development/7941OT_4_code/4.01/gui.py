e97a958b6ef520fdab9fc17631eeb4a43a65d717
Arek Sanecki <areksanecki@gmail.com>
2024-06-21 10:58:05 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.01 gui.py(GUI - __init__)

---  Tkinter GUI Application Development/7941OT_4_code/4.01/gui.py
+++  Tkinter GUI Application Development/7941OT_4_code/4.01/gui.py
@@ -14,6 +14,13 @@ class GUI():
     color2 = "#A66D4F"
     dim_square = 64
 
+    def __init__(self, parent):
+        self.parent = parent
+        canvas_width = self.columns * self.dim_square
+        canvas_height = self.rows * self.dim_square
+        self.canvas = Canvas(parent, width=canvas_width, height=canvas_height)
+        self.canvas.pack(padx=8, pady=8)
+        self.draw_board()
 
 
 
