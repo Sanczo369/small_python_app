b4473d45fbc8264826f354b7d4873a3ac16cf887
Arek Sanecki <areksanecki@gmail.com>
2024-06-23 21:35:36 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.01(Player)

---
+++  Tkinter GUI Application Development/7941OT_5_code/5.01/player.py
@@ -0,0 +1,16 @@
+"""
+Code illustration: 5.01
+Basic implemetation of pyglet player class
+Tkinter GUI Application Development Hotshot
+"""
+import pyglet
+from threading import Thread
+
+
+class Player():
+    parent = None
+
+
+if __name__ == '__main__':
+
+    print('a pyglet player class implementation')
