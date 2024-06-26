c85f62c1b008081e211fef13eb9658ada43dd1b0
Arek Sanecki <areksanecki@gmail.com>
2024-06-26 12:19:34 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.03(main-gui.py)

---
+++  Tkinter GUI Application Development/7941OT_5_code/5.03/main-gui.py
@@ -0,0 +1,17 @@
+'''
+METHODS ADDED
+--------------------------------
+next_track
+prev_track
+
+METHODS MODIFIED
+--------------------------------
+create_button_frame - added all control buttons
+'''
+from tkinter import *
+import tkinter.filedialog
+import tkinter.messagebox
+import os
+import player
+class GUI:
+    pass
