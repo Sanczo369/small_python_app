20ea14aaaebbdbeac0bb0b73c5844002c31fd64d
Arek Sanecki <areksanecki@gmail.com>
2024-06-29 15:36:01 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.03(GUI - toggle_play_pause)

---  Tkinter GUI Application Development/7941OT_5_code/5.03/main-gui.py
+++  Tkinter GUI Application Development/7941OT_5_code/5.03/main-gui.py
@@ -73,15 +73,20 @@ class GUI:
                 self.listbox.insert(END, item)
             self.alltracks = list(self.listbox.get(0, END))
 
-
     def del_selected(self):
         while len(self.listbox.curselection()) > 0:
             self.listbox.delete(self.listbox.curselection()[0])
         self.alltracks = list(self.listbox.get(0, END))
 
-
     def clear_list(self):
         self.listbox.delete(0, END)
         self.alltracks = list(self.listbox.get(0, END))
 
+    def toggle_play_pause(self):
+        if self.playbtn['text'] == 'play':
+            self.playbtn.config(text='stop', image=self.stopicon)
+            self.identify_track_to_play()
+        elif self.playbtn['text'] == 'stop':
+            self.playbtn.config(text='play', image=self.playicon)
+            self.player.pause()
 
