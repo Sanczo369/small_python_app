8498168541242125fbeaf28c7a1be4a026d8b245
Arek Sanecki <areksanecki@gmail.com>
2024-07-02 23:06:07 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.04(GUI- create_button_frame(previous.gif))

---  Tkinter GUI Application Development/7941OT_5_code/5.04/main-gui.py
+++  Tkinter GUI Application Development/7941OT_5_code/5.04/main-gui.py
@@ -64,3 +64,10 @@ class GUI:
         self.progressBar.grid(row=2, columnspan=10, sticky='ew', padx=5)
 
         cnslfrm.grid(row=1, pady=1, padx=0)
+
+    def create_button_frame(self):
+        buttonframe = Frame(self.root)
+        previcon = PhotoImage(file='../icons/previous.gif')
+        prevbtn=Button(buttonframe, image=previcon, borderwidth=0, padx=0, command=self.prev_track)
+        prevbtn.image = previcon
+        prevbtn.grid(row=3, column=1, sticky='w')
