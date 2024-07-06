8442c79954fc9324ec105f0196524707b91f4b68
Arek Sanecki <areksanecki@gmail.com>
2024-07-06 19:10:09 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.04(Player)

---
+++  Tkinter GUI Application Development/7941OT_5_code/5.04/player.py
@@ -0,0 +1,30 @@
+"""
+Code illustration: 5.04
+
+NEW METHODS ADDED HERE:
+-------------------------
+song_len() - to calculate the total length of the song
+current_time() - to calculate the current duration of play
+
+METHODS MODIFIED:
+-----------------------
+start_play_thread - added a call to song_len to get the song length
+
+@Tkinter GUI Application Development Hotshot
+"""
+
+
+import pyglet
+from threading import Thread
+import time
+
+FWDREWNDTIME = 20
+
+class Player():
+    parent = None
+    metadata ={} #mp3 artist title year genre in dictionary form.
+    song_length = 0 #in seconds
+    paused = False
+    stopped = False
+    current_time = 0
+    vol = 0.0
