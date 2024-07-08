e3b898d95fde7e2c9a093a2948a37d66c97f996d
Arek Sanecki <areksanecki@gmail.com>
2024-07-08 22:09:04 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.05(GUI-init)

---
+++  Tkinter GUI Application Development/7941OT_5_code/5.05/main-gui.py
@@ -0,0 +1,28 @@
+"""
+Code illustration: 5.05
+ATRRIBUTES ADDED:
+loopchoices ( a dictionary of name and assigned raio button value)
+selectedloopchoice (1,2 or 3)
+METHODS MODIFIED:
+create_bottom_frame () -  #radio buttons added
+@Tkinter GUI Application Development Hotshot
+"""
+from Tkinter import *
+import tkFileDialog
+import tkMessageBox
+import ttk
+import os
+import time
+import player
+
+
+class GUI:
+    alltracks = []
+    indx = 0
+    currentTrack = ''
+    timer = [0, 0]  # corresponding to minutes and seconds
+    timepattern = '{0:02d}:{1:02d}'
+
+    # new attributes added here
+    loopchoices = [("No Loop", 1), ("Loop Current", 2), ("Loop All", 3)]
+    selectedloopchoice = 3  # deafult 'no loop'
