b4e1ebc59e32518ca278863392450326bb9b6fff
Arek Sanecki <areksanecki@gmail.com>
2024-07-20 15:16:45 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.06(Player-song_len)

---  Tkinter GUI Application Development/7941OT_5_code/5.06/player.py
+++  Tkinter GUI Application Development/7941OT_5_code/5.06/player.py
@@ -64,3 +64,11 @@ class Player():
         except:
             current_time = 0
         return current_time
+
+    def song_len(self):
+        try:
+            self.song_length = self.source.duration
+        except:
+            self.song_length = 0
+        return self.song_length
+
