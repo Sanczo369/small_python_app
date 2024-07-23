afa47453bda8a7f98368c9c957fd000b1f9a1c69
Arek Sanecki <areksanecki@gmail.com>
2024-07-23 22:45:12 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.07(GUI - list_frame)

---  Tkinter GUI Application Development/7941OT_5_code/5.07/main-gui.py
+++  Tkinter GUI Application Development/7941OT_5_code/5.07/main-gui.py
@@ -113,5 +113,17 @@ class GUI:
 
         buttonframe.grid(row=3, columnspan=5, sticky='w', pady=4, padx=5)
 
+    def list_frame(self):
+        list_frame = Frame(self.root)
+        self.listbox = Listbox(list_frame, activestyle='none', cursor='hand2', bg='#1C3D7D', fg='#A0B9E9', selectmode=EXTENDED, width=60, heigh=10)
+        self.listbox.pack(side=LEFT, fill=BOTH, expand=1)
+        self.listbox.bind("<Double-Button-1>", self.identify_track_to_play)
+        self.listbox.bind("<Button-3>", self.show_context_menu)
+        scrollbar = Scrollbar(list_frame)
+        scrollbar.pack(side=RIGHT, fill=BOTH)
+        self.listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.listbox.yview)
+        list_frame.grid(row=4, padx=5)
+
 
 
