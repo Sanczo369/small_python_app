1f5cd3d310a2a7bdb42acb118404bc1505c16138
Arek Sanecki <areksanecki@gmail.com>
2024-07-23 22:46:58 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 5.07(GUI - create_bottom_frame - del_selected)

---  Tkinter GUI Application Development/7941OT_5_code/5.07/main-gui.py
+++  Tkinter GUI Application Development/7941OT_5_code/5.07/main-gui.py
@@ -135,6 +135,12 @@ class GUI:
         add_filebtn.grid(row=5, column=1)
         self.balloon.bind(add_filebtn, 'Add New File')
 
+        del_selectedicon = PhotoImage(file='../icons/del_selected.gif')
+        del_selectedbtn=Button(bottomframe,image=del_selectedicon,borderwidth=0,padx=0, text='Delete', command=self.del_selected)
+        del_selectedbtn.image = del_selectedicon
+        del_selectedbtn.grid(row=5, column=2 )
+        self.balloon.bind(del_selectedbtn, 'Delete Selected')
+
 
 
 
