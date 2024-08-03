1ad8828347bdb7ec0389fb14ff2f07863b50c3a4
Arek Sanecki <areksanecki@gmail.com>
2024-08-03 16:44:02 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.01(GUI- create_menu)

---  Tkinter GUI Application Development/7941OT_6_code/6.01.py
+++  Tkinter GUI Application Development/7941OT_6_code/6.01.py
@@ -29,3 +29,13 @@ class GUI(framework.GUIFramework):
         self.create_top_bar()
         self.create_tool_bar()
         self.create_drawing_canvas()
+
+    def create_menu(self):
+        self.menubar = Menu(self.root)
+        self.menuitems = (
+            'File- &New/Ctrl+N/self.new_file, &Open/Ctrl+O/self.open_file, Save/Ctrl+S/self.save, SaveAs//self.save_as, Sep, Exit/Alt+F4/self.close',
+            'Edit- Undo/Ctrl+Z/self.undo, Sep',
+            'About- About//self.about'
+        )
+        self.build_menu()
+        self.root.config(menu=self.menubar)
