b913db3f19682097a2c08518e7fcdb518cbdcfb4
Arek Sanecki <areksanecki@gmail.com>
2024-08-03 16:43:51 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.01(GUI)

---
+++  Tkinter GUI Application Development/7941OT_6_code/6.01.py
@@ -0,0 +1,31 @@
+"""
+Code illustration: 6.01
+
+Creating the Overall Structure for our Drawing Program
+
+Tkinter GUI Application Development Hotshot
+"""
+
+
+
+from tkinter  import *
+import tkFileDialog
+import tkMessageBox
+import Image
+import ImageTk
+from tkColorChooser import askcolor
+import framework
+
+
+class GUI(framework.GUIFramework):
+    background = 'white'
+    foreground = 'red'
+    currentobject = None
+    filename = 'untitled'
+
+    def __init__(self, root):
+        self.root = root
+        self.create_menu()
+        self.create_top_bar()
+        self.create_tool_bar()
+        self.create_drawing_canvas()
