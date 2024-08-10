241758da5da28ef1f582ad82276f65ce795a8a08
Arek Sanecki <areksanecki@gmail.com>
2024-08-10 18:30:02 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.02(GUI init)

---  Tkinter GUI Application Development/7941OT_6_code/6.02.py
+++  Tkinter GUI Application Development/7941OT_6_code/6.02.py
@@ -176,3 +176,8 @@ class GUI(framework.GUIFramework):
         if tkMessageBox.askokcancel("Quit", "Do you really want to quit?"):
             self.root.destroy()
 
+if __name__ == '__main__':
+    root = Tk()
+    app = GUI(root)
+    root.mainloop()
+
