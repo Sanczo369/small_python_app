0f8a8d5e9970938d4e46e490708769827cce6d7c
Arek Sanecki <areksanecki@gmail.com>
2024-08-11 16:30:29 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.03(GUI)

---
+++  Tkinter GUI Application Development/7941OT_6_code/6.03.py
@@ -0,0 +1,37 @@
+"""
+Code illustration: 6.03
+Drawing Basic Shapes on the Canvas
+ATTRIBUTED ADDED HERE:
+all_toolbar_functions
+selected_toolbar_func_index
+METHODS MODIFIED:
+mouse_down_motion
+mouse_up
+selected_tool_bar_item
+NEW METHODS ADDED:
+draw_line
+draw_oval
+draw_rectangle
+drawPolygon
+draw_brush
+Tkinter GUI Application Development Hotshot
+"""
+from tkinter import *
+import tkFileDialog
+import tkMessageBox
+import Image
+import ImageTk
+from tkColorChooser import askcolor
+import framework
+class GUI(framework.GUIFramework):
+    background = 'white'
+    foreground = 'red'
+    currentobject = None
+    filename = 'untitled'
+    currentobject = None
+    startx = 0
+    starty = 0
+    lastx = 0
+    lasty = 0
+    all_toolbar_functions = ('draw_line', 'draw_rectangle', 'draw_oval', 'draw_brush')
+    selected_toolbar_func_index = 0 # draw_line
