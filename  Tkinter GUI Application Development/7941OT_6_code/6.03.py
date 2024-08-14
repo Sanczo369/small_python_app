41f9e5d2aae1c5d29f51effd62123c3c127f0c16
Arek Sanecki <areksanecki@gmail.com>
2024-08-14 23:45:00 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.03(GUI undo)

---  Tkinter GUI Application Development/7941OT_6_code/6.03.py
+++  Tkinter GUI Application Development/7941OT_6_code/6.03.py
@@ -175,6 +175,9 @@ class GUI(framework.GUIFramework):
         self.currentobject = self.canvas.create_image(10, 10, anchor=NW, image=self.image)
         self.canvas.config(scrollregion=self.canvas.bbox(ALL))
 
+    def undo(self, event=None):
+        self.canvas.delete(self.currentobject)
+
 
 
 
