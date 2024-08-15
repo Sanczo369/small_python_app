0bbbadea580b452048a5f8a20c646e3706527e03
Arek Sanecki <areksanecki@gmail.com>
2024-08-15 22:41:17 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.04(GUI)

---
+++  Tkinter GUI Application Development/7941OT_6_code/6.04.py
@@ -0,0 +1,63 @@
+"""
+Code illustration: 6.04
+Adding TopBar to display options for each of the selected buttons
+
+
+ATTRIBUTED ADDED HERE:
+arrow='none'
+width=1.0
+dash = None
+fill = 'red'
+outline = 'black'
+
+
+METHODS MODIFIED:
+selected_tool_bar_item
+draw_line
+draw_rectangle
+draw_oval
+draw_brush
+
+
+NEW METHODS ADDED:
+fill_options_combobox / set_fill
+OutlineOpt / set_outline
+width_options_combobox / set_width
+arrow_options_combobox / set_arrow
+dash_options_combobox  /set_dash
+
+
+
+Tkinter GUI Application Development Hotshot
+"""
+
+from tkinter import *
+import tkFileDialog
+import tkMessageBox
+import Image
+import ImageTk
+from tkColorChooser import askcolor
+
+import ttk
+
+import framework
+
+
+class GUI(framework.GUIFramework):
+    background = 'white'
+    foreground = 'red'
+    currentobject = None
+    filename = 'untitled'
+    currentobject = None
+    startx = 0
+    starty = 0
+    lastx = 0
+    lasty = 0
+    all_toolbar_functions = ('draw_line', 'draw_rectangle', 'draw_oval', 'draw_brush')
+    selected_toolbar_func_index = 0  # draw_line
+
+    arrow = 'none'
+    width = 1.0
+    dash = None
+    fill = 'red'
+    outline = 'black'
