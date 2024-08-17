a4ce07b02356737f2c7ba77e8db8578f2542396a
Arek Sanecki <areksanecki@gmail.com>
2024-08-17 18:27:44 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.04(GUI set_foreground_color)

---  Tkinter GUI Application Development/7941OT_6_code/6.04.py
+++  Tkinter GUI Application Development/7941OT_6_code/6.04.py
@@ -124,3 +124,14 @@ class GUI(framework.GUIFramework):
         except:pass
         self.colorpallete.itemconfig(self.backgroundpallete, outline=self.background, fill=self.background)
 
+    def set_foreground_color(self, event=None):
+        self.foreground = askcolor()[-1]
+        try:
+            self.set_fill()
+        except:
+            pass
+        try:
+            self.set_outline()
+        except:
+            pass
+        self.colorpallete.itemconfig(self.foregroundpallete, outline=self.foreground, fill=self.foreground)
