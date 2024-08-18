7a7a7b5e1df05004ea90a655decfd4810ed46232
Arek Sanecki <areksanecki@gmail.com>
2024-08-18 15:59:17 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 6.04(GUI - set_arrow)

---  Tkinter GUI Application Development/7941OT_6_code/6.04.py
+++  Tkinter GUI Application Development/7941OT_6_code/6.04.py
@@ -203,4 +203,6 @@ class GUI(framework.GUIFramework):
     def set_width(self, event):
         self.width = float(self.widthcmbobx.get())
 
+    def set_arrow(self, event):
+        self.arrow = self.arrowcmbobx.get()
 
