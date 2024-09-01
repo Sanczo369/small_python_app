a0f238668292faa01158a8da3490d5ec45622b52
Arek Sanecki <areksanecki@gmail.com>
2024-09-01 16:27:57 +0200

    Projekt "Tkinter GUI Application Development" dodanie 4.02(GUI)

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.02/gui.py
@@ -0,0 +1,17 @@
+"""
+Code illustration: 4.02
+#no changes
+
+Tkinter GUI Application Development Hotshot
+"""
+
+from tkinter import *
+
+
+class GUI(dict):
+    rows = 8
+    columns = 8
+    color1 = "#DDB88C"
+    color2 = "#A66D4F"
+    dim_square = 64
+    images = {}
