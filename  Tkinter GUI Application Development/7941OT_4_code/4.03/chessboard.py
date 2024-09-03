626bba7a6dfd4203451629fb3b67d4e87befed40
Arek Sanecki <areksanecki@gmail.com>
2024-09-03 23:18:48 +0200

    Projekt "Tkinter GUI Application Development" dodanie do code 4.03 chessboard.py

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.03/chessboard.py
@@ -0,0 +1,19 @@
+"""
+Code illustration: 4.03
+
+--->  no changes here from the previous iteration
+
+Tkinter GUI Application Development Hotshot
+"""
+import pieces
+import re
+
+START_PATTERN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w 0 1'
+
+
+class Board(dict):
+    y_axis = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
+    x_axis = (1, 2, 3, 4, 5, 6, 7, 8)
+
+    def __init__(self, patt=None):
+        self.process_notation(START_PATTERN)
