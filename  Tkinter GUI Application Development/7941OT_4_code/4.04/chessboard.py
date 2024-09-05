cef0ae36cc918b68815be8b9dc54673fd94bd530
Arek Sanecki <areksanecki@gmail.com>
2024-09-05 23:46:44 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.04 chessboard.py(Board __init__)

---  Tkinter GUI Application Development/7941OT_4_code/4.04/chessboard.py
+++  Tkinter GUI Application Development/7941OT_4_code/4.04/chessboard.py
@@ -14,3 +14,7 @@ START_PATTERN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w 0 1'
 class Board(dict):
     y_axis = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
     x_axis = (1, 2, 3, 4, 5, 6, 7, 8)
+
+    def __init__(self, patt=None):
+        self.process_notation(START_PATTERN)
+
