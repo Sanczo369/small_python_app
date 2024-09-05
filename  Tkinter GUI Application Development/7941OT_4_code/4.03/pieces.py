46a8604c27d76e5a1460c32ed4cba10d60bc97c8
Arek Sanecki <areksanecki@gmail.com>
2024-09-05 23:44:03 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.03 Pieces(create_piece)

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.03/pieces.py
@@ -0,0 +1,19 @@
+"""
+Code illustration: 4.03
+Displaying Pieces on the board
+
+Tkinter GUI Application Development Hotshot
+"""
+
+import sys
+
+SHORT_NAME = { 'R':'Rook', 'N':'Knight', 'B':'Bishop', 'Q':'Queen', 'K':'King', 'P':'Pawn'}
+
+def create_piece(piece, color='white'):
+    ''' Takes a piece name or shortname and returns the corresponding piece instance '''
+    if piece in (None, ' '): return
+    if piece.isupper(): color = 'white'
+    else: color = 'black'
+    piece = SHORT_NAME[piece.upper()]
+    module = sys.modules[__name__]
+    return module.__dict__[piece](color)
