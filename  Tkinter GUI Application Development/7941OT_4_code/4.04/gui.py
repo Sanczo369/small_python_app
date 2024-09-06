56c315fd3c0ef55fd9bad50a794e03e33343f5b6
Arek Sanecki <areksanecki@gmail.com>
2024-09-06 16:36:17 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.04 GUI(__init__)

---  Tkinter GUI Application Development/7941OT_4_code/4.04/gui.py
+++  Tkinter GUI Application Development/7941OT_4_code/4.04/gui.py
@@ -19,3 +19,11 @@ class GUI(dict):
     dim_square = 64
     images = {}
 
+    def __init__(self, parent, chessboard):
+        self.parent = parent
+        self.chessboard = chessboard
+        canvas_width = self.columns * self.dim_square
+        canvas_height = self.rows * self.dim_square
+        self.canvas = Canvas(parent, width=canvas_width, height=canvas_height)
+        self.canvas.pack(padx=8, pady=8)
+        self.draw_board()
