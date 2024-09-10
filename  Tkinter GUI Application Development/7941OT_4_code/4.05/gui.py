1b2583ec6dad9c89e1bbde779b11411d228c4602
Arek Sanecki <areksanecki@gmail.com>
2024-09-10 23:27:13 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.05 GUI(GUI)

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.05/gui.py
@@ -0,0 +1,19 @@
+"""
+Code illustration: 4.05
+
+--> No changes done here from the previous iteration
+
+Tkinter GUI Application Development Hotshot
+"""
+
+from Tkinter import *
+import chessboard
+from PIL import ImageTk
+
+class GUI(dict):
+    rows = 8
+    columns = 8
+    color1 = "#DDB88C"
+    color2 = "#A66D4F"
+    dim_square = 64
+    images = {}
