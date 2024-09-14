d1c472987ca92d52a84169d91b54bdd5b227c566
Arek Sanecki <areksanecki@gmail.com>
2024-09-14 18:30:09 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.06 pieces(SHORT_NAME)

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.06/pieces.py
@@ -0,0 +1,17 @@
+"""
+Code illustration: 4.06
+--> No changes done here from the previous iteration
+
+Tkinter GUI Application Development Hotshot
+"""
+
+import sys
+
+SHORT_NAME = {
+ 'R':'Rook',
+ 'N':'Knight',
+ 'B':'Bishop',
+ 'Q':'Queen',
+ 'K':'King',
+ 'P':'Pawn'
+}
