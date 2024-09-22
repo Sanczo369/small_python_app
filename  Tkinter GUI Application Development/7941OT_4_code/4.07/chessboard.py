9d6f1fc199534d6bc49cf38e4a710e3ffcfab64b
Arek Sanecki <areksanecki@gmail.com>
2024-09-22 16:16:27 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.07 chessboard(Board king_in_check)

---  Tkinter GUI Application Development/7941OT_4_code/4.07/chessboard.py
+++  Tkinter GUI Application Development/7941OT_4_code/4.07/chessboard.py
@@ -105,3 +105,11 @@ class Board(dict):
             if isinstance(self[pos], pieces.King) and self[pos].color == color:
                 return pos
 
+    def king_in_check(self, color):
+        kingpos =  self.position_of_king(color)
+        opponent = ('black' if color =='white' else 'white')
+        for pieces in self.iteritems():
+            if kingpos in self.all_moves_available(opponent):
+                return True
+            else:
+                return False
