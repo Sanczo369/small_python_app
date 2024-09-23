a1c5eae176e64768afcfe28d2554a9c4632124b6
Arek Sanecki <areksanecki@gmail.com>
2024-09-23 23:21:36 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 4.07 pieces(SHORT_NAME)

---
+++  Tkinter GUI Application Development/7941OT_4_code/4.07/pieces.py
@@ -0,0 +1,18 @@
+"""
+Code illustration: 4.07
+
+NO CHANGES FROM PREVIOUS ITERATION
+
+Tkinter GUI Application Development Hotshot
+"""
+
+import sys
+
+SHORT_NAME = {
+ 'R':'Rook',
+ 'N':'Knight',
+ 'B':'Bishop',
+ 'Q':'Queen',
+ 'K':'King',
+ 'P':'Pawn'
+}
