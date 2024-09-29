6ff4a5b37cde535137bff0fdcfe09afc7fe4ef91
Arek Sanecki <areksanecki@gmail.com>
2024-09-29 16:32:43 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.06 weather reporter.py(WeatherReporter- display_frame)

---  Tkinter GUI Application Development/7941OT_7_code/7.06 weather reporter.py
+++  Tkinter GUI Application Development/7941OT_7_code/7.06 weather reporter.py
@@ -29,3 +29,10 @@ class WeatherReporter:
         self.enteredlocation = StringVar()
         Entry(topfrm, textvariable=self.enteredlocation).grid(row=1, column=3, sticky='w')
         ttk.Button(topfrm, text='Show Weather Info', command= self.show_weather_button_clicked).grid(row=1, column=4, sticky='w')
+
+    def display_frame(self):
+        displayfrm = Frame(self.root)
+        displayfrm.grid(row=2, sticky='ew', columnspan=5)
+        self.canvas = Canvas(displayfrm, height='410', width='300', background='black', borderwidth=5)
+        self.canvas.create_rectangle( 5, 5,305,415, fill='#F6AF06')
+        self.canvas.grid(row=2, sticky='w', columnspan=5)
