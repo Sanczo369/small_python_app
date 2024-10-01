79eee84fd1b4bc9bde6442e8ce6694666a24691d
Arek Sanecki <areksanecki@gmail.com>
2024-10-01 23:00:25 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.06 pie chart.py(prop)

---
+++  Tkinter GUI Application Development/7941OT_7_code/7.08 pie chart.py
@@ -0,0 +1,17 @@
+"""
+Code illustration: 7.08
+
+Piechart
+
+Tkinter GUI Application Development Hotshot
+"""
+
+
+import tkinter
+
+root = tkinter.Tk()
+
+#pie chart
+def prop(n):
+    return 360.0 * n / 1000
+
