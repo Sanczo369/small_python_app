390ca92725f261b933979327ee2fcc47af9c4143
Arek Sanecki <areksanecki@gmail.com>
2024-10-02 23:44:15 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.09 scatter plot.py(y-axis)

---  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
+++  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
@@ -19,3 +19,10 @@ c.create_line(50,250,300,250, width=3)
 for i in range(12):
     x = 50 + (i * 20)
     c.create_text(x,255,anchor='n', text='%d'% (20*i) )
+
+
+#y-axis
+c.create_line(50,250,50,20, width=3)
+for i in range(12):
+    y = 250 - (i * 20)
+    c.create_text(45,y, anchor='e', text='%d'% (20*i))
