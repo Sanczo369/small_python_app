407787fcf46b7ca5eb0a3ec2cb732c3deed316aa
Arek Sanecki <areksanecki@gmail.com>
2024-10-02 23:44:31 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.09 scatter plot.py(create scatter plots from random x-y values)

---  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
+++  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
@@ -26,3 +26,11 @@ c.create_line(50,250,50,20, width=3)
 for i in range(12):
     y = 250 - (i * 20)
     c.create_text(45,y, anchor='e', text='%d'% (20*i))
+
+#create scatter plots from random x-y values
+for i in range(35):
+    x,y = random.randint(100,210), random.randint(50,250)
+    c.create_oval(x-3,y-3,x+3,y+3, width=1, fill='red')
+
+
+root.mainloop()
