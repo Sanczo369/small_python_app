7d2f4ca4a6df90340cbb0eaa33baae618a9f846e
Arek Sanecki <areksanecki@gmail.com>
2024-10-02 23:44:02 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.09 scatter plot.py(create x-axis)

---  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
+++  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
@@ -12,3 +12,10 @@ root = tkinter.Tk()
 
 c = tkinter.Canvas(root, width=350, height=280, bg='white')
 c.grid()
+
+
+#create x-axis
+c.create_line(50,250,300,250, width=3)
+for i in range(12):
+    x = 50 + (i * 20)
+    c.create_text(x,255,anchor='n', text='%d'% (20*i) )
