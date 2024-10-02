ef7fb3f12248659b098008ba8f27890b81679c56
Arek Sanecki <areksanecki@gmail.com>
2024-10-02 23:43:44 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.09 scatter plot.py

---
+++  Tkinter GUI Application Development/7941OT_7_code/7.09 scatter plot.py
@@ -0,0 +1,14 @@
+"""
+Code illustration: 7.09
+
+Scatter Plot
+
+Tkinter GUI Application Development Hotshot
+"""
+
+import tkinter
+import random
+root = tkinter.Tk()
+
+c = tkinter.Canvas(root, width=350, height=280, bg='white')
+c.grid()
