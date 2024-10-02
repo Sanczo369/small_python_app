6c6b222eb26d7c63c6f098add1c3e15c3e196350
Arek Sanecki <areksanecki@gmail.com>
2024-10-02 23:46:12 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.10 bar graph.py(plotdata)

---  Tkinter GUI Application Development/7941OT_7_code/7.10 bar graph.py
+++  Tkinter GUI Application Development/7941OT_7_code/7.10 bar graph.py
@@ -19,3 +19,17 @@ barWidth = 20
 
 canv = Tkinter.Canvas(root, width=cwidth, height=cheight, bg= 'white')
 canv.pack()
+
+plotdata= [random.randint(0,200) for r in xrange(12)]
+for x, y in enumerate(plotdata):
+    x1 = x  + x * barWidth
+    y1 = cheight - y
+    x2 = x  + x * barWidth + barWidth
+    y2 = cheight
+    canv.create_rectangle(x1, y1, x2, y2, fill="blue")
+    canv.create_text(x1+3, y1, text=str(y),anchor='sw' )
+
+
+
+
+root.mainloop()
