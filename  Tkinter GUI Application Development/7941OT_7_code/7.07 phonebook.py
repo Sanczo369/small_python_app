ff1624374162889bfa38b259a26bdd2b697caaa3
Arek Sanecki <areksanecki@gmail.com>
2024-10-03 23:12:36 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 7.07 phonebook.p(PhoneBook -Label)

---  Tkinter GUI Application Development/7941OT_7_code/7.07 phonebook.py
+++  Tkinter GUI Application Development/7941OT_7_code/7.07 phonebook.py
@@ -17,3 +17,16 @@ class PhoneBook:
         label = Label(image=photo)
         label.image = photo
         label.grid(row=0, column=0)
+
+
+        Label(fr, text='Name:').grid(row=1, column=1, sticky=W, pady=2)
+        self.name= StringVar()
+        self.namefield = Entry(fr, textvariable= self.name)
+        self.namefield.grid(row=1, column=2, sticky=W, padx=5, pady=2)
+        Label(fr, text='Contact Number:').grid(row=2, column=1,sticky=W,  pady=2)
+        self.num= IntVar()
+        self.numfield = Entry(fr, textvariable= self.num)
+        self.numfield.grid(row=2, column=2, sticky=W,padx=5, pady=2)
+        ttk.Button(fr, text= 'Add Record', command=self.create_record).grid(row=3, column=2, sticky=E,padx=5, pady=2)
+        showbtn = ttk.Button(text="Show Records", command = self.view_records)
+        showbtn.grid(row=3, column=0, sticky=W)
