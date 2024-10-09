7c30bf3b6e76023749453eb8d95bdf388e5e34f4
Arek Sanecki <areksanecki@gmail.com>
2024-10-09 20:29:45 +0200

    Projekt "Tkinter GUI Application Development" dodanie do 8.07 formatting entry widget to display date.py (FormatEntryWidgetDemo __init__)

---
+++  Tkinter GUI Application Development/7941OT_8_code/8.07 formatting entry widget to display date.py
@@ -0,0 +1,16 @@
+"""
+Code illustration: 8.07
+Formatting Entry Widget
+Tkinter GUI Application Development Hotshot
+"""
+from tkinter import *
+class FormatEntryWidgetDemo:
+    def __init__(self, root):
+        Label(root, text='Date(MM/DD/YYYY)').pack()
+        self.entereddata = StringVar()
+        self.dateentrywidget = Entry(textvariable=self.entereddata)
+        self.dateentrywidget.pack(padx=5, pady=5)
+        self.dateentrywidget.focus_set()
+        self.slashpositions = [2, 5]
+        root.bind('<Key>', self.format_date_entry_widget)
+
