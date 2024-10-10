7b3767cc4f80206f2ff75e9258249b1e7b2b5096
Arek Sanecki <areksanecki@gmail.com>
2024-10-10 23:35:58 +0200

    Projekt "Tkinter GUI Application Development" dodanie 8.10 line encoding.py

---
+++  Tkinter GUI Application Development/7941OT_8_code/8.10 line encoding.py
@@ -0,0 +1,9 @@
+"""
+Code illustration: 8.10
+Line Unicode Encoding
+Tkinter GUI Application Development Hotshot
+"""
+from tkinter import *
+t = Tk()
+Label(text =u'भारत में आपका स्वागत है' ).pack()
+t.mainloop()
