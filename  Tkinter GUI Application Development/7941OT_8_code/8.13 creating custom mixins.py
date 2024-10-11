d9d66d089ddbc17f21ebc1eee64fc829b1a08175
Arek Sanecki <areksanecki@gmail.com>
2024-10-11 18:50:48 +0200

    Projekt "Tkinter GUI Application Development" dodanie 8.13 creating custom mixins.py(label)

---  Tkinter GUI Application Development/7941OT_8_code/8.13 creating custom mixins.py
+++  Tkinter GUI Application Development/7941OT_8_code/8.13 creating custom mixins.py
@@ -10,3 +10,8 @@ def frame(parent,  row, col):
     widget = Frame(parent)
     widget.grid(row= row, column=col)
     return widget
+
+def label(parent,  row, col, text):
+    widget = Label(parent, text=text)
+    widget.grid(row= row, column=col,  sticky='w', padx=2)
+    return widget
