354117c8e862d66990f978057b7d4fba4c9faae7
Arek Sanecki <areksanecki@gmail.com>
2024-10-13 15:37:37 +0200

    Projekt "Lung-Cancer-Detection " dodanie lcd_cnn.py(LCD_CNN __init__ img4)

--- Lung-Cancer-Detection/lcd_cnn.py
+++ Lung-Cancer-Detection/lcd_cnn.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from PIL import Image,ImageTk
+
 class LCD_CNN:
     def __init__(self,root):
         self.root=root
@@ -5,3 +8,8 @@ class LCD_CNN:
         self.root.geometry("1006x500+0+0")
         self.root.resizable(False, False)
         self.root.title("Lung Cancer Detection")
+
+        img4=Image.open(r"Images\Lung-Cancer-Detection.jpg")
+        img4=img4.resize((1006,500),Image.ANTIALIAS)
+        #Antialiasing is a technique used in digital imaging to reduce the visual defects that occur when high-resolution images are presented in a lower resolution.
+        self.photoimg4=ImageTk.PhotoImage(img4)
