268a35437186195fcd78ea2aa3d671e799af64fa
Arek Sanecki <areksanecki@gmail.com>
2024-10-17 23:52:52 +0200

    Projekt "Tkinter-login-form" login()

--- Tkinter-login-form/main.py
+++ Tkinter-login-form/main.py
@@ -6,3 +6,13 @@ window = tkinter.Tk()
 window.title("Login form")
 window.geometry('340x440')
 window.configure(bg='#333333')
+
+def login():
+    username = "johnsmith"
+    password = "12345"
+    if username_entry.get()==username and password_entry.get()==password:
+        messagebox.showinfo(title="Login Success", message="You successfully logged in.")
+    else:
+        messagebox.showerror(title="Error", message="Invalid login.")
+
+frame = tkinter.Frame(bg='#333333')
