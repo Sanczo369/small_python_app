cdbfe82115ebe5ed5015a3414fea35c186f0ee82
Arek Sanecki <areksanecki@gmail.com>
2024-10-17 23:54:36 +0200

    Projekt "tkinter-data-entry" (enter_data)

---
+++ tkinter-data-entry/main.py
@@ -0,0 +1,32 @@
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+
+
+def enter_data():
+    accepted = accept_var.get()
+
+    if accepted == "Accepted":
+        # User info
+        firstname = first_name_entry.get()
+        lastname = last_name_entry.get()
+
+        if firstname and lastname:
+            title = title_combobox.get()
+            age = age_spinbox.get()
+            nationality = nationality_combobox.get()
+
+            # Course info
+            registration_status = reg_status_var.get()
+            numcourses = numcourses_spinbox.get()
+            numsemesters = numsemesters_spinbox.get()
+
+            print("First name: ", firstname, "Last name: ", lastname)
+            print("Title: ", title, "Age: ", age, "Nationality: ", nationality)
+            print("# Courses: ", numcourses, "# Semesters: ", numsemesters)
+            print("Registration status", registration_status)
+            print("------------------------------------------")
+        else:
+            tkinter.messagebox.showwarning(title="Error", message="First name and last name are required.")
+    else:
+        tkinter.messagebox.showwarning(title="Error", message="You have not accepted the terms")
