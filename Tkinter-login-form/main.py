b99ec55b5f654fb610cf11118aca03255183720b
Arek Sanecki <areksanecki@gmail.com>
2024-10-17 23:53:27 +0200

    Projekt "Tkinter-login-form" (Placing widgets on the screen)

--- Tkinter-login-form/main.py
+++ Tkinter-login-form/main.py
@@ -28,3 +28,15 @@ password_label = tkinter.Label(
     frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
 login_button = tkinter.Button(
     frame, text="Login", bg="#FF3399", fg="#FFFFFF", font=("Arial", 16), command=login)
+
+# Placing widgets on the screen
+login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1, column=1, pady=20)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1, pady=20)
+login_button.grid(row=3, column=0, columnspan=2, pady=30)
+
+frame.pack()
+
+window.mainloop()
