692877daa0f57c7d5081a8937659c11737d97a57
Arek Sanecki <areksanecki@gmail.com>
2024-10-22 23:27:17 +0200

    Projekt "tkinter-data-entry" (numcourses)

--- tkinter-data-entry/main.py
+++ tkinter-data-entry/main.py
@@ -82,3 +82,14 @@ registered_check = tkinter.Checkbutton(courses_frame, text="Currently Registered
 
 registered_label.grid(row=0, column=0)
 registered_check.grid(row=1, column=0)
+
+numcourses_label = tkinter.Label(courses_frame, text= "# Completed Courses")
+numcourses_spinbox = tkinter.Spinbox(courses_frame, from_=0, to='infinity')
+numcourses_label.grid(row=0, column=1)
+numcourses_spinbox.grid(row=1, column=1)
+
+numsemesters_label = tkinter.Label(courses_frame, text="# Semesters")
+numsemesters_spinbox = tkinter.Spinbox(courses_frame, from_=0, to="infinity")
+numsemesters_label.grid(row=0, column=2)
+numsemesters_spinbox.grid(row=1, column=2)
+
