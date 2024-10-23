f4a24c19d15d32e3a183961337dd755e10905610
Arek Sanecki <areksanecki@gmail.com>
2024-10-23 23:27:52 +0200

    Projekt "Breadcrumbsinvoice-generator-tkinter-and-doxtpl" (generate_invoice)

--- invoice-generator-tkinter-and-doxtpl/main.py
+++ invoice-generator-tkinter-and-doxtpl/main.py
@@ -35,3 +35,12 @@ def new_invoice():
     tree.delete(*tree.get_children())
 
     invoice_list.clear()
+
+
+def generate_invoice():
+    doc = DocxTemplate("invoice_template.docx")
+    name = first_name_entry.get() + last_name_entry.get()
+    phone = phone_entry.get()
+    subtotal = sum(item[3] for item in invoice_list)
+    salestax = 0.1
+    total = subtotal * (1 - salestax)
