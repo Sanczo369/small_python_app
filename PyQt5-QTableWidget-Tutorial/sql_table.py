97598ef31dd15be840df7dc20b8c9cd7527de7d1
Arek Sanecki <areksanecki@gmail.com>
2024-10-25 17:47:56 +0200

    Projekt "PyQt5-QTableWidget-Tutorial" sql_table.py(MainWindow __init__())

---
+++ PyQt5-QTableWidget-Tutorial/sql_table.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+import sqlite3
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("tabletutorial.ui",self)
+        self.tableWidget.setColumnWidth(0, 250)
+        self.tableWidget.setColumnWidth(1, 100)
+        self.tableWidget.setColumnWidth(2, 350)
+        self.tableWidget.setHorizontalHeaderLabels(["City","Country","Subcountry"])
+        self.loaddata()
