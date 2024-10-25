d94c1c658eacb05ee00a5a12b722bcc4e9ff01d1
Arek Sanecki <areksanecki@gmail.com>
2024-10-25 17:45:03 +0200

    Projekt "PyQt5-QTableWidget-Tutorial" main.py(MainWindow loaddata()

--- PyQt5-QTableWidget-Tutorial/main.py
+++ PyQt5-QTableWidget-Tutorial/main.py
@@ -12,3 +12,14 @@ class MainWindow(QDialog):
         self.tableWidget.setColumnWidth(1,100)
         self.tableWidget.setColumnWidth(2,350)
         self.loaddata()
+
+    def loaddata(self):
+        people=[{"name":"John","age":45,"address":"New York"}, {"name":"Mark", "age":40,"address":"LA"},
+                {"name":"George","age":30,"address":"London"}]
+        row=0
+        self.tableWidget.setRowCount(len(people))
+        for person in people:
+            self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(person["name"]))
+            self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(str(person["age"])))
+            self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(person["address"]))
+            row=row+1
