8ec27e62c6c832ce3640cb8e9f6123f3022d7102
Arek Sanecki <areksanecki@gmail.com>
2024-10-26 18:37:11 +0200

    Projekt "tkinter-ai-image-generator" main.py(generate)

---
+++ tkinter-ai-image-generator/main.py
@@ -0,0 +1,17 @@
+import customtkinter as ctk # pip install customtkinter
+import tkinter
+import os
+import openai
+from PIL import Image, ImageTk
+import requests, io
+
+def generate():
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    user_prompt = prompt_entry.get("0.0", tkinter.END)
+    user_prompt += "in style: " + style_dropdown.get()
+
+    response = openai.Image.create(
+        prompt=user_prompt,
+        n=int(number_slider.get()),
+        size="512x512"
+    )
