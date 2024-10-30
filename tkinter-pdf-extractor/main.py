a1dafc25036fd3ae6199478b7c6b6169f3ab5a85
Arek Sanecki <areksanecki@gmail.com>
2024-10-30 22:41:19 +0100

    Projekt "tkinter-pdf-extractor" main.py(openFile)

--- tkinter-pdf-extractor/main.py
+++ tkinter-pdf-extractor/main.py
@@ -6,3 +6,9 @@ def openFile():
     filename = filedialog.askopenfilename(title="Open PDF file",
                                                   initialdir='D:\codefirst.io\Tkinter Extract PDF Text',
                                                   filetypes=[('PDF files', '*.pdf')])
+    filename_label.configure(text=filename)
+    outputfile_text.delete("1.0", tkinter.END)
+    reader = PyPDF2.PdfReader(filename)
+    for i in range (reader.numPages):
+        current_text = reader.getPage(i).extractText()
+        outputfile_text.insert(tkinter.END, current_text)
