5596000c8c0fc410fa3d61a39fe9d119d6d36840
Arek Sanecki <areksanecki@gmail.com>
2024-11-03 23:51:57 +0100

    Projekt "PyQt5-Daily-Task-Planner-App" main.py(Window - saveChanges)

--- PyQt5-Daily-Task-Planner-App/main.py
+++ PyQt5-Daily-Task-Planner-App/main.py
@@ -38,3 +38,24 @@ class Window(QWidget):
             elif result[1] == "NO":
                 item.setCheckState(QtCore.Qt.Unchecked)
             self.tasksListWidget.addItem(item)
+
+    def saveChanges(self):
+        db = sqlite3.connect("data.db")
+        cursor = db.cursor()
+        date = self.calendarWidget.selectedDate().toPyDate()
+
+        for i in range(self.tasksListWidget.count()):
+            item = self.tasksListWidget.item(i)
+            task = item.text()
+            if item.checkState() == QtCore.Qt.Checked:
+                query = "UPDATE tasks SET completed = 'YES' WHERE task = ? AND date = ?"
+            else:
+                query = "UPDATE tasks SET completed = 'NO' WHERE task = ? AND date = ?"
+            row = (task, date,)
+            cursor.execute(query, row)
+        db.commit()
+
+        messageBox = QMessageBox()
+        messageBox.setText("Changes saved.")
+        messageBox.setStandardButtons(QMessageBox.Ok)
+        messageBox.exec()
