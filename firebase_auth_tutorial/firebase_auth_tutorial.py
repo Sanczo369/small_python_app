899d468b3ed7c6c1275bb4ea96133a34094682a6
Arek Sanecki <areksanecki@gmail.com>
2024-11-08 00:57:34 +0100

    Projekt "firebase_auth_tutorial" Signup Function

--- firebase_auth_tutorial/firebase_auth_tutorial.py
+++ firebase_auth_tutorial/firebase_auth_tutorial.py
@@ -29,3 +29,19 @@ def login():
     except:
         print("Invalid email or password")
     return
+
+
+#Signup Function
+
+def signup():
+    print("Sign up...")
+    email = input("Enter email: ")
+    password=input("Enter password: ")
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        ask=input("Do you want to login?[y/n]")
+        if ask=='y':
+            login()
+    except:
+        print("Email already exists")
+    return
