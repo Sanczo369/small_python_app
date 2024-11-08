dbefa66ebc12a9b372a01876cbf25c5967105a66
Arek Sanecki <areksanecki@gmail.com>
2024-11-08 23:24:33 +0100

    Projekt "tkinter-matplotlib-dashboard" Chart 1

---
+++ tkinter-matplotlib-dashboard/dashboard.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from data import sales_data, inventory_data, product_data, sales_year_data, inventory_month_data
+
+
+plt.rcParams["axes.prop_cycle"] = plt.cycler(
+    color=["#4C2A85", "#BE96FF", "#957DAD", "#5E366E", "#A98CCC"])
+
+# Chart 1: Bar chart of sales data
+fig1, ax1 = plt.subplots()
+ax1.bar(sales_data.keys(), sales_data.values())
+ax1.set_title("Sales by Product")
+ax1.set_xlabel("Product")
+ax1.set_ylabel("Sales")
+# plt.show()
