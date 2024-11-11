685386b8dceda99e71756e7ac1d417a29dd9e517
Arek Sanecki <areksanecki@gmail.com>
2024-11-11 23:24:58 +0100

    Projekt "PyQT5-with-Firebase-Auth-Login-and-Signup-form" main.py firebaseConfig

---
+++ PyQT5-with-Firebase-Auth-Login-and-Signup-form/main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+import pyrebase
+
+firebaseConfig={'apiKey': "AIzaSyD9Bgw0XS4Oj_9viko9Fy3fZ2Wd7W0u72k",
+    'authDomain': "authdemo-16c35.firebaseapp.com",
+    'databaseURL': "https://authdemo-16c35.firebaseio.com",
+    'projectId': "authdemo-16c35",
+    'storageBucket': "authdemo-16c35.appspot.com",
+    'messagingSenderId': "752256979947",
+    'appId': "1:752256979947:web:2f71846c1d795d09db3fae",
+    'measurementId': "G-W4KNLK3307"}
