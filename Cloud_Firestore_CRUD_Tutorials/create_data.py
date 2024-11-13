471f72d03873902a4701fb42a20e777f9746ece2
Arek Sanecki <areksanecki@gmail.com>
2024-11-13 23:13:02 +0100

    Projekt "Cloud_Firestore_CRUD_Tutorials" create_data.py Using add to add documents with auto generated keys

--- Cloud_Firestore_CRUD_Tutorials/create_data.py
+++ Cloud_Firestore_CRUD_Tutorials/create_data.py
@@ -7,3 +7,10 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db=firestore.client()
+
+# Using add to add documents with auto generated keys
+db.collection('persons').add({'name':'John', 'age':40, 'address': "New York"})
+db.collection('persons').add({'name':'Jane', 'age':50, 'address': "Los Angeles"})
+db.collection('persons').add({'name':'Mark', 'age':40, 'address': "Paris"})
+db.collection('persons').add({'name':'Harry', 'age':40, 'address': "London"})
+db.collection('persons').add({'name':'Ron', 'age':40, 'address': "Milan"})
