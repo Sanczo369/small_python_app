bd175adcd0d461e3aad92644352d1eafedd0cc95
Arek Sanecki <areksanecki@gmail.com>
2024-11-14 23:27:39 +0100

    Projekt "Cloud_Firestore_CRUD_Tutorials" delete_data.py Delete a field - unknown ID

--- Cloud_Firestore_CRUD_Tutorials/delete_data.py
+++ Cloud_Firestore_CRUD_Tutorials/delete_data.py
@@ -20,3 +20,9 @@ docs = db.collection('persons').where("age", ">=", 50).get() # Get all documents
 for doc in docs:
     key = doc.id
     db.collection('persons').document(key).delete()
+
+# Delete a field - unknown ID
+docs = db.collection('persons').where("age", ">=", 40).get() # Get all documents with age >=40
+for doc in docs:
+    key = doc.id
+    db.collection('persons').document(key).update("age", firestore.DELETE_FIELD)
