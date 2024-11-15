3b4382e3741fec241d474b8fb2c9f923f4486893
Arek Sanecki <areksanecki@gmail.com>
2024-11-15 23:45:24 +0100

    Projekt "Cloud_Firestore_CRUD_Tutorials" update_data.py Update data with known key

--- Cloud_Firestore_CRUD_Tutorials/update_data.py
+++ Cloud_Firestore_CRUD_Tutorials/update_data.py
@@ -7,3 +7,11 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db=firestore.client()
+
+# Update data with known key
+db.collection('persons').document("p1").update({"age": 50}) # field already exists
+db.collection('persons').document("p1").update({"age": firestore.Increment(2)}) # increment a field
+db.collection('persons').document("p1").update({"occupation": "engineer"}) # the field will be added
+db.collection('persons').document("p1").update({"occupation": "engineer"})
+db.collection('persons').document("p2").update({"socials": firestore.ArrayRemove(['linkedin'])})
+db.collection('persons').document("p1").update({"socials": firestore.ArrayUnion(['linkedin'])})
