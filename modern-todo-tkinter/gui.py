1b37b899d8cbecd1ae889c14b1aecdeb17cc58cf
Arek Sanecki <areksanecki@gmail.com>
2024-11-15 23:47:44 +0100

    Projekt "modern-todo-tkinter " update_data.py root

--- modern-todo-tkinter/gui.py
+++ modern-todo-tkinter/gui.py
@@ -5,3 +5,8 @@ def add_todo():
     label = ctk.CTkLabel(scrollable_frame, text=todo)
     label.pack()
     entry.delete(0, ctk.END)
+
+
+root = ctk.CTk()
+root.geometry("750x450")
+root.title("Todo App")
