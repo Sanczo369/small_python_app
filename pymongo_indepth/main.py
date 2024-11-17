92595d50bb75dd00d4b08fc441137850507578ad
Arek Sanecki <areksanecki@gmail.com>
2024-11-17 22:16:19 +0100

    Projekt "pymongo_indepth " Create a collection

--- pymongo_indepth/main.py
+++ pymongo_indepth/main.py
@@ -6,3 +6,11 @@ client = pymongo.MongoClient()
 mydb = client["mydatabase"]
 #Print the list of databases
 print(client.list_database_names())
+
+#mydatabase will not appear in the list
+#MongoDB waits until you have created a collection with
+#at least one document (record) before it actually
+#creates the database
+
+#Create a collection
+mycol = mydb["people"]
