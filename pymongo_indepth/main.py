401a227ac2f9dc8529a32e6c7402a4d22c717052
Arek Sanecki <areksanecki@gmail.com>
2024-11-17 22:15:53 +0100

    Projekt "pymongo_indepth " Create a Database called mydatabase

--- pymongo_indepth/main.py
+++ pymongo_indepth/main.py
@@ -1,3 +1,8 @@
 import pymongo
 # Connect to MongoDB instance running on localhost
 client = pymongo.MongoClient()
+
+#Create a Database called mydatabase
+mydb = client["mydatabase"]
+#Print the list of databases
+print(client.list_database_names())
