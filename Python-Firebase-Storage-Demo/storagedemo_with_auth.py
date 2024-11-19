b39c8a675cbc00c4150cfd89091e11b9d74e7b4c
Arek Sanecki <areksanecki@gmail.com>
2024-11-19 23:13:50 +0100

    Projekt "Python Firebase Storage Demo" storagedemo_with_auth #setting up firebase

---
+++ Python-Firebase-Storage-Demo/storagedemo_with_auth.py
@@ -0,0 +1,14 @@
+import urllib
+import pyrebase
+
+#setting up firebase
+firebaseConfig={"apiKey": "AIzaSyDm2HeGl3bApix5KsbhI8NOjdwXkhNTaJM",
+    "authDomain": "trialauth-7eea1.firebaseapp.com",
+    "databaseURL": "https://trialauth-7eea1.firebaseio.com",
+    "projectId": "trialauth-7eea1",
+    "storageBucket": "trialauth-7eea1.appspot.com",
+    "messagingSenderId": "441088628124",
+    "appId": "1:441088628124:web:e894968e55f29cd52f2459",
+    "measurementId": "G-P4Y5QEDVXH"}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
