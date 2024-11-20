c4588b52cf63bdc26a0333ddde868c127596f215
Arek Sanecki <areksanecki@gmail.com>
2024-11-20 17:00:28 +0100

    Projekt "Python Firebase Storage Demo" firebasestoragedemo define storage

--- Python-Firebase-Storage-Demo/firebasestoragedemo.py
+++ Python-Firebase-Storage-Demo/firebasestoragedemo.py
@@ -10,3 +10,8 @@ firebaseConfig={"apiKey": "AIzaSyDm2HeGl3bApix5KsbhI8NOjdwXkhNTaJM",
     "messagingSenderId": "441088628124",
     "appId": "1:441088628124:web:e894968e55f29cd52f2459",
     "measurementId": "G-P4Y5QEDVXH"}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+#define storage
+storage=firebase.storage()
