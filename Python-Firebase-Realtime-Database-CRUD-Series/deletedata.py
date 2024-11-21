da85dda4685cef3f939d35d9184b7919dd4e4cb4
Arek Sanecki <areksanecki@gmail.com>
2024-11-21 23:02:15 +0100

    Projekt "Python-Firebase-Realtime-Database-CRUD-Series" createdata.py #Initialize Firebase

---
+++ Python-Firebase-Realtime-Database-CRUD-Series/deletedata.py
@@ -0,0 +1,14 @@
+import pyrebase
+
+firebaseConfig={"apiKey": "AIzaSyBPhwfUTQqOwKq2xH9087gHEslqEQTSNro",
+    "authDomain": "pyrebaserealtimedbdemo.firebaseapp.com",
+    "databaseURL": "https://pyrebaserealtimedbdemo.firebaseio.com",
+    "projectId": "pyrebaserealtimedbdemo",
+    "storageBucket": "pyrebaserealtimedbdemo.appspot.com",
+    "messagingSenderId": "843349173643",
+    "appId": "1:843349173643:web:90ff345ff844aa89d5fb8e",
+    "measurementId": "G-DT093HRL5R"}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+db=firebase.database()
