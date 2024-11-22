0a3b8647a7ee6a78c5b035a11b2156519ad0ab2c
Arek Sanecki <areksanecki@gmail.com>
2024-11-22 23:54:33 +0100

    Projekt "Python-Firebase-Realtime-Database-CRUD-Series" updatedata.py #Update data with unknown key

--- Python-Firebase-Realtime-Database-CRUD-Series/updatedata.py
+++ Python-Firebase-Realtime-Database-CRUD-Series/updatedata.py
@@ -20,3 +20,10 @@ db.child("todolistA").child("monday").child("paper").update({"deadline":"1pm"})
 #Multi-location update data
 data={"todolistA/monday/paper":{"details":"v2"}, "todolistA/tuesday/filmvideo":{"deadline":"7pm"}}
 db.update(data)
+
+#Update data with unknown key
+monday_tasks=db.child("todolistB").child("monday").get()
+for task in monday_tasks.each():
+    if(task.val()['name']=="paper"):
+        key=task.key()
+db.child("todolistB").child("monday").child(key).update({"deadline":"1pm"})
