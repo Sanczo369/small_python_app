c7ecd33dfa90a4cbe78a95d6e8dc7dd23f050aca
Arek Sanecki <areksanecki@gmail.com>
2024-11-23 23:11:00 +0100

    Projekt "pyqt5-login-form " updatedata.py

--- pyqt5-login-form/main.py
+++ pyqt5-login-form/main.py
@@ -37,3 +37,13 @@ class CreateAcc(QDialog):
             login=Login()
             widget.addWidget(login)
             widget.setCurrentIndex(widget.currentIndex()+1)
+
+
+app=QApplication(sys.argv)
+mainwindow=Login()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(480)
+widget.setFixedHeight(620)
+widget.show()
+app.exec_()
