b8f8984e3c39941ba1fa959776d652f642936ba4
Arek Sanecki <areksanecki@gmail.com>
2024-11-23 23:09:39 +0100

    Projekt "pyqt5-login-form " updatedata.py #Login __init__

---
+++ pyqt5-login-form/main.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login,self).__init__()
+        loadUi("login.ui",self)
+        self.loginbutton.clicked.connect(self.loginfunction)
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.createaccbutton.clicked.connect(self.gotocreate)
