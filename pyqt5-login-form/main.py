23483429611db5c60cda7884c549690f564e7f47
Arek Sanecki <areksanecki@gmail.com>
2024-11-23 23:10:24 +0100

    Projekt "pyqt5-login-form " updatedata.py #CreateAcc __init__

--- pyqt5-login-form/main.py
+++ pyqt5-login-form/main.py
@@ -20,3 +20,11 @@ class Login(QDialog):
         createacc=CreateAcc()
         widget.addWidget(createacc)
         widget.setCurrentIndex(widget.currentIndex()+1)
+
+class CreateAcc(QDialog):
+    def __init__(self):
+        super(CreateAcc,self).__init__()
+        loadUi("createacc.ui",self)
+        self.signupbutton.clicked.connect(self.createaccfunction)
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.confirmpass.setEchoMode(QtWidgets.QLineEdit.Password)
