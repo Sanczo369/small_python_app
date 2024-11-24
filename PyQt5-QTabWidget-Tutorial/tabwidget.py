a9f6cc77ab751598ddb8fca12d85ab9e8298ff78
Arek Sanecki <areksanecki@gmail.com>
2024-11-24 23:13:40 +0100

    Projekt "PyQt5-QTabWidget-Tutorial" tabwidget.py MainWindow __init__

---
+++ PyQt5-QTabWidget-Tutorial/tabwidget.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QTabWidget, QWidget
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("tabtutorial.ui",self)
+        self.tabWidget.currentChanged.connect(self.tabChanged)
+        self.change.clicked.connect(self.changeTabText)
+        self.insert.clicked.connect(self.insertTab)
+        self.remove.clicked.connect(self.removeTab)
