d733d190d7adb3ab666e63c1e4b37fb1aa4413af
Arek Sanecki <areksanecki@gmail.com>
2024-11-25 22:02:19 +0100

    Projekt "BreadcrumbsAdd-Image-To-PyQt5-Tutorial" add_image_tutorial.py MainWindow main

--- Add-Image-To-PyQt5-Tutorial/add_image_tutorial.py
+++ Add-Image-To-PyQt5-Tutorial/add_image_tutorial.py
@@ -14,3 +14,17 @@ class MainWindow(QDialog):
         #Add the image
         qpixmap = QPixmap('pythonlogo.png')
         self.imglabel.setPixmap(qpixmap)
+
+
+# main
+app = QApplication(sys.argv)
+mainwindow = MainWindow()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedHeight(430)
+widget.setFixedWidth(765)
+widget.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
