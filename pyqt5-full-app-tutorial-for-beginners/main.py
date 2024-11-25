a1ddf3c61c36cfa5575ad305228fc13fa4259203
Arek Sanecki <areksanecki@gmail.com>
2024-11-25 22:04:19 +0100

    Projekt "pyqt5-full-app-tutorial-for-beginners " main.py WelcomeScreen __init__

---
+++ pyqt5-full-app-tutorial-for-beginners/main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.QtGui import QPixmap
+
+import sqlite3
+
+
+class WelcomeScreen(QDialog):
+    def __init__(self):
+        super(WelcomeScreen, self).__init__()
+        loadUi("welcomescreen.ui",self)
+        self.login.clicked.connect(self.gotologin)
+        self.create.clicked.connect(self.gotocreate)
