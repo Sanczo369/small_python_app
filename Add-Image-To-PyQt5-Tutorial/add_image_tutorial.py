f2bc54ed54df9fb9a96219fd77e7e815d15609ab
Arek Sanecki <areksanecki@gmail.com>
2024-11-25 22:01:51 +0100

    Projekt "BreadcrumbsAdd-Image-To-PyQt5-Tutorial" add_image_tutorial.py MainWindow __init__

---
+++ Add-Image-To-PyQt5-Tutorial/add_image_tutorial.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.QtGui import QPixmap
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("addimagetutorial.ui",self)
+        self.button.clicked.connect(self.addimage)
