ff355c3f64161512e666410f1ff76fe686aaa9a2
Arek Sanecki <areksanecki@gmail.com>
2024-11-30 16:32:26 +0100

    Projekt "tkinter-url-shortener" main.py UI

--- tkinter-url-shortener/main.py
+++ tkinter-url-shortener/main.py
@@ -10,3 +10,17 @@ def shorten():
 root = tkinter.Tk()
 root.title("URL Shortener")
 root.geometry("300x150")
+
+longurl_label = tkinter.Label(root, text="Enter Long URL")
+longurl_entry = tkinter.Entry(root)
+shorturl_label=tkinter.Label(root, text="Output shortened URL")
+shorturl_entry = tkinter.Entry(root)
+shorten_button = tkinter.Button(root, text="Shorten URL", command=shorten)
+
+longurl_label.pack()
+longurl_entry.pack()
+shorturl_label.pack()
+shorturl_entry.pack()
+shorten_button.pack()
+
+root.mainloop()
