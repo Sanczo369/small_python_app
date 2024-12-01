42dc01556a4184c1495c9ae6a237155b6870d960
Arek Sanecki <areksanecki@gmail.com>
2024-12-01 23:23:50 +0100

    Projekt "tkinter-excel-app" main.py load_data()

---
+++ tkinter-excel-app/main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+import openpyxl
+
+
+def load_data():
+    path = "D:\codefirst.io\Tkinter Excel App\people.xlsx"
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+
+    list_values = list(sheet.values)
+    print(list_values)
+    for col_name in list_values[0]:
+        treeview.heading(col_name, text=col_name)
+
+    for value_tuple in list_values[1:]:
+        treeview.insert('', tk.END, values=value_tuple)
