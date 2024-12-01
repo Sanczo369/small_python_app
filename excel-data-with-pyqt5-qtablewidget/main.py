eb162fd1bfd826508abb9406b5ef64715ac7973f
Arek Sanecki <areksanecki@gmail.com>
2024-12-01 23:19:13 +0100

    Projekt "excel-data-with-pyqt5-qtablewidget" main.py Main load_data

--- excel-data-with-pyqt5-qtablewidget/main.py
+++ excel-data-with-pyqt5-qtablewidget/main.py
@@ -15,3 +15,22 @@ class Main(QWidget):
         layout.addWidget(self.table_widget)
 
         self.load_data()
+
+    def load_data(self):
+        path = "D:\codefirst.io\PyQt5 Excel to QTableWidget\list-countries-world.xlsx"
+        workbook = openpyxl.load_workbook(path)
+        sheet = workbook.active
+
+        self.table_widget.setRowCount(sheet.max_row)
+        self.table_widget.setColumnCount(sheet.max_column)
+
+        list_values = list(sheet.values)
+        self.table_widget.setHorizontalHeaderLabels(list_values[0])
+
+        row_index = 0
+        for value_tuple in list_values[1:]:
+            col_index = 0
+            for value in value_tuple:
+                self.table_widget.setItem(row_index, col_index, QTableWidgetItem(str(value)))
+                col_index += 1
+            row_index += 1
