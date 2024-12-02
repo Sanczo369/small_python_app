585b66f3e42896413518e1be9eda0ea24d05e763
Arek Sanecki <areksanecki@gmail.com>
2024-12-02 23:55:26 +0100

    Projekt "tkinter-excel-app" main.py age_spinbox

--- tkinter-excel-app/main.py
+++ tkinter-excel-app/main.py
@@ -69,3 +69,8 @@ name_entry = ttk.Entry(widgets_frame)
 name_entry.insert(0, "Name")
 name_entry.bind("<FocusIn>", lambda e: name_entry.delete('0', 'end'))
 name_entry.grid(row=0, column=0, padx=5, pady=(0, 5), sticky="ew")
+
+
+age_spinbox = ttk.Spinbox(widgets_frame, from_=18, to=100)
+age_spinbox.insert(0, "Age")
+age_spinbox.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
