abbb4cf41a56b34e625ccac0e16e24da078c1a8f
Arek Sanecki <areksanecki@gmail.com>
2024-12-03 23:27:44 +0100

    Projekt "excel-to-word-docs" main.py Generate docs

--- excel-to-word-docs/main.py
+++ excel-to-word-docs/main.py
@@ -9,3 +9,15 @@ sheet = workbook.active
 
 list_values = list(sheet.values)
 print(list_values)
+
+# Generate docs
+doc = DocxTemplate("certificate.docx")
+
+for value_tuple in list_values[1:]:
+    doc.render({"name": value_tuple[0],
+                "course": value_tuple[1],
+                "date": value_tuple[2],
+                "instructor": value_tuple[3]})
+
+    doc_name = "certificate" + value_tuple[0] + value_tuple[1] + ".docx"
+    doc.save(doc_name)
