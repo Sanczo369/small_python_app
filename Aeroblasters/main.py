c7b35ae5224f8a34ee7e202a2f6b308056895b58
Arek Sanecki <areksanecki@gmail.com>
2024-12-04 23:44:47 +0100

    Projekt "Aeroblasters " main.py FONTS

--- Aeroblasters/main.py
+++ Aeroblasters/main.py
@@ -49,3 +49,16 @@ sound_on_img = pygame.image.load("Assets/Buttons/soundOnBtn.png")
 home_btn = Button(home_img, (24, 24), WIDTH // 4 - 18, HEIGHT//2 + 120)
 replay_btn = Button(replay_img, (36,36), WIDTH // 2  - 18, HEIGHT//2 + 115)
 sound_btn = Button(sound_on_img, (24, 24), WIDTH - WIDTH // 4 - 18, HEIGHT//2 + 120)
+
+# FONTS ***********************************************************************
+
+game_over_font = 'Fonts/ghostclan.ttf'
+tap_to_play_font = 'Fonts/BubblegumSans-Regular.ttf'
+score_font = 'Fonts/DalelandsUncialBold-82zA.ttf'
+final_score_font = 'Fonts/DroneflyRegular-K78LA.ttf'
+
+game_over_msg = Message(WIDTH//2, 230, 30, 'Game Over', game_over_font, WHITE, win)
+score_msg = Message(WIDTH-50, 28, 30, '0', final_score_font, RED, win)
+final_score_msg = Message(WIDTH//2, 280, 30, '0', final_score_font, RED, win)
+tap_to_play_msg = tap_to_play = BlinkingText(WIDTH//2, HEIGHT-60, 25, "Tap To Play",
+				 tap_to_play_font, WHITE, win)
