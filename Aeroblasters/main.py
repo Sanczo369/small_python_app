5a04726f347c0485e3be3d37fc7711a6885c9fe5
Arek Sanecki <areksanecki@gmail.com>
2024-12-04 23:44:23 +0100

    Projekt "Aeroblasters " main.py IMAGES

--- Aeroblasters/main.py
+++ Aeroblasters/main.py
@@ -30,3 +30,16 @@ BLUE = (30, 144,255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 20)
+
+# IMAGES **********************************************************************
+
+plane_img = pygame.image.load('Assets/plane.png')
+logo_img = pygame.image.load('Assets/logo.png')
+fighter_img = pygame.image.load('Assets/fighter.png')
+clouds_img = pygame.image.load('Assets/clouds.png')
+clouds_img = pygame.transform.scale(clouds_img, (WIDTH, 350))
+
+home_img = pygame.image.load('Assets/Buttons/homeBtn.png')
+replay_img = pygame.image.load('Assets/Buttons/replay.png')
+sound_off_img = pygame.image.load("Assets/Buttons/soundOffBtn.png")
+sound_on_img = pygame.image.load("Assets/Buttons/soundOnBtn.png")
