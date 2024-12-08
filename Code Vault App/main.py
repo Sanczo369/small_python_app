8de0306ff065f26c932570cd838f7ee78e29b3b6
Arek Sanecki <areksanecki@gmail.com>
2024-12-08 23:23:39 +0100

    Projekt "Code Vault App"  main.py encrypt()

--- Code Vault App/main.py
+++ Code Vault App/main.py
@@ -25,3 +25,28 @@ def decrypt():
         messagebox.showerror("ENCRYPTION","Input Password")
     elif password!="1234":
         messagebox.showerror("ENCRYPTION","Invalid Paasword")
+
+
+def encrypt():
+    password = code.get()
+    if password == "1234":
+        screen1 = Toplevel(screen)
+        screen1.title("ENCRYPTION")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+
+        message = text1.get(1.0, END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt = base64_bytes.decode("ascii")
+
+        Label(screen1, text="ENCRYPT", font="arial", fg="white", bg="#ed3833").place(x=10, y=0)
+        text2 = Text(screen1, font="Robote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END, encrypt)
+
+    elif password == "":
+        messagebox.showerror("ENCRYPTION", "Input Password")
+    elif password != "1234":
+        messagebox.showerror("ENCRYPTION", "Invalid Paasword")
