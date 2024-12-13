1f1c009feda08a709e29898535f0c54b95b13f36
Arek Sanecki <areksanecki@gmail.com>
2024-12-13 22:57:58 +0100

    Projekt "Hotel_app" Hotel.py class Hotel add_client()

--- Hotel_app/hotel/hotel.py
+++ Hotel_app/hotel/hotel.py
@@ -10,3 +10,9 @@ class Hotel:
     def __init__(self, db_path):
         self.connection = sqlite3.connect(db_path)
         self.cursor = self.connection.cursor()
+
+    def add_client(self, first_name, last_name, age):
+        query = "INSERT INTO clients (first_name, last_name, age) VALUES (?, ?, ?)"
+        self.cursor.execute(query, (first_name, last_name, age))
+        self.connection.commit()
+        return self.cursor.lastrowid
