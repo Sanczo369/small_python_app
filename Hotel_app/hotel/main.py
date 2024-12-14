bd7e20bb3f744066c5fd9c68e38dacbc5ef28081
Arek Sanecki <areksanecki@gmail.com>
2024-12-14 19:25:33 +0100

    Projekt "Hotel_app" main.py Pobierz imię i nazwisko klienta

--- Hotel_app/hotel/main.py
+++ Hotel_app/hotel/main.py
@@ -8,3 +8,7 @@ def main():
     # Dodaj klienta
     client_id = hotel.add_client("Mark", "Lee", 35)
     print(f"Dodano klienta o ID: {client_id}")
+
+    # Pobierz imię i nazwisko klienta
+    name = hotel.get_client_name(client_id)
+    print(f"Imię i nazwisko klienta: {name}")
