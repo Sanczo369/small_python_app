cb3559b18af31b338024ba6dd654e18a0708a035
Arek Sanecki <areksanecki@gmail.com>
2024-12-14 19:25:21 +0100

    Projekt "Hotel_app" main.py Dodaj klienta

---
+++ Hotel_app/hotel/main.py
@@ -0,0 +1,10 @@
+from hotel.hotel import Hotel
+
+DB_PATH = "hotel.db"
+
+def main():
+    hotel = Hotel(DB_PATH)
+
+    # Dodaj klienta
+    client_id = hotel.add_client("Mark", "Lee", 35)
+    print(f"Dodano klienta o ID: {client_id}")
