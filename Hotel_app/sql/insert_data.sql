4f8b7adbc2bd032b323f925dc7ced728b3fc08c8
Arek Sanecki <areksanecki@gmail.com>
2024-12-15 16:22:45 +0100

    Projekt "Hotel_app" insert_data.sql INSERT INTO reservations

--- Hotel_app/sql/insert_data.sql
+++ Hotel_app/sql/insert_data.sql
@@ -7,3 +7,7 @@ INSERT INTO rooms (floor, bed_type, area) VALUES
 (1, 'queen', 20.5),
 (2, 'king', 25.0),
 (1, 'single', 15.0);
+
+INSERT INTO reservations (client_id, room_id, date) VALUES
+(1, 1, '2024-12-20'),
+(2, 2, '2024-12-21');
