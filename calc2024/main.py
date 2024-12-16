7c8597c456235a64c5b46f9cf8ee08e18a52e327
Arek Sanecki <areksanecki@gmail.com>
2024-12-16 23:33:40 +0100

    Projekt "calc2024" main.py calculate()

--- calc2024/main.py
+++ calc2024/main.py
@@ -19,3 +19,14 @@ def clear():
     global equation
     equation =""
     label_result.config(text=equation)
+
+def calculate():
+    global equation
+    result=""
+    if equation!="":
+        try:
+            result=eval(equation)
+        except:
+            result="Error"
+            equation= ""
+        label_result.config(text=result)
