1f4543dd95f5d11220b2b6a4b157d105ba4367aa
Arek Sanecki <areksanecki@gmail.com>
2024-12-16 23:31:42 +0100

    Projekt "calc2024" main.py

---
+++ calc2024/main.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title("CALCULATOR")
+root.geometry("570x600")
+root.resizable(False,False)
+root.configure(bg="#17161b")
+root.iconbitmap(r'C:\coding in different different languages\PYTHON\CALCULATOR\Calculator-icon.ico')
+
+equation=""
