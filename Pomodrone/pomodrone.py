61dbb9a907ee09d316c7996b433016185159165e
Arek Sanecki <areksanecki@gmail.com>
2024-12-17 18:58:32 +0100

    Projekt "Pomodrone" pomodrone.py work()

--- Pomodrone/pomodrone.py
+++ Pomodrone/pomodrone.py
@@ -16,3 +16,16 @@ class Pomodoro:
         self.sec.set(f"{seconds:02d}")
         self.root.update()
         time.sleep(1)
+
+        def work(self):
+            timer = 25 * 60
+            while timer >= 0:
+                pomo.work_break(timer)
+                if timer == 0:
+                    # once work is done play
+                    # a sound and switch for break
+                    playsound("sound.ogg")
+                    messagebox.showinfo(
+                        "Good Job", "Take A Break, \
+        				nClick Break Button")
+                timer -= 1
