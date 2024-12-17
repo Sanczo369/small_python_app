0ce2cda6cea76c3d9876b94ad589446a910b11b9
Arek Sanecki <areksanecki@gmail.com>
2024-12-17 18:56:01 +0100

    Projekt "Piano Application " piano.py piano() __init__

--- Piano Application/piano.py
+++ Piano Application/piano.py
@@ -18,3 +18,12 @@ frame1.place(x=0,y=100)
 class piano():
     def PianoSound(self, sound):
         playsound(f'Piano{sound}.mp3')
+
+    def __init__(self, index):
+        self.index = index
+        if self.index % 2 != 0:
+            Button(frame1, padx=10, pady=100, bg="black", fg="white", relief=RAISED, borderwidth=3,
+                   command=lambda: self.PianoSound(self.index), cursor="hand2").grid(row=0, column=self.index)
+        else:
+            Button(frame1, padx=10, pady=100, bg="white", fg="black", relief=RAISED, borderwidth=2,
+                   command=lambda: self.PianoSound(self.index), cursor="hand2").grid(row=0, column=self.index)
