0959f2f5348550ed415b1c061413d3ad160c1017
Arek Sanecki <areksanecki@gmail.com>
2024-12-17 18:58:14 +0100

    Projekt "Pomodrone" pomodrone.py work_break()

--- Pomodrone/pomodrone.py
+++ Pomodrone/pomodrone.py
@@ -7,3 +7,12 @@ import time
 class Pomodoro:
 	def __init__(self, root):
 		self.root = root
+
+    def work_break(self, timer):
+        # common block to display minutes
+        # and seconds on GUI
+        minutes, seconds = divmod(timer, 60)
+        self.min.set(f"{minutes:02d}")
+        self.sec.set(f"{seconds:02d}")
+        self.root.update()
+        time.sleep(1)
