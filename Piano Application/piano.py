dd150e3f8cbb3a7f00aa98836fa3ed8534c7da4c
Arek Sanecki <areksanecki@gmail.com>
2024-12-17 18:55:37 +0100

    Projekt "Piano Application " piano.py piano()

--- Piano Application/piano.py
+++ Piano Application/piano.py
@@ -13,3 +13,8 @@ icon_label = Label(root,image=icon)
 icon_label.place(x=295,y=10)
 frame1 = Frame(root,width=700,height=198,bg="white")
 frame1.place(x=0,y=100)
+
+
+class piano():
+    def PianoSound(self, sound):
+        playsound(f'Piano{sound}.mp3')
