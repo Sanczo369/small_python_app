234387d95f047f25799a3949d7094f66c31eee20
Arek Sanecki <areksanecki@gmail.com>
2024-12-18 13:34:05 +0100

    Projekt "Name Generators" name-generator.py

---
+++ Name Generators/name-generator.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import tkinter as tk
+from tkinter.ttk import Combobox
+from tkinter.scrolledtext import ScrolledText
+import names
+
+root  = tk.Tk()
+root.geometry('400x200')
+root.title("Name Generators")
