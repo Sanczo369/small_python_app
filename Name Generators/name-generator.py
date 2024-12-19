f58407f9f9d5fab9e29c064f59312d3894982546
Arek Sanecki <areksanecki@gmail.com>
2024-12-19 23:54:37 +0100

    Projekt "Name Generators" name-generator.py search(Female)

--- Name Generators/name-generator.py
+++ Name Generators/name-generator.py
@@ -22,3 +22,13 @@ def search():
     elif Gender == 'Male' and Type == "Last Name":
         name = names.get_last_name()
         text.insert('end', name)
+
+    elif Gender == 'Female' and Type == "Full Name":
+        name = names.get_full_name(gender="female")
+        text.insert('end', name)
+    elif Gender == 'Female' and Type == "First Name":
+        name = names.get_first_name()
+        text.insert('end', name)
+    elif Gender == 'Female' and Type == "Last Name":
+        name = names.get_last_name()
+        text.insert('end', name)
