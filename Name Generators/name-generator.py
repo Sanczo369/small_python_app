5ace9d6a34f310d26f3b10b4692db308caa78bad
Arek Sanecki <areksanecki@gmail.com>
2024-12-19 23:54:21 +0100

    Projekt "Name Generators" name-generator.py search(Male)

--- Name Generators/name-generator.py
+++ Name Generators/name-generator.py
@@ -7,3 +7,18 @@ import names
 root  = tk.Tk()
 root.geometry('400x200')
 root.title("Name Generators")
+
+
+def search():
+    Gender = gender.get()
+    Type = types.get()
+
+    if Gender == 'Male' and Type == "Full Name":
+        name = names.get_full_name(gender="male")
+        text.insert('end', name)
+    elif Gender == 'Male' and Type == "First Name":
+        name = names.get_first_name()
+        text.insert('end', name)
+    elif Gender == 'Male' and Type == "Last Name":
+        name = names.get_last_name()
+        text.insert('end', name)
