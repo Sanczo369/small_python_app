455cae38d1a38b6df2c949c28605bd34785d504e
Arek Sanecki <areksanecki@gmail.com>
2024-12-23 22:18:11 +0100

    Projekt "TKinter Hangman Game" main.py  Application() create_widgets() # Create instruction label for Program

--- TKinter Hangman Game/main.py
+++ TKinter Hangman Game/main.py
@@ -5,3 +5,12 @@ class Application(Frame):
         Frame.__init__(self, master)
         self.grid()
         self.create_widgets()
+
+    def create_widgets(self):
+        """ Create button, text, and entry widgets. """
+
+        """ Instruction Label """
+
+        # Create instruction label for Program
+        self.inst_lbl = Label(self, text="Welcome to Guess the Word!")
+        self.inst_lbl.grid(row=0, column=0, columnspan=2, sticky=W)
