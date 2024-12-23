a1bcb2a1eb6156393d9389b93da8a612d8236bbf
Arek Sanecki <areksanecki@gmail.com>
2024-12-23 22:18:48 +0100

    Projekt "TKinter Hangman Game" main.py  Application() create_widgets() # Create label for entering Guess

--- TKinter Hangman Game/main.py
+++ TKinter Hangman Game/main.py
@@ -14,3 +14,9 @@ class Application(Frame):
         # Create instruction label for Program
         self.inst_lbl = Label(self, text="Welcome to Guess the Word!")
         self.inst_lbl.grid(row=0, column=0, columnspan=2, sticky=W)
+
+        """ Guess Input """
+
+        # Create label for entering Guess
+        self.guess_lbl = Label(self, text="Enter your Guess:")
+        self.guess_lbl.grid(row=2, column=0, sticky=W)
