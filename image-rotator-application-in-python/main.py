23dd2fca6e3b28dedfe321dd872394015666e8de
Arek Sanecki <areksanecki@gmail.com>
2024-12-31 18:46:58 +0100

    Projekt "Image-rotator-application-in-python" main.py Image_Rotator # Configuring the menubar

--- image-rotator-application-in-python/main.py
+++ image-rotator-application-in-python/main.py
@@ -39,3 +39,15 @@ class Image_Rotator:
         exit = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label='Exit', menu=exit)
         exit.add_command(label='Exit', command=self.Exit)
+
+        # Configuring the menubar
+        self.window.config(menu=self.menubar)
+        # ===================End=======================
+
+        left_btn = Button(self.window, text="Left",
+        font=("Helvetica", 15, 'bold'), command=self.rotate_left)
+        left_btn.place(x=15,y=270)
+
+        right_btn = Button(self.window, text="Right",
+        font=("Helvetica", 15, 'bold'), command=self.rotate_right)
+        right_btn.place(x=865,y=270)
