2df33fbf0b6fa2aa0843e9baea3167e35c9dcf2e
Arek Sanecki <areksanecki@gmail.com>
2024-12-31 18:45:54 +0100

    Projekt "Image-rotator-application-in-python" main.py Image_Rotator __init__

--- image-rotator-application-in-python/main.py
+++ image-rotator-application-in-python/main.py
@@ -3,3 +3,18 @@ import cv2
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog, messagebox
+
+class Image_Rotator:
+    def __init__(self, root):
+        self.window = root
+        self.window.geometry("960x600")
+        self.window.title('Image Rotator')
+        self.window.resizable(width=False, height=False)
+
+        self.img = img
+        self.rot_image = rot_image
+        self.image_name = image_name
+        self.Image_Path = image_path
+
+        self.width = 740
+        self.height = 480
