517e96475d29e023b5ba8f02be2013cdb9715cc6
Arek Sanecki <areksanecki@gmail.com>
2025-01-02 23:40:35 +0100

    Projekt "Multiple Files Renamer" main.py #File Extensions: You can modify it as per your requirements

--- Multiple Files Renamer/main.py
+++ Multiple Files Renamer/main.py
@@ -4,3 +4,6 @@ from tkinter import *
 from threading import *
 from PIL import ImageTk, Image
 from tkinter import messagebox, ttk, filedialog
+
+# File Extensions: You can modify it as per your requirements
+file_types = ['.jpg', 'jpeg', '.png', '.mp3', '.mp4', '.pdf']
