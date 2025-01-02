197005524b415a970804c75fbad05f7a0444e364
Arek Sanecki <areksanecki@gmail.com>
2025-01-02 23:41:02 +0100

    Projekt "Multiple Files Renamer" main.py File_Renamer(__init__)

--- Multiple Files Renamer/main.py
+++ Multiple Files Renamer/main.py
@@ -7,3 +7,13 @@ from tkinter import messagebox, ttk, filedialog
 
 # File Extensions: You can modify it as per your requirements
 file_types = ['.jpg', 'jpeg', '.png', '.mp3', '.mp4', '.pdf']
+
+
+class File_Renamer:
+    def __init__(self, root):
+        # Setting the Tkinter main window
+        self.window = root
+        self.window.geometry("720x500")
+        self.window.title('File Renamer - PySeek')
+        self.window.resizable(width = False, height = False)
+        self.window.configure(bg='gray90')
