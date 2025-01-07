832de8e7b3b75b39a6dc682ad6dd4e8af0ca6fcb
Arek Sanecki <areksanecki@gmail.com>
2025-01-07 23:50:11 +0100

    Projekt "PDF Editor" main.py #Import the necessary modules

---
+++ PDF Editor/main.py
@@ -0,0 +1,9 @@
+import os
+import PyPDF2
+import os.path
+from tkinter import *
+from functools import partial
+from tkinter import filedialog
+from PyPDF2 import PdfFileReader
+from tkinter import ttk, messagebox
+from PyPDF2.pdf import PdfFileWriter
