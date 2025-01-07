c7213cdb59d1107e511e9ffe4e768d67dffa19f5
Arek Sanecki <areksanecki@gmail.com>
2025-01-07 23:50:49 +0100

    Projekt "PDF Editor" main.py #Declare PDF Editor Class __init__

--- PDF Editor/main.py
+++ PDF Editor/main.py
@@ -7,3 +7,9 @@ from tkinter import filedialog
 from PyPDF2 import PdfFileReader
 from tkinter import ttk, messagebox
 from PyPDF2.pdf import PdfFileWriter
+
+class PDF_Editor:
+    def __init__(self, root):
+        self.window = root
+        self.window.geometry("740x480")
+        self.window.title('PDF Editor')
