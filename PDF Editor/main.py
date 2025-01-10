edece71d0f48fad330e55181631fecc148959d13
Arek Sanecki <areksanecki@gmail.com>
2025-01-10 11:10:36 +0100

    Projekt "PDF Editor" main.py Merge_PDFs_Data # Constant

--- PDF Editor/main.py
+++ PDF Editor/main.py
@@ -195,3 +195,8 @@ class PDF_Editor:
         Cur_Directory = Label(self.frame_1, text="Storing Location",
         font=(self.font_2, 18, 'bold'), bg=self.color_2, fg=self.color_1)
         Cur_Directory.place(x=40, y= 150)
+
+        # Constant
+        self.path_label = Label(self.frame_1, text='/',
+        font=(self.font_2, 16, 'bold'), bg=self.color_2, fg=self.color_3)
+        self.path_label.place(x=40, y= 190)
