ff89e13276c1dc8157b9a127b7f5251c816ffa32
Arek Sanecki <areksanecki@gmail.com>
2025-01-15 13:48:13 +0100

    Projekt "Music Player v2" main.py MusicPlayer(__init__)

--- Music_Player_v2/main.py
+++ Music_Player_v2/main.py
@@ -2,3 +2,9 @@ import os
 import vlc
 import tkinter as tk
 from tkinter import filedialog
+
+class MusicPlayer:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Music Player")
+        self.window.geometry("500x340")
