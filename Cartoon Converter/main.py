718a7e90066ed7d3f3667cc9de035a8c073456af
Arek Sanecki <areksanecki@gmail.com>
2025-01-21 10:28:49 +0100

    Projekt "Cartoon Converter" main.py # init in Image_Cartoonify class

---
+++ Cartoon Converter/main.py
@@ -0,0 +1,18 @@
+import cv2
+import pathlib
+import pyautogui
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+
+class Image_Cartoonify:
+    def __init__(self, root):
+        self.window = root
+        self.window.geometry("960x560")
+        self.window.title('Cartoonify')
+        self.window.resizable(width = False, height = False)
+
+        self.width = 740
+        self.height = 480
+
+        self.Image_Path = ''
