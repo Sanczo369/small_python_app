e1ac4212d6fe9cb4e3579b734954c326253568d0
Arek Sanecki <areksanecki@gmail.com>
2025-01-21 10:26:02 +0100

    Projekt "Image Rotator Application" main.py # fix error

--- Image Rotator Application/main.py
+++ Image Rotator Application/main.py
@@ -4,6 +4,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog, messagebox
 
+
 class Image_Rotator:
     def __init__(self, root):
         self.window = root
@@ -25,7 +26,7 @@ class Image_Rotator:
         # Adding Edit Menu and its sub menus
         edit = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label='Open', menu=edit)
-        edit.add_command(label='Open Image',command=self.open_image)
+        edit.add_command(label='Open Image', command=self.open_image)
 
         # Adding About Menu
         about = Menu(self.menubar, tearoff=0)
@@ -35,33 +36,27 @@ class Image_Rotator:
         # Exit the Application
         exit = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label='Exit', menu=exit)
-        exit.add_command(label='Exit', command=self.Exit)
+        exit.add_command(label='Exit', command=self.exit)
 
         # Configuring the menubar
         self.window.config(menu=self.menubar)
         # ===================End=======================
 
+        left_btn = Button(self.window, text="Left", font=("Helvetica", 15, 'bold'), command=self.rotate_left)
+        left_btn.place(x=15, y=270)
 
-        left_btn = Button(self.window, text="Left",
-        font=("Helvetica", 15, 'bold'), command=self.rotate_left)
-        left_btn.place(x=15,y=270)
-
-        right_btn = Button(self.window, text="Right",
-        font=("Helvetica", 15, 'bold'), command=self.rotate_right)
-        right_btn.place(x=865,y=270)
+        right_btn = Button(self.window, text="Right", font=("Helvetica", 15, 'bold'), command=self.rotate_right)
+        right_btn.place(x=865, y=270)
 
         # Creating a Frame
-        self.frame_1 = Frame(self.window,
-        width=self.width,height=self.height)
+        self.frame_1 = Frame(self.window, width=self.width, height=self.height)
         self.frame_1.pack()
         self.frame_1.place(anchor='center', relx=0.5, rely=0.5)
 
     # Onen an Image
     def open_image(self):
         self.clear_screen()
-        self.Image_Path =
-        filedialog.askopenfilename(title = "Select an Image",
-        filetypes = (("Image files", "*.jpg *.jpeg *.png"),))
+        self.Image_Path = filedialog.askopenfilename(title="Select an Image", filetypes=(("Image files", "*.jpg *.jpeg *.png"),))
         if len(self.Image_Path) != 0:
             self.image_name = os.path.basename(self.Image_Path)
             if self.image_name != '':
@@ -108,12 +103,11 @@ class Image_Rotator:
 
     # About window
     def about(self):
-        messagebox.showinfo("Image Rotator",
-        "Image Rotator AppnDeveloped by Subhankar Rakshit")
+        messagebox.showinfo("Image Rotator", "Image Rotator AppnDeveloped by Subhankar Rakshit")
 
     # It destroys the main GUI window of the
     # application
-    def Exit(self):
+    def exit(self):
         self.window.destroy()
 
 
