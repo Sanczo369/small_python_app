abec2faf0d2fb7686ba1f2b1fb361673ec181efb
Arek Sanecki <areksanecki@gmail.com>
2025-01-24 23:02:04 +0100

    Projekt "Image Encryption App" main() #Import the Necessary Modules

---
+++  Image Encryption App/main.py
@@ -0,0 +1,15 @@
+import os
+import io
+import random
+import string
+import numpy as np
+import customtkinter
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+from Crypto.Util import Counter
+from Crypto.Hash import SHA256, HMAC
