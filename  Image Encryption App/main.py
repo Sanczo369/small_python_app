1af293abfe50d825df294ab9c03e9393fbf7f99f
Arek Sanecki <areksanecki@gmail.com>
2025-01-28 10:13:02 +0100

    Projekt "Image Encryption App" display_decrypted_image() # Create a new inner frame for the resized imagek

---  Image Encryption App/main.py
+++  Image Encryption App/main.py
@@ -185,3 +185,7 @@ class ImageEncryption:
 
         # Create an object of tkinter ImageTk
         self.image = ImageTk.PhotoImage(resized_image)
+
+        # Create a new inner frame for the resized image
+        inner_frame = Frame(self.frame1, width=self.width, height=self.height)
+        inner_frame.pack()
