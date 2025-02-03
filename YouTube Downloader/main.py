e50df4ed438cff95fe1a49b626c8113e3a029af9
Arek Sanecki <areksanecki@gmail.com>
2025-02-03 23:07:42 +0100

    Projekt "YouTube Downloader" #Video Resolutions and Audio Selection

--- YouTube Downloader/main.py
+++ YouTube Downloader/main.py
@@ -6,3 +6,6 @@ from pytube import YouTube
 from PIL import ImageTk, Image
 from tkinter import filedialog
 from tkinter import messagebox, ttk
+
+# Resolution Options(Audio Option is also available)
+download_quality = ['144p', '360p', '720p', 'Audio Only']
