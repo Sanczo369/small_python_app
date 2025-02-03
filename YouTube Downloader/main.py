c597727b762c5fbc789acd1c3650ce1b3b1e627c
Arek Sanecki <areksanecki@gmail.com>
2025-02-03 23:11:49 +0100

    Projekt "YouTube Downloader" #class Yt_Downloader(__init__)

--- YouTube Downloader/main.py
+++ YouTube Downloader/main.py
@@ -9,3 +9,13 @@ from tkinter import messagebox, ttk
 
 # Resolution Options(Audio Option is also available)
 download_quality = ['144p', '360p', '720p', 'Audio Only']
+
+class Yt_Downloader:
+    def __init__(self, root):
+        # Setting the Tkinter main window
+        self.window = root
+        self.window.geometry("700x480")
+        self.window.title('YouTube Video and Audio Downloader')
+        self.window.resizable(width=False, height=False)
+
+        self.save_to_loc = ''
