49fd47762fa9ff478b36ca4ea55a0be45716ed22
Arek Sanecki <areksanecki@gmail.com>
2025-02-03 23:07:25 +0100

    Projekt "YouTube Downloader" #Import Necessary Modules

---
+++ YouTube Downloader/main.py
@@ -0,0 +1,8 @@
+import pyperclip
+from tkinter import *
+from threading import *
+from tkinter import ttk
+from pytube import YouTube
+from PIL import ImageTk, Image
+from tkinter import filedialog
+from tkinter import messagebox, ttk
