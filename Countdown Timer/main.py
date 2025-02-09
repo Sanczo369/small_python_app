c5b46e9456ed0cae6f7b15e012c7a0bc5765da07
Arek Sanecki <areksanecki@gmail.com>
2025-02-09 21:31:27 +0100

    Project "Countdown Timer " start_time() # When the time is over, a piece of music will

--- Countdown Timer/main.py
+++ Countdown Timer/main.py
@@ -131,3 +131,13 @@ class CountDown:
             # sleep function: for 1 second
             time.sleep(1)
             self.time_left = self.time_left -1
+            # When the time is over, a piece of music will
+            # play in the background
+            if self.time_left <= 0:
+                process = multiprocessing.Process(target=playsound,
+                args=('Ringtones/romantic.mp3',))
+                process.start()
+                messagebox.showinfo('Time Over', 'Please ENTER to stop playing')
+                process.terminate()
+                # Clearing the 'self.button_frame' frame
+                self.Clear_Screen()
