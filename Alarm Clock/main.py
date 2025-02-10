4da6c8a52e5093031b13ad4d0116608c23d4b037
Arek Sanecki <areksanecki@gmail.com>
2025-02-10 23:46:34 +0100

    Project " Alarm Clock" # Import the modules

---
+++ Alarm Clock/main.py
@@ -0,0 +1,8 @@
+import time
+from tkinter import *
+from PIL import ImageTk
+from tkinter import ttk, messagebox
+from playsound import playsound
+import multiprocessing
+from datetime import datetime
+from threading import *
