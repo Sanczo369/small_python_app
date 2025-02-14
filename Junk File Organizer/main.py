16a7168969a6a14f381147e2fdc8f7a4fa63b35b
Arek Sanecki <areksanecki@gmail.com>
2025-02-14 19:01:46 +0100

    Project "Junk File Organizer" # Available File Types

--- Junk File Organizer/main.py
+++ Junk File Organizer/main.py
@@ -4,3 +4,12 @@ from tkinter import *
 from threading import *
 from PIL import ImageTk, Image
 from tkinter import messagebox, filedialog
+
+file_types={
+   'Documents' : ('.pdf','.doc','.xls','txt','.csv','.xml','.zip', '.docx', '.DOCX', '.odt'),
+   'Pictures' : ('.jpg','.jpeg','.png','.JPG', '.webp'),
+   'Videos' : ('.mp4','.mkv','.3gp','.flv','.mpeg'),
+   'Music' : ('.mp3','.wav','.m4a','.webm'),
+   'Programs' : ('.py','.cpp','.c','.sh','.js'),
+   'Apps' : ('.exe','.apk'),
+}
