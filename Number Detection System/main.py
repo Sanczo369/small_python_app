b9344bc3c69c0c2e6a097a04cc7df6ec8fba4aa0
Arek Sanecki <areksanecki@gmail.com>
2025-02-14 18:58:31 +0100

    Project "Number Detection System" # Perform object detection

--- Number Detection System/main.py
+++ Number Detection System/main.py
@@ -34,3 +34,32 @@ video_capture = cv2.VideoCapture(0)
 capture_thread = threading.Thread(target=capture_video, args=(video_capture,))
 capture_thread.daemon = True
 capture_thread.start()
+
+while True:
+    # Perform object detection
+    results = yolo_model(frame)
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+            w, h = x2 - x1, y2 - y1
+
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+
+            if conf > 0.3:
+                cvzone.cornerRect(frame, (x1, y1, w, h), t=2)
+                cvzone.putTextRect(frame, f'{class_labels[cls]} {conf}', (x1, y1 - 10), scale=0.8, thickness=1,
+                                   colorR=(255, 0, 0))
+
+    # Display the frame with detections
+    cv2.imshow("Image", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cv2.waitKey(1)
