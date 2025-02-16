7f33521dc6b728552c256efe14b09e0a6dafc1f9
Arek Sanecki <areksanecki@gmail.com>
2025-02-16 23:39:11 +0100

    Project "Typing Speed Tester" # The Welcome Window

--- Typing Speed Tester/main.py
+++ Typing Speed Tester/main.py
@@ -29,3 +29,11 @@ class TypingTest:
 
         # Calling the function, startWindow()
         self.startWindow()
+
+    def startWindow(self):
+        # A Tkinter Label to display the title
+        titleLabel = Label(self.frame, text="Typing Speed Test", bg=st.color2, fg=st.color1 ,font=(st.font3, 35, "bold"))
+        titleLabel.place(x=175, y=80)
+
+        startButton = Button(self.frame, text="Start Here", border=0, cursor='hand2' ,fg=st.color2, bg=st.color1, font=(st.font4, 18), command=self.startTest)
+        startButton.place(x=320, y=215)
