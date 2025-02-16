16bb1afe8e12353f0a6ef6538b79deef22b16fa4
Arek Sanecki <areksanecki@gmail.com>
2025-02-16 23:37:22 +0100

    Project "Typing Speed Tester" # Import Necessary Modules

---
+++ Typing Speed Tester/main.py
@@ -0,0 +1,8 @@
+import time
+import random
+import text as tt
+from tkinter import *
+import settings as st
+from threading import *
+from PIL import Image, ImageTk
+from pynput.keyboard import Listener
