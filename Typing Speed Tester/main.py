f405a557b3bbdbc8b0eac5290afe00f5487def95
Arek Sanecki <areksanecki@gmail.com>
2025-02-16 23:37:42 +0100

    Project "Typing Speed Tester" # Create the Main Application Window

--- Typing Speed Tester/main.py
+++ Typing Speed Tester/main.py
@@ -6,3 +6,12 @@ import settings as st
 from threading import *
 from PIL import Image, ImageTk
 from pynput.keyboard import Listener
+
+class TypingTest:
+    def __init__(self, root):
+        # Window Settings
+        self.window = root
+        self.window.geometry(f"{st.width}x{st.height}")
+        self.window.title('Typing Tester')
+        self.window.resizable(width=False, height=False)
+        self.window.configure(bg=st.color2)
