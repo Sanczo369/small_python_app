39d383c55d8ce23e44bf289ef97579de0e4e3545
Arek Sanecki <areksanecki@gmail.com>
2025-02-18 23:36:56 +0100

    Project " Create a Game in Python" # Background Window Image

--- Create a Game in Python/main.py
+++ Create a Game in Python/main.py
@@ -16,3 +16,6 @@ class AppleCatcher:
         self.screen = pg.display.set_mode((self.settings.screen_width, \
         self.settings.screen_height), self.settings.flag)
         pg.display.set_caption('Apple Catcher Game')
+
+        # Background Window Image
+        self.background = pg.image.load('Images/background.png')
