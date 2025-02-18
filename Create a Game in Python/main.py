43feb0303bab39d58806c8339c1170f3ee81dfca
Arek Sanecki <areksanecki@gmail.com>
2025-02-18 23:36:13 +0100

    Project " Create a Game in Python" # includes methods for initializing the game

---
+++ Create a Game in Python/main.py
@@ -0,0 +1,9 @@
+import pygame as pg
+import sys
+from apple import Apple
+from settings import Settings
+from basket import Basket
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+from sounds import Music
