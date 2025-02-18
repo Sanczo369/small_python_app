cbc2366caf218e0b237b2a436b28b81fa8ae5657
Arek Sanecki <areksanecki@gmail.com>
2025-02-18 23:36:35 +0100

    Project " Create a Game in Python" # AppleCatcher init

--- Create a Game in Python/main.py
+++ Create a Game in Python/main.py
@@ -7,3 +7,12 @@ from game_stats import GameStats
 from button import Button
 from scoreboard import Scoreboard
 from sounds import Music
+
+class AppleCatcher:
+    def __init__(self):
+        pg.init()
+
+        self.settings = Settings()
+        self.screen = pg.display.set_mode((self.settings.screen_width, \
+        self.settings.screen_height), self.settings.flag)
+        pg.display.set_caption('Apple Catcher Game')
