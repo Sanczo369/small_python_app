7e0b1b1c08c3bc028206e8a4db7887e4399a5f87
Arek Sanecki <areksanecki@gmail.com>
2025-02-21 22:37:52 +0100

    Project " Create a Game in Python" basket.py # Settings blitme

--- Create a Game in Python/basket.py
+++ Create a Game in Python/basket.py
@@ -21,3 +21,18 @@ class Basket:
         # Movement flag
         self.moving_right = False
         self.moving_left = False
+
+    def update(self):
+        """Update the basket position based on the movement flag."""
+        # Update the basket's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.basket_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.basket_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the basket at its current location"""
+        self.screen.blit(self.image, self.rect)
