c56230dbe74ff15c79faf7717b5b0fa54ef19915
Arek Sanecki <areksanecki@gmail.com>
2025-02-21 23:20:52 +0100

    Project " Create a Game in Python" button.py

---
+++ Create a Game in Python/button.py
@@ -0,0 +1,19 @@
+import pygame.font
+
+class Button:
+    def __init__(self, ac_game, text):
+        self.screen = ac_game.screen
+        self.screen_rect = ac_game.screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center it
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        self._show_text(text)
