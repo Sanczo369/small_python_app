f22ac4c0272913c5052c525967e0c4b6a791f068
Arek Sanecki <areksanecki@gmail.com>
2025-02-22 18:51:37 +0100

    Project " Create a Game in Python" apple.py class Apple

---
+++ Create a Game in Python/apple.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+from pygame.sprite import Sprite
+
+
+class Apple(Sprite):
+    '''A class to manage the Apples'''
+
+    def __init__(self, ac_game):
+        '''Initialize the apple and set its starting position.'''
+        super().__init__()
+        self.screen = ac_game.screen
+        self.settings = ac_game.settings
+        self.screen_rect = ac_game.screen.get_rect()
+
+        # Load the Apple Image
+        self.image = pygame.image.load('Images/apple.bmp')
+        self.rect = self.image.get_rect()
+
+        # Define the location from where the apple will enter
+        self.rect.x = random.randrange(20, self.screen_rect.right - 30)
+        self.rect.y = 0
+
+        # Store the decimal value for the apple's vertical position.
+        self.y = float(self.rect.y)
