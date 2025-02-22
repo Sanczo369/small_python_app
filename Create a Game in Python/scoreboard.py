4783281611a222ce3f1d369981568be2085a1f18
Arek Sanecki <areksanecki@gmail.com>
2025-02-22 18:47:59 +0100

    Project " Create a Game in Python" scoreboard.py Scoreboard prep_score()

--- Create a Game in Python/scoreboard.py
+++ Create a Game in Python/scoreboard.py
@@ -36,3 +36,12 @@ class Scoreboard:
             apple.rect.x = 10 + apple_number * apple.rect.width
             apple.rect.y = 10
             self.apples.add(apple)
+
+    # It shows the total score at the top right corner of the screen.
+    def prep_score(self):
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True,
+        self.text_color, self.settings.bg_color)
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
