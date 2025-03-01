7f91dc1af410f8f3c2781a6646acb38c736506d9
Arek Sanecki <areksanecki@gmail.com>
2025-03-01 20:34:28 +0100

    Project "SpaceShip" main.py # Game Loop

--- SpaceShip/main.py
+++ SpaceShip/main.py
@@ -90,3 +90,15 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
         return True
     else:
         return False
+
+# Game Loop
+running = True
+while running:
+
+    # RGB = Red, Green, Blue
+    screen.fill((0, 0, 0))
+    # Background Image
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
