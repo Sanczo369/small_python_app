1b4e9e52ce1ac51918843b8188ab31e5eb0aae7a
Arek Sanecki <areksanecki@gmail.com>
2025-03-02 17:57:37 +0100

    Project "monster_run" main.py # class Obstacle() init()

--- monster_run/main.py
+++ monster_run/main.py
@@ -45,3 +45,23 @@ class Player(pygame.sprite.Sprite):
 		self.player_input()
 		self.apply_gravity()
 		self.animation_state()
+
+
+class Obstacle(pygame.sprite.Sprite):
+	def __init__(self, type):
+		super().__init__()
+
+		if type == 'fly':
+			fly_1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+			fly_2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+			self.frames = [fly_1, fly_2]
+			y_pos = 210
+		else:
+			snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+			snail_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+			self.frames = [snail_1, snail_2]
+			y_pos = 300
+
+		self.animation_index = 0
+		self.image = self.frames[self.animation_index]
+		self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
